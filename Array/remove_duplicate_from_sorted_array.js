3ba466f31aeaec946a79d56c81aed7f1408730dd
Naman <namansingla830@gmail.com>
2026-05-20 12:41:26 +0530

    Optimal Solution (Two Pointer) for removeDuplicates

--- Array/remove_duplicate_from_sorted_array.js
+++ Array/remove_duplicate_from_sorted_array.js
@@ -4,17 +4,33 @@
 
 // Approach 1 — Brute Force
 // Using a Set (Use another data structure like Set to store unique values.)
-
-function removeDuplicates(nums){
+// TC: O(n) and SC: O(n)
+function removeDuplicates(nums) {
     // Create a Set to store unique values
-    let unique = [...new Set(nums)]
+    let unique = [...new Set(nums)];
     // Traverse the unique array and modify the original array in-place
     for (let i = 0; i < unique.length; i++) {
-        nums[i] = unique[i]
+        nums[i] = unique[i];
     }
-    return unique.length
+    return unique.length;
 }
 
-let nums = [1,1,2,2,3];
-console.log(removeDuplicates(nums));
+// Approach 2 - Optimal Solution (Two Pointer)
+// one pointer for unique position
+// one pointer for traversal
+// TC: O(n) and SC: O(1)
+
+function removeDuplicates1(nums) {
+    let i = 0; // Pointer for unique poisition
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            i++; //Move the unique position pointer
+            nums[i] = nums[j]; // Update the unique position with the new unique value
+        }
+    }
+    return i + 1; // Return the length of the unique array
+}
+
+let nums = [1, 1, 2, 2, 3];
+console.log(removeDuplicates1(nums));
 console.log(nums);

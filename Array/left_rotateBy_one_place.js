2f398550adc35350fb18aab7b422ee603f8cd81a
Naman <namansingla830@gmail.com>
2026-05-21 10:47:16 +0530

    left roate array by one Optimal Solution

---
+++ Array/left_rotateBy_one_place.js
@@ -0,0 +1,21 @@
+// It means:
+// The first element moves to the last
+// All other elements shift one step left
+
+// Optimal Solution (In-place)
+// 1. Store first element
+// 2. Shift every element left
+// 3. Put stored element at end
+// TC: O(n) and SC: O(1)
+
+function leftRotateByOne(arr) {
+  let temp = arr[0];
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr[arr.length - 1] = temp;
+
+  return arr;
+}
+
+console.log(leftRotateByOne([1, 2, 3, 4, 5]));

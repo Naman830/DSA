06687454614efd2527d4ab2632153938d051fd67
Naman <namansingla830@gmail.com>
2026-05-21 12:34:11 +0530

    Optimal solution added

--- Array/unionOf_two_sorted_array.js
+++ Array/unionOf_two_sorted_array.js
@@ -27,4 +27,54 @@ function unionArray(arr1, arr2) {
   return [...set];
 }
 
-console.log(unionArray([1, 2, 3], [2, 3, 4]));
+// Better / Optimal Solution (Two Pointers)
+
+// How do we avoid duplicates?
+// Before inserting into answer:
+// Check: Is this already the last inserted element?
+// If yes:
+// skip it.
+
+// TC: O(n + m) and SC: O(n + m)
+
+function unionArray2(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+
+  let ans = [];
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      if (ans.length === 0 || ans[ans.length - 1] !== arr1[i]) {
+        ans.push(arr1[i]);
+      }
+      i++;
+    } else {
+      if (ans.length === 0 || ans[ans.length - 1] !== arr2[j]) {
+        ans.push(arr2[j]);
+      }
+      j++;
+    }
+  }
+
+  // remaining elements of arr1
+  while (i < arr1.length) {
+    if (ans[ans.length - 1] !== arr1[i]) {
+      ans.push(arr1[i]);
+    }
+
+    i++;
+  }
+
+  // remaining elements of arr2
+  while (j < arr2.length) {
+    if (ans[ans.length - 1] !== arr2[j]) {
+      ans.push(arr2[j]);
+    }
+
+    j++;
+  }
+
+  return ans;
+}
+console.log(unionArray2([1, 2, 3, 4, 5], [2, 3, 4, 4, 5, 6]));

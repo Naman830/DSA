01669ad0ce845f4425d5ac9ac81c9ec1c76385b5
Naman <namansingla830@gmail.com>
2026-05-21 12:16:17 +0530

    brute force solution

--- Array/unionOf_two_sorted_array.js
+++ Array/unionOf_two_sorted_array.js
@@ -1,4 +1,4 @@
-// It means: 
+// It means:
 // Take all UNIQUE elements from both arrays.
 
 // Arrays are already SORTED. [This is a HUGE clue.]
@@ -8,5 +8,23 @@
 // Can I use two pointers? (Usually yes.)
 
 // Brute Force Approach:
-// Take all elements into one data structure that automatically removes duplicates.
-// Set in JavaScript is a good choice for this.
+// Take all elements into one data structure that automatically removes duplicates. [Set in JavaScript is a good choice for this.]
+
+// Time Complexity: O(n + m) where n and m are the lengths of the two arrays.
+// Space Complexity: O(n + m) in the worst case when all elements are unique.
+
+function unionArray(arr1, arr2) {
+  let set = new Set();
+  //  Insert all elemenets of arr1 into the set.
+  for (let num of arr1) {
+    set.add(num);
+  }
+  //  Insert all elemenets of arr2 into the set.
+  for (let num of arr2) {
+    set.add(num);
+  }
+
+  return [...set];
+}
+
+console.log(unionArray([1, 2, 3], [2, 3, 4]));

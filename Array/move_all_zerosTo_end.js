118299dd3d9bb2663107f1659f8d30d63f470752
Naman <namansingla830@gmail.com>
2026-05-21 11:44:07 +0530

    add observation for optimal soultion

--- Array/move_all_zerosTo_end.js
+++ Array/move_all_zerosTo_end.js
@@ -23,3 +23,15 @@ function moveZeros(arr){
 }
 
 console.log(moveZeros([1,0,2,3,0,4,0,1]));
+
+// Better / Optimal Solution (Two Pointer Approach)
+// Instead of using extra array: We move non-zero elements toward left.
+
+// We only care about: Where is the first zero?
+// Because: non-zero elements before it are already correct
+// next non-zero can replace that zero
+
+// We use:
+// i → position of zero
+// j → searching non-zero element
+// We swap ONLY when: arr[j] is non-zero

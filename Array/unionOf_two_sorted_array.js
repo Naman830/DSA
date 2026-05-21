0360bc5d90125d38a9e954c7cf6b0cf464d2624b
Naman <namansingla830@gmail.com>
2026-05-21 12:12:00 +0530

    union of two sorted array observation

---
+++ Array/unionOf_two_sorted_array.js
@@ -0,0 +1,12 @@
+// It means: 
+// Take all UNIQUE elements from both arrays.
+
+// Arrays are already SORTED. [This is a HUGE clue.]
+// Whenever arrays are sorted,
+
+// your brain should think:
+// Can I use two pointers? (Usually yes.)
+
+// Brute Force Approach:
+// Take all elements into one data structure that automatically removes duplicates.
+// Set in JavaScript is a good choice for this.

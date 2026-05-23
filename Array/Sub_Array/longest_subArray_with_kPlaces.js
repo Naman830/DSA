dd70e3f5c140d7babf0a398ca428a48dc62c8186
Naman <namansingla830@gmail.com>
2026-05-23 18:06:32 +0530

    structured subArray in text file

--- Array/Sub_Array/longest_subArray_with_kPlaces.js
+++ Array/Sub_Array/longest_subArray_with_kPlaces.js
@@ -1,27 +1,13 @@
 /*
-Subarray is: A continuous part of an array
+We need subarrays whose sum = 6
 
-Continuous means:
-elements must stay together
-no skipping allowed
+Possible:
 
-Possible subarrays:
+[1,2,3] = 6 → length 3
+[3,1,1,1] = 6 → length 4
+[1,1,1,1,2] not continuous
 
-[1]
-[2]
-[3]
+Answer: 4
 
-[1,2]
-[2,3]
-[3,4]
-
-[1,2,3]
-[2,3,4]
-
-[1,2,3,4,5]
-
-
-Formula of Total Subarrays
-If array size = N
-Then total subarrays: N(N+1)/2
+Because [3,1,1,1] is longest.
 */

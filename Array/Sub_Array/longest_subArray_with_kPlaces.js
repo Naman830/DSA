a75b123c70452445ce17f2968231b2c15c14dd79
Naman <namansingla830@gmail.com>
2026-05-23 18:04:19 +0530

    subarray explained

---
+++ Array/Sub_Array/longest_subArray_with_kPlaces.js
@@ -0,0 +1,27 @@
+/*
+Subarray is: A continuous part of an array
+
+Continuous means:
+elements must stay together
+no skipping allowed
+
+Possible subarrays:
+
+[1]
+[2]
+[3]
+
+[1,2]
+[2,3]
+[3,4]
+
+[1,2,3]
+[2,3,4]
+
+[1,2,3,4,5]
+
+
+Formula of Total Subarrays
+If array size = N
+Then total subarrays: N(N+1)/2
+*/

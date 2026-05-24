f00f7c6c2a4b2164bdf7e11486368e26d32fe103
Naman <namansingla830@gmail.com>
2026-05-24 12:33:47 +0530

    brute force js code

--- Array/two_sum.js
+++ Array/two_sum.js
@@ -12,3 +12,22 @@ for i from 0 to nums.length - 1:
             return [i, j]
 */
 // WHY: j = i + 1? Because we do not want to use the same number twice.
+
+function twoSumBrute(nums, target) {
+  // First loop picks the first number
+  for (let i = 0; i < nums.length; i++) {
+    // Second loop picks the second number
+    // Start from i + 1 because we cannot use the same element twice
+    for (let j = i + 1; j < nums.length; j++) {
+      // Check if both numbers add up to target
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+
+  // If no answer is found
+  return [];
+}
+
+console.log(twoSumBrute([2, 7, 11, 15], 9));

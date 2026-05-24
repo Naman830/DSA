36d54083d31b38f09e57b6c8512caadcb8911c98
Naman <namansingla830@gmail.com>
2026-05-24 12:44:19 +0530

    js code added

--- Array/two_sum.js
+++ Array/two_sum.js
@@ -31,10 +31,6 @@ function twoSumBrute(nums, target) {
   return [];
 }
 
-console.log(twoSumBrute([2, 7, 11, 15], 9));
-
-
-
 // Better Solution Using Hash Map
 // Instead of checking again and again, we store already seen numbers in a map.
 
@@ -43,11 +39,41 @@ console.log(twoSumBrute([2, 7, 11, 15], 9));
 create empty map
 
 for i from 0 to nums.length - 1:
-    current = nums[i]
-    needed = target - current
+current = nums[i]
+needed = target - current
 
-    if needed exists in map:
-        return [map[needed], i]
+if needed exists in map:
+return [map[needed], i]
 
-    store current with index in map
+store current with index in map
 */
+
+function twoSumBetter(nums, target) {
+  // Map will store number as key and its index as value
+  // Example: numberToIndex.set(2, 0)
+  const numberToIndex = new Map();
+
+  // Traverse the array one by one
+  for (let i = 0; i < nums.length; i++) {
+    const currentNumber = nums[i];
+
+    // Find the number we need to complete the target sum
+    const neededNumber = target - currentNumber;
+
+    // Check if needed number already exists in the map
+    if (numberToIndex.has(neededNumber)) {
+      // If yes, we found the answer
+      // Return index of needed number and current index
+      return [numberToIndex.get(neededNumber), i];
+    }
+
+    // If needed number is not found,
+    // store current number with its index for future use
+    numberToIndex.set(currentNumber, i);
+  }
+
+  // If no pair is found
+  return [];
+}
+
+console.log(twoSumBetter([2, 7, 11, 15], 9));

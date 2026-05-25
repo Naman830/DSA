08a1e58d083043e8eca22cbcc7ee9a257bef0215
Naman <namansingla830@gmail.com>
2026-05-25 10:47:00 +0530

    optimal for precompuation

--- Hashing/02_precomputation.js
+++ Hashing/02_precomputation.js
@@ -3,7 +3,6 @@
 // When we use hashing for this, we store the precomputed data in a:
 // HashMap / Object / Map / Array frequency table
 
-
 /*
 Optimal Pseudocode
 create empty hash map
@@ -20,3 +19,36 @@ for each query:
     else:
         print 0
 */
+
+function countFrequenciesUsingHashing(arr, queries) {
+  // Step 1: Create an empty frequency map
+  // Key = number from array
+  // Value = how many times that number appears
+  const frequencyMap = new Map();
+
+  // Step 2: Precompute frequency of every number
+  for (let num of arr) {
+    // If number already exists in map, increase its count
+    if (frequencyMap.has(num)) {
+      frequencyMap.set(num, frequencyMap.get(num) + 1);
+    }
+    // If number is coming for the first time, store count as 1
+    else {
+      frequencyMap.set(num, 1);
+    }
+  }
+
+  // Step 3: Answer each query using precomputed map
+  for (let query of queries) {
+    // If query exists in map, get its frequency
+    // Otherwise answer is 0
+    const count = frequencyMap.get(query) || 0;
+
+    console.log(`${query} appears ${count} times`);
+  }
+}
+
+const arr = [1, 2, 3, 1, 2, 1, 4];
+const queries = [1, 2, 4, 10];
+
+countFrequenciesUsingHashing(arr, queries);

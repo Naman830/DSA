aba635ae493b0f35410257ea126fd734479375cd
Naman <namansingla830@gmail.com>
2026-05-25 11:05:34 +0530

    what is division method

---
+++ Hashing/04_collion_divisionMethod.js
@@ -0,0 +1,11 @@
+/*
+What is Division Method?
+The division method is one of the simplest hashing methods.
+
+Formula: hash(key) = key % tableSize
+
+Here:
+% means remainder
+
+So we divide the key by the table size and use the remainder as the index.
+*/

f699463f7247d3a0ea4a31dfba2457c7793aeaa0
Naman <namansingla830@gmail.com>
2026-05-25 10:29:28 +0530

    what is hashing

---
+++ Hashing/Basic_Of_hashing.js
@@ -0,0 +1,30 @@
+/*
+Imagine you have a school bag.
+Inside the bag, you have many pockets.
+Now suppose you want to keep your pencil in one fixed pocket, your eraser in one fixed pocket, and your sharpener in one fixed pocket.
+
+So next time, when you need your pencil, you do not search the whole bag.
+You directly go to the pencil pocket.
+
+That is the idea of hashing.
+In programming, hashing helps us store and find data very fast.
+Usually, searching in an array takes time.
+
+Example:
+let arr = [10, 20, 30, 40, 50];
+
+If I ask:
+Is 40 present?
+
+You may check:
+10 → no
+20 → no
+30 → no
+40 → yes
+
+This takes time.
+But hashing allows us to find things almost directly.
+*/
+
+// DEFINATION OF HASHING
+// Hashing is a technique where we store data in such a way that we can search, insert, and delete very fast.

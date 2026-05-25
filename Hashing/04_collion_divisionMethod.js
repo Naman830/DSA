2f54bb93c5ad558137dd3f4b616c1d6ba0081c64
Naman <namansingla830@gmail.com>
2026-05-25 11:06:23 +0530

    what is collison

--- Hashing/04_collion_divisionMethod.js
+++ Hashing/04_collion_divisionMethod.js
@@ -9,3 +9,32 @@ Here:
 
 So we divide the key by the table size and use the remainder as the index.
 */
+
+
+/*
+What is Collision?
+A collision happens when two different keys get the same hash index.
+
+Simple meaning:
+Two people want the same locker.
+
+Example:
+tableSize = 10
+
+Keys:
+25 and 35
+
+Calculate:
+hash(25) = 25 % 10 = 5
+hash(35) = 35 % 10 = 5
+
+Both want index 5.
+This is called a collision.
+
+25 -> index 5
+35 -> index 5
+
+Problem:
+Index 5 already has 25.
+Now where should we put 35?
+*/

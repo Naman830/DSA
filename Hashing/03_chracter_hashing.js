2d3739a973e822e6c10765c2a297880d4b3b804f
Naman <namansingla830@gmail.com>
2026-05-25 10:58:04 +0530

    lowercase charcter hasing

--- Hashing/03_chracter_hashing.js
+++ Hashing/03_chracter_hashing.js
@@ -25,3 +25,30 @@ const str = "aabbccabc";
 const queries = ["a", "b", "c", "d"];
 
 countCharacterFrequency(str, queries);
+
+// What If characters are only lowercase English letters a to z, we can use an array of size 26.
+
+function countLowercaseCharacters(str, queries) {
+  // Array of size 26 because there are 26 lowercase letters
+  // frequency[0] stores count of 'a'
+  // frequency[1] stores count of 'b'
+  // frequency[25] stores count of 'z'
+  const frequency = new Array(26).fill(0);
+
+  // Precompute character frequencies
+  for (let ch of str) {
+    const index = ch.charCodeAt(0) - "a".charCodeAt(0);
+    frequency[index]++;
+  }
+
+  // Answer queries
+  for (let query of queries) {
+    const index = query.charCodeAt(0) - "a".charCodeAt(0);
+    console.log(`${query} appears ${frequency[index]} times`);
+  }
+}
+
+const str = "aabbccabc";
+const queries = ["a", "b", "c", "d"];
+
+countLowercaseCharacters(str, queries);

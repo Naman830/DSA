5235a245d7274a190155c194d2f70003a0c441ec
Naman <namansingla830@gmail.com>
2026-05-26 18:15:59 +0530

    bubble sort pseudo code added

---
+++ Sorting/bubble_sort.js
@@ -0,0 +1,15 @@
+// Bubble Sort works by repeatedly comparing two side-by-side elements and swapping them if they are in the wrong order.
+
+/*
+Pseudo Code
+
+bubbleSort(arr):
+    n = length of arr
+
+    for i from 0 to n - 2:
+        for j from 0 to n - 2:
+            if arr[j] > arr[j + 1]:
+                swap arr[j] and arr[j + 1]
+
+    return arr
+*/

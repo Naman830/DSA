55fed8e528977667c5954a02601d9010c19773b2
Naman <namansingla830@gmail.com>
2026-05-26 18:23:43 +0530

    insertion sort observation added

---
+++ Sorting/insertion_sort.js
@@ -0,0 +1,8 @@
+// Insertion Sort takes one element at a time and inserts it into its correct position.
+
+/*
+1. Pick the current element.
+2. Compare it with previous elements.
+3. Shift bigger elements to the right.
+4. Insert current element at the correct place.
+ */

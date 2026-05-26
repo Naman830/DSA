31a5c568d4502b410046387e09f6fa9387ea52c7
Naman <namansingla830@gmail.com>
2026-05-26 18:18:13 +0530

    js code added

--- Sorting/bubble_sort.js
+++ Sorting/bubble_sort.js
@@ -13,3 +13,29 @@ bubbleSort(arr):
 
     return arr
 */
+
+function bubbleSort(arr) {
+  let n = arr.length;
+
+  // Outer loop controls the passes
+  for (let i = 0; i < n - 1; i++) {
+    // After every pass, the largest element moves to the end
+    // So we reduce the inner loop by i
+    for (let j = 0; j < n - 1 - i; j++) {
+      // Compare adjacent elements
+      if (arr[j] > arr[j + 1]) {
+        // Swap when elements are in wrong order
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+let nums = [5, 1, 4, 2, 8];
+
+console.log(bubbleSort(nums));
+// Output: [1, 2, 4, 5, 8]

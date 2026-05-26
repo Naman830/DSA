c8e51867d51256b81b6ac12db794e78018501dad
Naman <namansingla830@gmail.com>
2026-05-26 18:24:29 +0530

    psudo code added

--- Sorting/insertion_sort.js
+++ Sorting/insertion_sort.js
@@ -6,3 +6,20 @@
 3. Shift bigger elements to the right.
 4. Insert current element at the correct place.
  */
+
+/*
+Pseudo Code
+
+insertionSort(arr):
+    for i from 1 to n - 1:
+        current = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > current:
+            arr[j + 1] = arr[j]
+            j--
+
+        arr[j + 1] = current
+
+    return arr
+*/

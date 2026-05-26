e8e7114fac26a1251edd08bb4224972fd0904188
Naman <namansingla830@gmail.com>
2026-05-26 17:58:38 +0530

    selecting sort observation write

---
+++ Sorting/selection_sort.js
@@ -0,0 +1,9 @@
+// Selection Sort finds the minimum element from the unsorted part of the array and swaps it with the first element of that unsorted part.
+
+/*
+1. Assume current index has minimum value.
+2. Search the remaining array.
+3. Find actual minimum index.
+4. Swap current element with minimum element.
+*/
+

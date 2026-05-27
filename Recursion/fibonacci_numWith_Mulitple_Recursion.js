8d74d1360685e3825efc37b907d39548edff5d2d
Naman <namansingla830@gmail.com>
2026-05-27 13:29:18 +0530

    js code finding fibonaci number

--- Recursion/fibonacci_numWith_Mulitple_Recursion.js
+++ Recursion/fibonacci_numWith_Mulitple_Recursion.js
@@ -24,3 +24,23 @@ fib(5)
     │   └── fib(0)
     └── fib(1)
 */
+
+function fibonacci(n) {
+  // Base case 1:
+  // Fibonacci of 0 is 0
+  if (n === 0) {
+    return 0;
+  }
+
+  // Base case 2:
+  // Fibonacci of 1 is 1
+  if (n === 1) {
+    return 1;
+  }
+
+  // Multiple recursion:
+  // Here function calls itself two times
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(5)); // Output: 5

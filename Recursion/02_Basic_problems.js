d3807abd1639c960b12570761b522fe0eeb13c2d
Naman <namansingla830@gmail.com>
2026-05-27 12:58:53 +0530

    1 to n and n to 1

--- Recursion/02_Basic_problems.js
+++ Recursion/02_Basic_problems.js
@@ -11,3 +11,21 @@ function nameNtimes(i, n) {
 }
 
 nameNtimes(1, 5);
+
+// Print Numbers from 1 to N
+// for N to 1 (just proint before smaller number subtracting)
+
+function print1toN(n) {
+  // Base case: when n becomes 0, stop recursion
+  if (n === 0) {
+    return;
+  }
+
+  // First go to smaller problem
+  print1toN(n - 1);
+
+  // Print while coming back
+  console.log(n);
+}
+
+print1toN(5);

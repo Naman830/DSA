3545df78fa820daa2a8ee991e2120b258b3f7c09
Naman <namansingla830@gmail.com>
2026-05-27 12:32:39 +0530

    how recursion works

--- Array/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-  if (arr[i] === arr[j]) {
-        count++;
-      }
-    }
-
-    // If element appears only once return it
---
+++ Recursion/01_recursion.js
@@ -0,0 +1,25 @@
+// Introduction to Recursion, Recursion Tree, and Stack Space
+
+// Recursion means:
+// A function calling itself again and again to solve a smaller version of the same problem.
+
+/* 
+Real-Life Example of Recursion
+
+Imagine you have 5 boxes inside each other.
+
+Box 5
+  Box 4
+    Box 3
+      Box 2
+        Box 1
+
+To open Box 5, you need to open Box 4.
+To open Box 4, you need to open Box 3.
+To open Box 3, you need to open Box 2.
+To open Box 2, you need to open Box 1.
+
+When Box 1 is opened, the process stops.
+
+This is exactly how recursion works.
+*/

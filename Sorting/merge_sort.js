1032d932d1711bc4f43d8b3bf91cd5889dcba521
Naman <namansingla830@gmail.com>
2026-05-28 12:35:51 +0530

    merge sort observation

---
+++ Sorting/merge_sort.js
@@ -0,0 +1,14 @@
+// Merge Sort is a sorting algorithm used to arrange elements in increasing or decreasing order.
+
+// Main idea: Divide the array into smaller parts, sort those parts, then merge them back.
+
+/*
+1. Divide
+Break the big problem into smaller problems.
+
+2. Conquer
+Solve the smaller problems.
+
+3. Combine
+Join the solved parts to get the final answer.
+*/

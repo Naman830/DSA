641d65fc8830faa84f128956d43c7c49ba97e278
Naman <namansingla830@gmail.com>
2026-05-29 12:33:36 +0530

    majority element obseravtion added

---
+++ Array/majority_element.js
@@ -0,0 +1,30 @@
+// You have to find the element that appears more than n / 2 times, where n is the size of the array.
+// frequency of element > n / 2
+
+/*
+Why Can There Be Only One Majority Element?
+
+A very important interview point.
+Can two elements both appear more than n / 2 times?
+
+No.
+
+Example:
+If array size is 10,
+
+Majority element must appear more than 5 times.
+
+Suppose:
+A appears 6 times
+B appears 6 times
+
+Total would become:
+
+6 + 6 = 12
+
+But array size is only 10.
+
+Impossible.
+
+So there can be only one majority element.
+*/

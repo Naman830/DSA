0d8d9fca03d448dd4905546e5ce4c9b4d15e2adc
Naman <namansingla830@gmail.com>
2026-05-29 11:44:36 +0530

    brute force Approach in Sort an array of 0s 1s and 2s

---
+++ Array/sort_arrayOf_0s_1s_2s.js
@@ -0,0 +1,7 @@
+// This is a very famous DSA problem, also known as:
+// Sort Colors || Dutch National Flag Algorithm
+
+// Brute Force Solution
+// Just sort the array using built-in sort. (Like: Merge Sort)
+// TC: O(n log n) and SC: O(1) [Because we are sorting in same array]
+

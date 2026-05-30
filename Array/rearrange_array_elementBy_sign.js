4337178ba4274ec3ed528d296e1eba7e7d2bc20d
Naman <namansingla830@gmail.com>
2026-05-30 11:39:28 +0530

    add observation for rearrange array element by sign

---
+++ Array/rearrange_array_elementBy_sign.js
@@ -0,0 +1,20 @@
+/*
+You are given an array containing positive and negative numbers.
+
+You need to rearrange the array so that:
+
+Positive and negative numbers come alternately.
+The first element should be positive.
+The relative order of positive numbers should remain the same.
+The relative order of negative numbers should remain the same.
+
+Since the answer starts with positive:
+
+positive, negative, positive, negative, positive, negative
+
+Positive numbers will always be placed at even indices:
+0, 2, 4, 6...
+
+Negative numbers will always be placed at odd indices:
+1, 3, 5, 7...
+*/

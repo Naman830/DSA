82cbc9f78b6e2cb89d1868cfb2779993d34b8b44
Naman <namansingla830@gmail.com>
2026-05-30 11:15:32 +0530

    add observation for best time to buy stock in dynamic proggraming

---
+++ Dynamic_Programming/best_timeTO_buyOr_sell_stock.js
@@ -0,0 +1,17 @@
+/*
+In stock problems, we usually have an array:
+
+prices = [7, 1, 5, 3, 6, 4]
+
+Each value means stock price on that day.
+
+Your goal is usually:
+Buy at a low price and sell at a high price to get maximum profit.
+
+But the rules change from problem to problem.
+*/
+
+// CONDITION_1
+// You can buy only once and sell only once.
+// Find the maximum profit.
+

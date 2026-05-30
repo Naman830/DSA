475b3208390e8fb8b7b2d9053af64fe53e18cf9f
Naman <namansingla830@gmail.com>
2026-05-30 10:53:27 +0530

    add subArray.txt file content

--- Array/Sub_Array/maximum_subArray_sum.js
+++ Array/Sub_Array/maximum_subArray_sum.js
@@ -1,9 +1,3 @@
-// You need to find the maximum sum of a continuous subarray.
-// Continuous means elements should be together, without skipping.
-// arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-
-// SUBARRAY-->[-2,1,-3] & SUBSEQUENCE-->[-2,-5,2]
-
 // 1. Brute Force Approach
 // Generate every possible subarray and calculate its sum.
 

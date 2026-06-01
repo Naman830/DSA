2fa3573167443dbed27ccb56f6a87e7e0bed14b0
Naman <namansingla830@gmail.com>
2026-06-01 13:02:27 +0530

    js code added

--- Array/leader_in_array.js
+++ Array/leader_in_array.js
@@ -67,15 +67,13 @@ function leadersInArray(arr) {
   return leaders;
 }
 
-console.log(leadersInArray([16, 17, 4, 3, 5, 2]));
-// Output: [17, 5, 2]
-
 
 //2. Optimal Approach
 // Instead of checking right side again and again, we move from right to left.
 
 /*
 Pseudo Code
+
 leaders = []
 maxRight = -infinity
 
@@ -88,3 +86,32 @@ reverse leaders
 
 return leaders
 */
+
+function leadersInArray(arr) {
+  let n = arr.length;
+  let leaders = [];
+
+  // This will store the maximum element seen from the right side
+  let maxRight = -Infinity;
+
+  // Traverse the array from right to left
+  for (let i = n - 1; i >= 0; i--) {
+    // If current element is greater than or equal to all elements on right,
+    // then it is a leader
+    if (arr[i] >= maxRight) {
+      leaders.push(arr[i]);
+
+      // Update maxRight because current element is now the biggest seen so far
+      maxRight = arr[i];
+    }
+  }
+
+  // We collected leaders from right to left,
+  // so reverse them to get correct left-to-right order
+  leaders.reverse();
+
+  return leaders;
+}
+
+console.log(leadersInArrayOptimal([16, 17, 4, 3, 5, 2]));
+// Output: [17, 5, 2]

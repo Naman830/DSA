6eca90595b61b17c084b70ab3007b8459dfd521e
Naman <namansingla830@gmail.com>
2026-06-03 12:59:15 +0530

    add observation of set matrix zeros

---
+++ Array/matrix/set_matrix_zeros.js
@@ -0,0 +1,17 @@
+/*
+Problem: Given an m x n matrix, if an element is 0, set its entire row and column to 0. The operation must be done in-place.
+
+Input:
+1 1 1
+1 0 1
+1 1 1
+
+Output:
+1 0 1
+0 0 0
+1 0 1
+
+Keep in Mind
+1. First identify rows/columns that should become zero.
+2. Then update the matrix.
+*/

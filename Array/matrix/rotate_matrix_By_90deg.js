b3acc32f724c502a19b2bde4567b5be2dd692ecb
Naman <namansingla830@gmail.com>
2026-06-04 08:31:50 +0530

    add brute force solution

--- Array/matrix/rotate_matrix_By_90deg.js
+++ Array/matrix/rotate_matrix_By_90deg.js
@@ -31,3 +31,21 @@ for i = 0 to n-1
     for j = 0 to n-1
         result[j][n-1-i] = matrix[i][j]
 */
+
+// Time: O(N²)  Space: O(N²)
+
+function rotateBrute(matrix) {
+  const n = matrix.length;
+
+  const result = Array.from({ length: n }, () => Array(n).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      result[j][n - 1 - i] = matrix[i][j];
+    }
+  }
+
+  return result;
+}
+
+console.log(rotateBrute([[1, 2, 3],[4, 5, 6],[7, 8, 9]]));

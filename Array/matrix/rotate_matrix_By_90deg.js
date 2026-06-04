9ba555119a1e24a66c57907191efede5fe06abba
Naman <namansingla830@gmail.com>
2026-06-04 08:27:40 +0530

    add observation of roate matrix by 90 degree

---
+++ Array/matrix/rotate_matrix_By_90deg.js
@@ -0,0 +1,21 @@
+/*
+Given an n × n matrix, rotate it 90 degrees clockwise in-place.
+
+Input:
+1 2 3
+4 5 6
+7 8 9
+
+Output:
+7 4 1
+8 5 2
+9 6 3
+
+1 → moves to top-right
+2 → moves to middle-right
+3 → moves to bottom-right
+
+Most interviewers expect:
+In-place rotation
+No extra matrix
+*/

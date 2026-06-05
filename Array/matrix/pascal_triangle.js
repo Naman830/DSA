c42f33c002ed6fc712c7d43394687c901e761905
Naman <namansingla830@gmail.com>
2026-06-05 10:03:25 +0530

    add observation for print entire pascal triangle

--- Array/matrix/pascal_triangle.js
+++ Array/matrix/pascal_triangle.js
@@ -179,3 +179,40 @@ function printNthRow(n) {
 }
 
 console.log(printNthRow(5));
+
+// =================================================================================================
+// =================================================================================================
+// =================================================================================================
+
+// 3. Print Entire Pascal Triangle
+/*
+Given:
+n = 5
+
+Output:
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+
+Brute Force
+For every row:
+For every column:
+
+nCr(row,col)
+
+Complexity: O(n³)
+
+because nCr itself takes O(n).
+
+Better Solution
+Use previous row.
+
+Formula:
+triangle[i][j]
+=
+triangle[i-1][j-1]
++
+triangle[i-1][j]
+*/

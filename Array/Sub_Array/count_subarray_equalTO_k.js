505f3064c37cda6a2a9d20736e76edbe1c0a3cf0
Naman <namansingla830@gmail.com>
2026-06-05 08:48:06 +0530

    add observation of count subarray equal to k

---
+++ Array/Sub_Array/count_subarray_equalTO_k.js
@@ -0,0 +1,21 @@
+/*
+Given an array arr and an integer k, count how many subarrays have sum exactly equal to k.
+
+The question is not asking for the maximum sum.
+
+It is asking:
+How many continuous ranges have sum exactly equal to k?
+
+Notice the word COUNT.
+
+Whenever you see:
+Count
+Number of ways
+How many
+Frequency
+
+Think:
+HashMap
+Prefix Sum
+Frequency Map
+*/

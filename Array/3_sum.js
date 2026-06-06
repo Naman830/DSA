c13b49a23e46f972949efda78b3448250922b067
Naman <namansingla830@gmail.com>
2026-06-06 09:35:58 +0530

    add js code with tc and sc

--- Array/3_sum.js
+++ Array/3_sum.js
@@ -83,8 +83,6 @@ function threeSumBrute(nums) {
 
 console.log(threeSumBrute([-1, 0, 1, 2, -1, -4]));
 
-
-
 // 2. Better Solution
 /*
 Idea
@@ -118,4 +116,89 @@ Step 3: Use Two Pointers
 left = i + 1
 right = n - 1
 
+Found triplet.
+Store it.
+Move both pointers.
 */
+
+// TC: O(n) & SC: O(1)
+
+function threeSum(nums) {
+  // Final answer array
+  const result = [];
+
+  // Sort array in ascending order
+  nums.sort((a, b) => a - b);
+
+  // Fix first element of triplet
+  for (let i = 0; i < nums.length - 2; i++) {
+    // -----------------------------------------
+    // Skip duplicate first elements
+    // Example:
+    // [-1, -1, 0, 1]
+    //
+    // If current -1 is same as previous -1
+    // we already processed it
+    // -----------------------------------------
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    // Two pointers
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    // Search pair for current fixed element
+    while (left < right) {
+      // Current triplet sum
+      const sum = nums[i] + nums[left] + nums[right];
+
+      // ----------------------------------
+      // Sum too small
+      // Need larger number
+      // ----------------------------------
+      if (sum < 0) {
+        left++;
+      }
+
+      // ----------------------------------
+      // Sum too large
+      // Need smaller number
+      // ----------------------------------
+      else if (sum > 0) {
+        right--;
+      }
+
+      // ----------------------------------
+      // Found valid triplet
+      // ----------------------------------
+      else {
+        result.push([nums[i], nums[left], nums[right]]);
+
+        // Move both pointers
+        left++;
+        right--;
+
+        // ----------------------------------
+        // Skip duplicate values on left
+        // Example:
+        // [-1,0,0,0,1]
+        // ----------------------------------
+        while (left < right && nums[left] === nums[left - 1]) {
+          left++;
+        }
+
+        // ----------------------------------
+        // Skip duplicate values on right
+        // ----------------------------------
+        while (left < right && nums[right] === nums[right + 1]) {
+          right--;
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));

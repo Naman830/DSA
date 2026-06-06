2c78f31159c7f4615375a139730ee026e14f8f84
Naman <namansingla830@gmail.com>
2026-06-06 10:14:05 +0530

    add observation for 4 sum

---
+++ Array/4_sum.js
@@ -0,0 +1,19 @@
+/*
+4Sum is an extension of 2Sum and 3Sum.
+
+Problem
+Given an array nums and a target value, return all unique quadruplets:
+
+nums = [1,0,-1,0,-2,2]
+target = 0
+
+Output:
+[
+  [-2,-1,1,2],
+  [-2,0,0,2],
+  [-1,0,0,1]
+]
+
+Fix 2 numbers
+Use Two Pointers for remaining 2 numbers
+*/

fe0f8db76749d2f0be47cc707da169cd25c9a6af
Naman <namansingla830@gmail.com>
2026-06-06 10:14:39 +0530

    add brute pseudo code

--- Array/4_sum.js
+++ Array/4_sum.js
@@ -13,7 +13,20 @@ Output:
   [-2,0,0,2],
   [-1,0,0,1]
 ]
+*/
 
-Fix 2 numbers
-Use Two Pointers for remaining 2 numbers
+// 1. Brute Force Solution
+/*
+Time  : O(n⁴)
+Space : O(1)
+
+Pseudocode
+
+for i
+    for j
+        for k
+            for l
+
+                if sum == target
+                    store answer
 */

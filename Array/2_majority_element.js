3053a2e227cdaf327b9337ffd99f302b17e49a01
Naman <namansingla830@gmail.com>
2026-06-06 08:35:53 +0530

    add pseudo code with tc and sc

--- Array/2_majority_element.js
+++ Array/2_majority_element.js
@@ -127,3 +127,54 @@ Verify candidates
 
 return answer
 */
+
+// TC: O(n) & SC: O(1)
+
+function majorityElementOptimal(nums) {
+  let candidate1 = null;
+  let candidate2 = null;
+
+  let count1 = 0;
+  let count2 = 0;
+
+  // Voting Phase
+  for (let num of nums) {
+    if (num === candidate1) {
+      count1++;
+    } else if (num === candidate2) {
+      count2++;
+    } else if (count1 === 0) {
+      candidate1 = num;
+      count1 = 1;
+    } else if (count2 === 0) {
+      candidate2 = num;
+      count2 = 1;
+    } else {
+      count1--;
+      count2--;
+    }
+  }
+
+  // Verification Phase
+  count1 = 0;
+  count2 = 0;
+
+  for (let num of nums) {
+    if (num === candidate1) count1++;
+    else if (num === candidate2) count2++;
+  }
+
+  const result = [];
+
+  if (count1 > Math.floor(nums.length / 3)) {
+    result.push(candidate1);
+  }
+
+  if (count2 > Math.floor(nums.length / 3)) {
+    result.push(candidate2);
+  }
+
+  return result;
+}
+
+console.log(majorityElementOptimal([1, 1, 1, 3, 3, 2, 2, 2]));

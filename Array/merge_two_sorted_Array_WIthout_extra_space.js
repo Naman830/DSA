6c16ac0c262c802c9d14ef3abb62453bb5ef4d1e
Naman <namansingla830@gmail.com>
2026-06-07 09:29:32 +0530

    psudo code add for beter solution

--- Array/merge_two_sorted_Array_WIthout_extra_space.js
+++ Array/merge_two_sorted_Array_WIthout_extra_space.js
@@ -9,3 +9,38 @@ All larger elements should be in arr2
 
 Both arrays must remain sorted.
 */
+
+// 1. Better Solution (Two Pointers + Sort Again)
+
+/*
+Compare:
+last element of arr1
+first element of arr2
+
+If:
+arr1[left] > arr2[right]
+
+swap them.
+
+After all swaps:
+sort arr1
+sort arr2
+*/
+
+/*
+Pseudocode
+left = n-1
+right = 0
+
+while left >= 0 and right < m
+
+    if arr1[left] > arr2[right]
+        swap
+        left--
+        right++
+    else
+        break
+
+sort arr1
+sort arr2
+*/

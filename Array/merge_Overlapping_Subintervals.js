06342589d36ed58ecf363ffac379cea2b180d212
Naman <namansingla830@gmail.com>
2026-06-07 08:41:17 +0530

    add observation for merge overlapping subinterval

---
+++ Array/merge_Overlapping_Subintervals.js
@@ -0,0 +1,61 @@
+/*
+You are given a list of intervals.
+
+Each interval has:
+[start, end]
+
+Example:
+[
+  [1,3],
+  [2,6],
+  [8,10],
+  [15,18]
+]
+
+An interval represents a range.
+[1,3] => 1 to 3
+[2,6] => 2 to 6
+
+Since these ranges overlap, they should be merged.
+Result:
+[
+  [1,6],
+  [8,10],
+  [15,18]
+]
+
+Input
+Array of intervals:
+[[start1,end1],[start2,end2],...]
+
+Output
+Merged intervals.
+Hidden Observation
+Two intervals overlap if:
+
+currentStart <= previousEnd
+
+Example:
+[1,3]
+[2,6]
+
+2 <= 3
+
+=> Overlap exists
+
+Merge:
+start = min(1,2) = 1
+end   = max(3,6) = 6
+
+=> [1,6]
+2. Intuition
+
+Imagine drawing intervals on a line:
+[1-----3]
+     [2--------6]
+Both touch each other.
+
+Instead of keeping both:
+[1-------------6]
+
+*/

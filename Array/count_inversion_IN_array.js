1abd9fc8084a0649a8a93bdc6d79d270685e2756
Naman <namansingla830@gmail.com>
2026-06-08 08:13:17 +0530

    add observation of count inversion

---
+++ Array/count_inversion_IN_array.js
@@ -0,0 +1,10 @@
+/*
+An inversion is a pair of indices (i, j) such that:
+
+i < j
+arr[i] > arr[j]
+
+In simple words:
+
+If a bigger number appears before a smaller number, it forms an inversion.
+*/

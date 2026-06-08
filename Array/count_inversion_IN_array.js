4e7898806fca8202ac48d2b91bc7946df4ef2ec1
Naman <namansingla830@gmail.com>
2026-06-08 08:14:13 +0530

    add pseduo code for brute force

--- Array/count_inversion_IN_array.js
+++ Array/count_inversion_IN_array.js
@@ -5,6 +5,29 @@ i < j
 arr[i] > arr[j]
 
 In simple words:
-
 If a bigger number appears before a smaller number, it forms an inversion.
 */
+
+// 1. Brute Force Solution
+// Generate every possible pair.
+
+/*
+Loop through every element.
+Compare with all elements after it.
+If greater, increment count.
+
+Pseudocode
+
+count = 0
+
+for i = 0 to n-1
+    for j = i+1 to n-1
+
+        if arr[i] > arr[j]
+            count++
+
+return count
+
+
+*/
+

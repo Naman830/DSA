e410e8d683b3681bf2709059aae7da9cc8c2fcce
Naman <namansingla830@gmail.com>
2026-06-08 08:44:43 +0530

    add observation of find missing and repeating number

---
+++ Array/find_missing&repeating_number.js
@@ -0,0 +1,12 @@
+/*
+You are given an array of size n containing numbers from 1 to n.
+
+One number appears twice (Repeating Number).
+One number is missing.
+
+Input: [4, 3, 6, 2, 1, 1]
+Numbers should be: 1, 2, 3, 4, 5, 6
+
+Repeating = 1
+Missing = 5
+*/

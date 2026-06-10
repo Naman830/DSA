e78bef371b2c2e5a75b10f13a0688f60cac3b8a6
Naman <namansingla830@gmail.com>
2026-06-10 09:01:49 +0530

    small fixes

--- Array/remove_duplicate_from_sorted_array.js
+++ Array/remove_duplicate_from_sorted_array.js
@@ -6,13 +6,13 @@
 // Using a Set (Use another data structure like Set to store unique values.)
 // TC: O(n) and SC: O(n)
 function removeDuplicates(nums) {
-    // Create a Set to store unique values
-    let unique = [...new Set(nums)];
-    // Traverse the unique array and modify the original array in-place
-    for (let i = 0; i < unique.length; i++) {
-        nums[i] = unique[i];
-    }
-    return unique.length;
+  // Create a Set to store unique values
+  let unique = [...new Set(nums)];
+  // Traverse the unique array and modify the original array in-place
+  for (let i = 0; i < unique.length; i++) {
+    nums[i] = unique[i];
+  }
+  return unique.length;
 }
 
 // Approach 2 - Optimal Solution (Two Pointer)
@@ -21,14 +21,14 @@ function removeDuplicates(nums) {
 // TC: O(n) and SC: O(1)
 
 function removeDuplicates1(nums) {
-    let i = 0; // Pointer for unique poisition
-    for (let j = 0; j < nums.length; j++) {
-        if (nums[j] !== nums[i]) {
-            i++; //Move the unique position pointer
-            nums[i] = nums[j]; // Update the unique position with the new unique value
-        }
+  let i = 0; // Pointer for unique poisition
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++; //Move the unique position pointer
+      nums[i] = nums[j]; // Update the unique position with the new unique value
     }
-    return i + 1; // Return the length of the unique array
+  }
+  return i + 1; // Return the length of the unique array
 }
 
 let nums = [1, 1, 2, 2, 3];

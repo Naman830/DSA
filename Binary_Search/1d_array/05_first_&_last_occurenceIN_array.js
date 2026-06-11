c3ac476b9e0b0e0970583c888948e6e4c7038e63
Naman <namansingla830@gmail.com>
2026-06-11 16:42:16 +0530

    add observation for first and last occurence in an array

---
+++ Binary_Search/1d_array/05_first_&_last_occurenceIN_array.js
@@ -0,0 +1,12 @@
+/*
+Given a sorted array and a target element x, find:
+The first occurrence of x
+The last occurrence of x
+
+arr = [1, 2, 2, 2, 3, 4]
+x = 2
+
+Output:
+First Occurrence = 1
+Last Occurrence = 3
+*/

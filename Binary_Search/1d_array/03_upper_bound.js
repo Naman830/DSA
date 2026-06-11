149bc69ead4684d290dc6ee842eecb40714d6140
Naman <namansingla830@gmail.com>
2026-06-11 09:48:28 +0530

    add intution

---
+++ Binary_Search/1d_array/03_upper_bound.js
@@ -0,0 +1,12 @@
+// The Upper Bound of x is:
+// The first index where the value is strictly greater than x ( > x ).
+
+/*
+Intuition
+
+Whenever we find an element:
+arr[mid] > x
+
+it can be the answer.
+Store it and continue searching on the left side.
+*/

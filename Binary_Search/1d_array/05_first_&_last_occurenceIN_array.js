cb61436a729b49e6b82f2e05e3f2207aed6b9760
Naman <namansingla830@gmail.com>
2026-06-11 16:43:42 +0530

    add js code with tc and sc

--- Binary_Search/1d_array/05_first_&_last_occurenceIN_array.js
+++ Binary_Search/1d_array/05_first_&_last_occurenceIN_array.js
@@ -10,3 +10,23 @@ Output:
 First Occurrence = 1
 Last Occurrence = 3
 */
+
+// TC: O(n) & SC: O(1)
+
+// 1. Brute Force Solution
+// Traverse the entire array.
+function firstLastOccurrenceBrute(arr, x) {
+  let first = -1;
+  let last = -1;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === x) {
+      if (first === -1) first = i;
+      last = i;
+    }
+  }
+
+  return [first, last];
+}
+
+console.log(firstLastOccurrenceBrute([1, 2, 2, 2, 3, 4], 2));

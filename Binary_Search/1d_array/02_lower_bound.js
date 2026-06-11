94746100d3bb95d4f67cc336aca4f3fbc32a5231
Naman <namansingla830@gmail.com>
2026-06-11 09:47:27 +0530

    add js code

--- Binary_Search/1d_array/02_lower_bound.js
+++ Binary_Search/1d_array/02_lower_bound.js
@@ -8,3 +8,26 @@ It can be a possible answer.
 But there may be another valid answer on the left side.
 So store it and move left.
 */
+
+function lowerBound(arr, x) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  // If lower bound doesn't exist,
+  // answer will become arr.length
+  let ans = arr.length;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+
+    // Current element can be a lower bound
+    if (arr[mid] >= x) {
+      ans = mid;
+      high = mid - 1; // search for earlier occurrence
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return ans;
+}

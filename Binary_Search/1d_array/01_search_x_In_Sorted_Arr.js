8a82a47385dbb7be55bbccccd51a93dd964505b3
Naman <namansingla830@gmail.com>
2026-06-11 08:53:55 +0530

    how binary search works

---
+++ Binary_Search/1d_array/01_search_x_In_Sorted_Arr.js
@@ -0,0 +1,22 @@
+// Binary Search is a searching algorithm used on a sorted array.
+/*
+Imagine you are searching for the word "Monkey" in a dictionary.
+
+Linear Search Approac
+Start from page 1 and keep turning pages until you find it.
+
+Page 1 → No
+Page 2 → No
+...
+Very slow
+Binary Search Approach
+Open the dictionary from the middle.
+Suppose middle word is "Lion".
+Since "Monkey" comes after "Lion", ignore the left half.
+Open the middle of the remaining right half.
+Repeat until found.
+
+Every step removes half of the remaining pages.
+
+This is exactly how Binary Search works.
+*/

28e3fc4de58da7ac24c848f86428a2f751e4a7cf
Naman <namansingla830@gmail.com>
2026-06-13 09:34:09 +0530

    add js code

--- Binary_Search/1d_array/06_search_element_IN_roated_Array1.js
+++ Binary_Search/1d_array/06_search_element_IN_roated_Array1.js
@@ -16,3 +16,40 @@ means right half is sorted
 
 One of them must be sorted.
 */
+
+// For interview first you will tell that we do it from linear searcha and then tell we can do it from binary search
+
+// Solution
+
+function search(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low <= high) {
+    const mid = low + Math.floor((high - low) / 2);
+
+    // Target found
+    if (target === arr[mid]) {
+      return mid;
+    } else if (arr[low] <= arr[mid]) {
+      //left half is sorted
+      if (target >= arr[low] && target < arr[mid]) {
+        //check if target is greater then low and lesses then mid
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    } else {
+      //right half is sorted
+      //check if target is greater then mid and lesser then high
+      if (arr[mid] < target && target <= arr[high]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  }
+  return -1;
+}
+
+console.log(search([4, 5, 6, 7, 0, 1, 2], 0));

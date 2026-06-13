9b96c9cafcd093e4c08526b62d5675ac343ef781
Naman <namansingla830@gmail.com>
2026-06-13 09:50:21 +0530

    add js code

--- Binary_Search/1d_array/07_search_element_In_duplicate_array_2.js
+++ Binary_Search/1d_array/07_search_element_In_duplicate_array_2.js
@@ -30,3 +30,46 @@ high--;
 
 This removes duplicates from both ends.
 */
+
+// CODE IS FULLY SIMILAR TO PREVIOUS CODE
+function search(nums, target) {
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low <= high) {
+    const mid = low + Math.floor((high - low) / 2);
+
+    // Target found
+    if (nums[mid] === target) {
+      return true;
+    }
+
+    // Cannot determine sorted half due to duplicates [THIS IS ADDED ONLY]
+    if (nums[low] === nums[mid] && nums[mid] === nums[high]) {
+      low++;
+      high--;
+      continue;
+    }
+
+    // Left half sorted
+    if (nums[low] <= nums[mid]) {
+      if (target >= nums[low] && target < nums[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    // Right half sorted
+    else {
+      if (target > nums[mid] && target <= nums[high]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  }
+
+  return false;
+}
+console.log(search([1, 1, 0, 1, 1, 1, 1], 0));

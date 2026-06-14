e4b9a6ac623665cb030b6c9555f08f584259fbb4
Naman <namansingla830@gmail.com>
2026-06-14 09:42:39 +0530

    remove unessary observatoiopn

--- Binary_Search/1d_array/10_single_element_In_sorted_array.js
+++ Binary_Search/1d_array/10_single_element_In_sorted_array.js
@@ -34,48 +34,6 @@ Pairs start at even indexes
 (6,7)
 
 This is the key observation.
-
-==========================================================
-==========================================================
-2. Intuition
-
-Suppose:
-[1,1,2,2,3,4,4,5,5]
-Single element = 3
-
-Indexes:
-0 1 2 3 4 5 6 7 8
-1 1 2 2 3 4 4 5 5
-
-Before the single element:
-1 1
-2 2
-
-Pairs start at even indexes.
-even -> first occurrence
-odd  -> second occurrence
-
-After the single element:
-4 4
-5 5
-
-Pattern changes!
-odd  -> first occurrence
-even -> second occurrence
-
-Because one element has shifted everything by one position.
-Pattern to Remember
-Before single element
-index: 0 1 2 3
-value: 1 1 2 2
-
-nums[even] == nums[even+1]
-After single element
-index: 5 6 7 8
-value: 4 4 5 5
-
-nums[odd] == nums[odd+1]
-This change lets us apply Binary Search
 */
 
 // 1. Brute Force Solution [Use HashMap.]
@@ -112,3 +70,5 @@ function singleNonDuplicate(nums) {
 
   return xor;
 }
+
+// Optimal Solution (Binary Search)

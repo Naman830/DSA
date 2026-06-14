a8f4fa6d5930fcc9b6494fd7c313de168ae04daa
Naman <namansingla830@gmail.com>
2026-06-14 09:08:57 +0530

    add js code with tc and sc

--- Binary_Search/1d_array/09_how_Many_time_array_Roated.js
+++ Binary_Search/1d_array/09_how_Many_time_array_Roated.js
@@ -14,3 +14,31 @@ Answer: 2
 
 because the array was rotated 2 times.
 */
+
+/*
+Time  : O(log n)
+Space : O(1)
+*/
+
+function countRotations(arr) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low < high) {
+    const mid = Math.floor((low + high) / 2);
+
+    // Minimum lies on right side
+    if (arr[mid] > arr[high]) {
+      low = mid + 1;
+    }
+    // Minimum lies on left side (including mid)
+    else {
+      high = mid;
+    }
+  }
+
+  // low points to minimum element
+  return low;
+}
+
+console.log(countRotations([15, 18, 2, 3, 6, 12]));

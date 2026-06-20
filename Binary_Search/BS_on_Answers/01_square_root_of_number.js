319ef6c44fc6a618d50eeeeaec564b284e9ad419
Naman <namansingla830@gmail.com>
2026-06-20 17:12:52 +0530

    add observation for square root on number in bs

---
+++ Binary_Search/BS_on_Answers/01_square_root_of_number.js
@@ -0,0 +1,28 @@
+/*
+1. Most beginners think: Binary Search works only on arrays.
+Wrong.
+2. Binary Search works whenever: Search Space is sorted.
+
+For square root:
+n = 28
+
+Possible answers:
+0 1 2 3 4 5 6 ...
+
+Observe:
+mid	mid²
+1	1
+2	4
+3	9
+4	16
+5	25
+6	36
+
+The values of mid² increase continuously.
+1 < 4 < 9 < 16 < 25 < 36
+
+This is a sorted search space.
+
+Hence: Binary Search on Answer
+
+*/

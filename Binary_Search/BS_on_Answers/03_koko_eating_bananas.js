856e2eb268eadfbd23acdb354b26ece14e0a3874
Naman <namansingla830@gmail.com>
2026-06-20 17:53:10 +0530

    add pseudo code for brute force

--- Binary_Search/BS_on_Answers/03_koko_eating_bananas.js
+++ Binary_Search/BS_on_Answers/03_koko_eating_bananas.js
@@ -7,14 +7,25 @@ h = 8
 Koko can choose an eating speed k bananas/hour.
 
 Rules:
-
 Every hour she picks one pile.
 She eats at most k bananas from that pile.
 If the pile has fewer than k, she eats the whole pile.
 After eating, if bananas remain in that pile, she'll continue it in future hours.
 
 We need to find:
-
 The minimum eating speed k so that Koko finishes all bananas within h hours.
 
+In simple we have to find minimum time in that koko can it whole banans and for that search we find our maxxium banana limit so that we can find our answer between the number
+*/
+
+/*
+Brute force approach is simple try every possible number and when number find return the value
+
+Pseudo code
+for speed from 1 to maxPile
+
+    calculate hours
+
+    if hours <= h
+        return speed
 */

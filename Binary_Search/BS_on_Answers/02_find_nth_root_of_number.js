3b8bf68cc3df579f9b77b53c3cc496899fb1fe4e
Naman <namansingla830@gmail.com>
2026-06-20 17:19:34 +0530

    add observation of nth root of a number

---
+++ Binary_Search/BS_on_Answers/02_find_nth_root_of_number.js
@@ -0,0 +1,23 @@
+/*
+Suppose:
+m = 64
+n = 3
+
+Possible cube roots:
+1³ = 1
+2³ = 8
+3³ = 27
+4³ = 64  ← Answer
+5³ = 125
+
+Notice:
+As x increases,
+xⁿ also increases.
+
+This is a monotonic function.
+
+Whenever:
+Increasing x → Increasing answer
+
+Use Binary Search 
+*/

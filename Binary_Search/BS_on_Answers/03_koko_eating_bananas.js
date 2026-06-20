b1e8f32d6326c14fd229aecee3e3f1507a53732e
Naman <namansingla830@gmail.com>
2026-06-20 17:49:45 +0530

    add observation for koko eating banans

---
+++ Binary_Search/BS_on_Answers/03_koko_eating_bananas.js
@@ -0,0 +1,20 @@
+/*
+Koko has some piles of bananas.
+
+piles = [3,6,7,11]
+h = 8
+
+Koko can choose an eating speed k bananas/hour.
+
+Rules:
+
+Every hour she picks one pile.
+She eats at most k bananas from that pile.
+If the pile has fewer than k, she eats the whole pile.
+After eating, if bananas remain in that pile, she'll continue it in future hours.
+
+We need to find:
+
+The minimum eating speed k so that Koko finishes all bananas within h hours.
+
+*/

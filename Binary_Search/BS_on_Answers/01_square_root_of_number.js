2d38c4deb366c44e022a421cd695d00e88aba50a
Naman <namansingla830@gmail.com>
2026-06-20 17:13:07 +0530

    add pseudo code

--- Binary_Search/BS_on_Answers/01_square_root_of_number.js
+++ Binary_Search/BS_on_Answers/01_square_root_of_number.js
@@ -26,3 +26,25 @@ This is a sorted search space.
 Hence: Binary Search on Answer
 
 */
+
+/*
+Pseudocode
+
+
+low = 1
+high = n
+ans = 0
+
+while(low <= high)
+
+    mid = (low + high)/2
+
+    if(mid*mid <= n)
+        ans = mid
+        low = mid + 1
+
+    else
+        high = mid - 1
+
+return ans
+*/

926350d6622ff6afa7a414764c1123418a5a0738
Naman <namansingla830@gmail.com>
2026-06-29 07:01:27 +0530

    remove pseudo code no need of it bcz i add proper ccomment

--- Binary_Search/BS_on_Answers/06_find_least_capacity_TOship_within_Ddays.js
+++ Binary_Search/BS_on_Answers/06_find_least_capacity_TOship_within_Ddays.js
@@ -2,17 +2,6 @@
 
 // 1. Brute force
 
-/*
-Pseudo
-
-Find the maximum weight in the array (minimum capacity possible).
-Calculate the sum of all weights (maximum capacity possible).
-
-For each capacity from max weight to sum:
-Simulate shipping: load packages one by one until capacity is reached, then move to next day.
-If total days used is ≤ d, return that capacity.
-*/
-
 // Function to check how many days needed for given capacity
 function daysNeeded(weights, capacity) {
   // Initialize day count to 1

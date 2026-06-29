149147855098e1717f376e06904b39e0c07d5b12
Naman <namansingla830@gmail.com>
2026-06-29 08:14:23 +0530

    add observation of aggresive cows in bs min and mac

---
+++ Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/01_aggressive_cows.js
@@ -0,0 +1,22 @@
+/*
+You are given:
+An array of stall positions.
+k cows.
+
+You must place all cows inside the stalls.
+
+Rule:
+The minimum distance between any two cows should be as large as possible.
+Easy: Make the smallest distance between every pair of adjacent cows as large as possible.
+
+Return that maximum possible minimum distance.
+*/
+
+/*
+Think of it like:
+You have friends.
+Everyone wants personal space.
+Arrange them so that the person with the LEAST space gets as much space as possible.
+
+We maximize the minimum distance.
+*/

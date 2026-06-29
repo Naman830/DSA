7da968f2872d606d485309f77fd4d6422373daa4
Naman <namansingla830@gmail.com>
2026-06-29 07:42:09 +0530

    add js code with comment

--- Binary_Search/BS_on_Answers/07_kth_missing_number.js
+++ Binary_Search/BS_on_Answers/07_kth_missing_number.js
@@ -6,3 +6,20 @@ Input Format: vec[]={4,7,9,10}, k = 1
 Result: 1
 Explanation: The missing numbers are 1, 2, 3, 5, 6, 8, 11, 12, ……, and so on. Since 'k' is 1, the first missing element is 1.
 */
+
+// 1. Brute force
+
+function MissingKFinder(vec, k) {
+  for (let i = 0; i < vec.length; i++) {
+    if (vec[i] <= k) {
+      k++; // Adjust k if current number is not missing
+    } else {
+      break; // Stop if current number is greater than k
+    }
+  }
+  return k; // k is now the k-th missing number
+}
+
+const vec = [4, 7, 9, 10];
+const k = 4;
+console.log(MissingKFinder(vec, k));

1077e1632473ebb72be01680f454b9c1175f03c8
Naman <namansingla830@gmail.com>
2026-06-29 06:53:16 +0530

    add brute force pseudo code

--- Binary_Search/BS_on_Answers/06_find_least_capacity_TOship_within_Ddays.js
+++ Binary_Search/BS_on_Answers/06_find_least_capacity_TOship_within_Ddays.js
@@ -1,1 +1,13 @@
 // Simple meaning you have to find the least capcity of ship that it can carry weight of the packages within d days
+
+// 1. Brute force
+
+/*
+Pseudo
+
+Find the maximum weight in the array (minimum capacity possible).
+Calculate the sum of all weights (maximum capacity possible).
+For each capacity from max weight to sum:
+Simulate shipping: load packages one by one until capacity is reached, then move to next day.
+If total days used is ≤ d, return that capacity.
+*/

2bbf30e47e62723e1c51f95428550cbcb8686bff
Naman <namansingla830@gmail.com>
2026-06-30 07:09:15 +0530

    add observation of book allocation

---
+++ Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/02_book_allocation.js
@@ -0,0 +1,12 @@
+/*
+Imagine you have some books arranged in a row.
+Each book contains some number of pages.
+
+Example:
+Books = [25, 46, 28, 49, 24]
+
+Suppose there are 2 students.
+Your job is to distribute all books among these students.
+
+
+*/

4ffeb25398f40c2bf6e0f2cb5d910c7763886462
Naman <namansingla830@gmail.com>
2026-06-30 07:09:27 +0530

    add rules for book allocation

--- Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/02_book_allocation.js
+++ Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/02_book_allocation.js
@@ -8,5 +8,14 @@ Books = [25, 46, 28, 49, 24]
 Suppose there are 2 students.
 Your job is to distribute all books among these students.
 
+But there are some rules.
+Rule 1: Every book must be assigned
+Rule 2: One book cannot be divided
+Rule 3: Every student must get at least one book
 
+Rule 4: Books must remain contiguous:
+
+[10,20,30,40]
+Allowed: Student1 → 10 20
+Not Allowed: Student1 → 10 30
 */

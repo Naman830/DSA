54801735f00ae3e4727ceec4e5d6227d6ecd5b55
Naman <namansingla830@gmail.com>
2026-06-30 07:10:33 +0530

    add what we want

--- Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/02_book_allocation.js
+++ Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/02_book_allocation.js
@@ -9,13 +9,20 @@ Suppose there are 2 students.
 Your job is to distribute all books among these students.
 
 But there are some rules.
+
 Rule 1: Every book must be assigned
 Rule 2: One book cannot be divided
 Rule 3: Every student must get at least one book
-
 Rule 4: Books must remain contiguous:
-
 [10,20,30,40]
 Allowed: Student1 → 10 20
 Not Allowed: Student1 → 10 30
+*/
+
+/*
+We want
+The minimum possible value of the maximum pages assigned to any student.
+
+SIMPLE: We want the student who gets the most pages to still have as few pages as possible.
+
 */

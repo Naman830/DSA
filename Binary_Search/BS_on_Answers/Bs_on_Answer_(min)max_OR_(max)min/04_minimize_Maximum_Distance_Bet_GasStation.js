1763bedae3bbb03f885b4fff8746cad2b18bda0d
Naman <namansingla830@gmail.com>
2026-06-30 08:14:08 +0530

    add example

--- Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/04_minimize_Maximum_Distance_Bet_GasStation.js
+++ Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/04_minimize_Maximum_Distance_Bet_GasStation.js
@@ -9,4 +9,27 @@ Your task is to place exactly k new gas stations between the existing stations s
 The maximum distance between any two adjacent gas stations becomes as small as possible.
 
 Return that minimum possible maximum distance.
+
+
+Example
+stations = [1, 7]
+k = 2
+
+Initially
+1 ---------------------- 7
+
+Distance = 6
+We can add two stations.
+
+Best placement:
+
+1 ---- 3 ---- 5 ---- 7
+
+Distances:
+2
+2
+2
+
+Maximum distance = 2
+Answer = 2
 */

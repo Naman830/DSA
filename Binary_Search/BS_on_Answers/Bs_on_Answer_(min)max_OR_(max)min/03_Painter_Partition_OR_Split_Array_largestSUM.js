5281a9e15981745d5213d4bcf9b563a0eb0c398b
Naman <namansingla830@gmail.com>
2026-06-30 07:39:59 +0530

    observation for painter partion and largest sum split

---
+++ Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/03_Painter_Partition_OR_Split_Array_largestSUM.js
@@ -0,0 +1,6 @@
+/*
+Painter's Partition and Split Array Largest Sum are direct applications of the Book Allocation problem.
+
+Only the story and variable names change.
+The Binary Search logic, search space (max → sum), and partition function remain exactly the same.
+*/

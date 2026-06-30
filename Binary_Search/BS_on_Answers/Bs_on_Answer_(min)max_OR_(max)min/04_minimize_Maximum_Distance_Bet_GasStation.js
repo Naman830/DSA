b9e466c7984989bc695ee2bb76a8e05e4ee6e2a4
Naman <namansingla830@gmail.com>
2026-06-30 08:13:50 +0530

    add observation for minmize max distance bet gas station

---
+++ Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/04_minimize_Maximum_Distance_Bet_GasStation.js
@@ -0,0 +1,12 @@
+/*
+You are given:
+
+A sorted array stations[] representing the positions of existing gas stations.
+An integer k, representing the number of new gas stations you can build.
+
+Your task is to place exactly k new gas stations between the existing stations so that:
+
+The maximum distance between any two adjacent gas stations becomes as small as possible.
+
+Return that minimum possible maximum distance.
+*/

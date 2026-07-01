95f4fc230584339fedb0ef5f1d6bd537d2a795cc
Naman <namansingla830@gmail.com>
2026-07-01 08:10:47 +0530

    add observation if riw with max number in 1st 2D ARRAY

---
+++ Binary_Search/2d_array/01_row_with_maximum_numberOF_1s.js
@@ -0,0 +1,19 @@
+/*
+You are given a binary matrix (contains only 0s and 1s).
+Each row is sorted, meaning:
+
+All 0s come first.
+Then all 1s.
+
+Example:
+
+[
+ [0,0,1,1],
+ [0,1,1,1],
+ [0,0,0,1],
+ [1,1,1,1]
+]
+
+Find the index of the row having the maximum number of 1s.
+If no row contains any 1, return -1.
+*/

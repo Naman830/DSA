6bd772a980d38b97d7b8bf95096b1dda192ea246
Naman <namansingla830@gmail.com>
2026-07-01 07:28:31 +0530

    add observation of median two sorted array

---
+++ Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/05_median_Of_Two_sorted_arrays.js
@@ -0,0 +1,20 @@
+/*
+What is Median? 
+Median is the middle element of a sorted array.
+
+You are given two sorted arrays.
+nums1 = [1,3]
+nums2 = [2]
+
+Merge them mentally (don't actually merge):
+[1,2,3]
+
+Now return the median.
+Output: 2
+
+Instead of merging,
+Can we somehow split both arrays so that
+Left Part | Right Part
+contains exactly half of the elements?
+If yes, Median can be calculated directly.
+*/

1e7f19c8e4264a94dbf78c269c6a92f22f2f87b1
Naman <namansingla830@gmail.com>
2026-07-01 08:39:07 +0530

    add observation of 2d matrix

---
+++ Binary_Search/2d_array/02_search_In_2d_Matrix.js
@@ -0,0 +1,20 @@
+/*
+===========================================
+Search in a 2D Matrix - I (Binary Search)
+===========================================
+
+Observation:
+- Every row is sorted.
+- First element of each row > Last element of previous row.
+- Therefore, the whole matrix behaves like ONE sorted array.
+
+Example:
+1  3  5  7
+10 11 16 20
+23 30 34 60
+
+Equivalent:
+[1,3,5,7,10,11,16,20,23,30,34,60]
+
+
+*/

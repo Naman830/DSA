48931499be44366b693a2a707f5c150ffd1f630a
Naman <namansingla830@gmail.com>
2026-07-01 07:33:18 +0530

    add js code

--- Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/05_median_Of_Two_sorted_arrays.js
+++ Binary_Search/BS_on_Answers/Bs_on_Answer_(min)max_OR_(max)min/05_median_Of_Two_sorted_arrays.js
@@ -25,11 +25,61 @@ If yes, Median can be calculated directly.
 
 /*
 Pseudocode
+
 Merge arrays
 
 If odd
     return middle
-
 Else
     return average of middle two
 */
+
+var findMedianSortedArrays = function (nums1, nums2) {
+  // Array to store the merged sorted elements
+  const merged = [];
+
+  // Pointers for both arrays
+  let i = 0;
+  let j = 0;
+
+  // Merge both arrays while both have elements
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] <= nums2[j]) {
+      merged.push(nums1[i]);
+      i++;
+    } else {
+      merged.push(nums2[j]);
+      j++;
+    }
+  }
+
+  // Copy remaining elements from nums1
+  while (i < nums1.length) {
+    merged.push(nums1[i]);
+    i++;
+  }
+
+  // Copy remaining elements from nums2
+  while (j < nums2.length) {
+    merged.push(nums2[j]);
+    j++;
+  }
+
+  // Total number of elements
+  const n = merged.length;
+
+  // If total elements are odd,
+  // return the middle element
+  if (n % 2 === 1) {
+    return merged[Math.floor(n / 2)];
+  }
+
+  // If total elements are even,
+  // return the average of the two middle elements
+  const mid1 = merged[n / 2 - 1];
+  const mid2 = merged[n / 2];
+
+  return (mid1 + mid2) / 2;
+};
+
+console.log(findMedianSortedArrays([1, 2], [3, 4]));

cadee6f982083ee9358eab11a5fed83c99bb401c
Naman <namansingla830@gmail.com>
2026-07-02 07:44:49 +0530

    add matrix code

--- Binary_Search/2d_array/05_median_In_a_RowWise_sorted_matrix.js
+++ Binary_Search/2d_array/05_median_In_a_RowWise_sorted_matrix.js
@@ -65,6 +65,12 @@ Instead, we binary search on the value.
  * Since the row is sorted,
  * this index is also equal to the number of elements <= target.
  */
+/**
+ * Returns the index of the first element > target.
+ *
+ * Since the row is sorted,
+ * this index is also equal to the number of elements <= target.
+ */
 function upperBound(arr, target) {
   let low = 0;
   let high = arr.length - 1;
@@ -89,3 +95,76 @@ function upperBound(arr, target) {
   // Number of elements <= target
   return ans;
 }
+
+/**
+ * Returns the median of a row-wise sorted matrix.
+ */
+function median(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  // -----------------------------------
+  // Step 1:
+  // Find the smallest and largest values
+  // in the matrix.
+  //
+  // Since every row is sorted,
+  // first element = row minimum
+  // last element = row maximum
+  // -----------------------------------
+
+  let low = Infinity;
+  let high = -Infinity;
+
+  for (let row = 0; row < rows; row++) {
+    low = Math.min(low, matrix[row][0]);
+    high = Math.max(high, matrix[row][cols - 1]);
+  }
+
+  // Total elements that should come before (or equal)
+  // to the median.
+  const required = Math.floor((rows * cols) / 2);
+
+  // -----------------------------------
+  // Binary Search on VALUE
+  // -----------------------------------
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+
+    let count = 0;
+
+    // Count numbers <= mid
+    for (let row = 0; row < rows; row++) {
+      count += upperBound(matrix[row], mid);
+    }
+
+    // --------------------------------
+    // Not enough numbers <= mid
+    // Median must be larger
+    // --------------------------------
+    if (count <= required) {
+      low = mid + 1;
+    }
+
+    // --------------------------------
+    // Enough numbers <= mid
+    // Try finding a smaller answer
+    // --------------------------------
+    else {
+      high = mid - 1;
+    }
+  }
+
+  // low is the smallest value
+  // having more than half elements
+  // smaller than or equal to it.
+  return low;
+}
+const matrix = [
+  [1, 3, 5],
+  [2, 6, 9],
+  [3, 6, 9],
+];
+
+console.log("Answer =", median(matrix));

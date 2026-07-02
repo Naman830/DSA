8715e80c415a20b92462877312ed31e0e20acd27
Naman <namansingla830@gmail.com>
2026-07-02 07:36:08 +0530

    add explanation for median in a row wise sorted matrix

---
+++ Binary_Search/2d_array/05_median_In_a_RowWise_sorted_matrix.js
@@ -0,0 +1,22 @@
+/*
+You are given a matrix where:
+
+Every row is sorted in increasing order.
+Rows are NOT sorted with respect to each other.
+
+Example:
+
+[
+ [1, 3, 5],
+ [2, 6, 9],
+ [3, 6, 9]
+]
+
+Notice:
+
+Row 1 : 1 3 5   ✓ sorted
+Row 2 : 2 6 9   ✓ sorted
+Row 3 : 3 6 9   ✓ sorted
+
+But columns are NOT necessarily sorted.
+*/

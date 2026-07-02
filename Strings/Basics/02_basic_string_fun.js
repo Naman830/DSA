688b6519808128f6088b442b4da5d7ce53b8e62d
Naman <namansingla830@gmail.com>
2026-07-02 08:38:01 +0530

    ad meaning of string'

---
+++ Strings/Basics/02_basic_string_fun.js
@@ -0,0 +1,20 @@
+// MEANING OF STRING
+// A string is simply a sequence of characters.
+// Think of it like a word or sentence where each letter has its own position (index).
+
+let str = "hello";
+
+// Memory looks like this:
+/*
+| Index     | 0 | 1 | 2 | 3 | 4 |
+| --------- | - | - | - | - | - |
+| Character | h | e | l | l | o |
+*/
+// Each character can be accessed using its index.
+
+console.log(str[0]); // h
+console.log(str[3]); // l
+
+// =================================================================
+// =================================================================
+// =================================================================

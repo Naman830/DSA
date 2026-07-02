8b55bf6b9b34153a272ba395857d32cfdacfafbc
Naman <namansingla830@gmail.com>
2026-07-02 07:40:35 +0530

    add uper bound js

--- Binary_Search/2d_array/05_median_In_a_RowWise_sorted_matrix.js
+++ Binary_Search/2d_array/05_median_In_a_RowWise_sorted_matrix.js
@@ -58,3 +58,34 @@ The biggest mistake most people make is trying to binary search on the matrix in
 We don't binary search on rows or columns.
 Instead, we binary search on the value.
 */
+
+/**
+ * Returns the index of the first element > target.
+ *
+ * Since the row is sorted,
+ * this index is also equal to the number of elements <= target.
+ */
+function upperBound(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  // Assume every element is <= target
+  let ans = arr.length;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+
+    // Found a bigger element
+    if (arr[mid] > target) {
+      ans = mid; // Store its index
+      high = mid - 1; // Look for an even earlier one
+    }
+    // Current element is <= target
+    else {
+      low = mid + 1;
+    }
+  }
+
+  // Number of elements <= target
+  return ans;
+}

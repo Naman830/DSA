944a1ee528adb06d1db7170e4f4967564d7fff8c
Naman <namansingla830@gmail.com>
2026-07-02 08:36:07 +0530

    add 2nd example

--- Strings/Basics/01_consoleLog_trick.js
+++ Strings/Basics/01_consoleLog_trick.js
@@ -3,3 +3,18 @@
 // 1st example
 const name = "Naman";
 console.log`Hey my name is ${name}`; // In this example, you can see that Naman appears outside the string because it is not part of the template literal. Instead, it is inserted from outside using ${Naman}.
+
+// 2nd example
+// We use function
+function study(text, collegeName, collegeRollNo) {
+  // Look at this text. Everything written inside the backticks (` `) is part of the string[ARRAY].
+  // `collegeName` and `collegeRollNo` are inserted into the string from outside using `${}`
+  // If you remove `${collegeRollNo}`, its value will no longer appear in the output.
+  console.log(text, collegeName, collegeRollNo);
+  return;
+}
+
+const college = "Indra gandhi college";
+const rollNo = 125420;
+
+study`I study from ${college} and my roll number is ${rollNo}`;

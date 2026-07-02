a80fc4ea57f838a81aea6fcbfaef9c15a6d23a31
Naman <namansingla830@gmail.com>
2026-07-02 08:35:53 +0530

    add string console trick 1st example

---
+++ Strings/Basics/01_consoleLog_trick.js
@@ -0,0 +1,5 @@
+// So, you know that we use console.log() with parentheses. We can also use template literals (backticks ` `), which allow us to embed variables or expressions inside a string using ${}.
+
+// 1st example
+const name = "Naman";
+console.log`Hey my name is ${name}`; // In this example, you can see that Naman appears outside the string because it is not part of the template literal. Instead, it is inserted from outside using ${Naman}.

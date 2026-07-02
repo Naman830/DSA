4fa392321ff26479d442eee01cf3e501814c622a
Naman <namansingla830@gmail.com>
2026-07-02 06:57:38 +0530

    add observation for peak element

---
+++ Binary_Search/2d_array/04_find_peak_element_II.js
@@ -0,0 +1,30 @@
+/*
+You are given a 2D matrix.
+You need to find any one Peak Element.
+
+A cell is called a Peak if it is strictly greater than its 4 neighbours:
+Top
+Bottom
+Left
+Right
+
+(Diagonal cells are NOT considered.)
+If a neighbour does not exist (boundary), simply ignore it.
+
+1   4   3
+6   7   8
+5   2   9
+Is 9 a peak?
+
+Check its neighbours
+Top    = 8
+Left   = 2
+Right  = Doesn't exist
+Bottom = Doesn't exist
+
+Since
+9 > 8
+9 > 2
+
+So yes, 9 is a Peak.
+*/

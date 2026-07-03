891d21e552e65e3433834f1621bf51f6e4b7c95e
Naman <namansingla830@gmail.com>
2026-07-03 08:55:31 +0530

    add observation witbh rules

---
+++ Strings/Problems/02_reverse_words_in_String.js
@@ -0,0 +1,17 @@
+/*
+You are given a string s.
+
+
+
+Example 1
+Input:
+"the sky is blue"
+
+Output:
+"blue is sky the"
+
+Rules: 
+1. Your task is to reverse the order of the words, not the characters.
+2. Multiple spaces between words should become only one space.
+3. Extra spaces at the beginning and end should be removed.
+*/

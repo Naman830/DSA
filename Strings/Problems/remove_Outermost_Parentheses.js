7402818ce71ef8906346a2e1002ca9dd70c64baf
Naman <namansingla830@gmail.com>
2026-07-03 07:51:20 +0530

    add observation for remove outermost parenthesis

---
+++ Strings/Problems/remove_Outermost_Parentheses.js
@@ -0,0 +1,15 @@
+/*
+You are given a valid parentheses string.
+Your task is to remove the outermost pair of parentheses from every primitive valid parentheses string.
+
+Example
+Input:  "(()())(())"
+
+Primitive 1: (()())
+Remove outermost -> ()()
+
+Primitive 2: (())
+Remove outermost -> ()
+
+Output: "()()()"
+*/

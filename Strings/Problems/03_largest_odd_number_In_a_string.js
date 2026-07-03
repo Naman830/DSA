8a0fa235031eb3f6b151f33fe3fcefa8e2dcdc0a
Naman <namansingla830@gmail.com>
2026-07-03 09:21:33 +0530

    add brute force solution

--- Strings/Problems/03_largest_odd_number_In_a_string.js
+++ Strings/Problems/03_largest_odd_number_In_a_string.js
@@ -9,3 +9,36 @@ We cannot rearrange digits.
 We can only remove digits from the end.
 Therefore, we need the rightmost odd digit.
 */
+
+// 1. Brute Force
+// Generate every prefix and check whether it is odd.
+// Keep updating the answer whenever you find an odd prefix.
+
+/*
+Problem:
+Given a string consisting of digits,
+return the largest-valued odd number (prefix).
+If none exists, return "".
+*/
+
+function largestOddNumber(num) {
+  let answer = "";
+
+  for (let i = 0; i < num.length; i++) {
+    const prefix = num.slice(0, i + 1);
+
+    // Last character of the current prefix
+    const lastDigit = Number(prefix[prefix.length - 1]);
+
+    if (lastDigit % 2 !== 0) {
+      answer = prefix;
+    }
+  }
+
+  return answer;
+}
+
+// Test Cases
+console.log(largestOddNumber("52")); // "5"
+console.log(largestOddNumber("35427")); // "35427"
+console.log(largestOddNumber("4206")); // ""

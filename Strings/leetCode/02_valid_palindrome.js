e36a4cbe09d8c44f4de7c637c30c741369a27974
Naman <namansingla830@gmail.com>
2026-07-03 08:45:42 +0530

    add optimal solution for palidrome

---
+++ Strings/leetCode/02_valid_palindrome.js
@@ -0,0 +1,18 @@
+function palidrome(s) {
+  let word = s.split(" ").join("");
+
+  let left = 0;
+  let right = word.length - 1;
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[left] !== word[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+let s = "race a car";
+
+console.log(palidrome(s));

ad68315989efc842204d78eba942a43ed40b3b9e
Naman <namansingla830@gmail.com>
2026-07-04 07:33:44 +0530

    when to use two pointer

---
+++ DSA_Patterns_Notes/Two_Pointers/two_pointer.js
@@ -0,0 +1,15 @@
+/*
+Two Pointer — Quick Revision Notes
+Core Idea
+
+Use 2 index variables instead of nested loops to scan a sorted array/string in O(n) instead of O(n²).
+
+When to Use (Recognize These Signals)
+
+1. Array/string is sorted (or can be sorted)
+2. Problem asks for a pair/triplet with a target sum
+3. Need to check palindrome
+4. Need to remove duplicates in-place
+5. Merging two sorted arrays
+5. Words like: "pair", "sum equals target", "container", "trapping water"
+*/

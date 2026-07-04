7d1412fa0258f61c52dc41523dba04f0aad03505
Naman <namansingla830@gmail.com>
2026-07-04 08:09:00 +0530

    what is linked list

---
+++ Linked_List/introduction_of_linked_list.js
@@ -0,0 +1,17 @@
+// ============================================
+// LINKED LIST — INTRODUCTION (JavaScript)
+// ============================================
+
+// ---------------------------------------------
+// 1. WHAT IS A LINKED LIST?
+// ---------------------------------------------
+// A Linked List is a linear data structure where elements (called "Nodes")
+// are NOT stored in contiguous memory (unlike arrays).
+// Each Node stores:
+//    -> data (the value)
+//    -> a pointer/reference to the NEXT node
+//
+// Think of it like a chain: Node1 -> Node2 -> Node3 -> null
+// Instead of Index it store next value like Node1 (next= m2) -> Node2 (next = m3) -> Node3 (next = m4) -> Node4 (null)
+// The last node points to "null" (means end of list)
+// First Node called 'Head' and last node called 'Tail

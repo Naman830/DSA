b765b3e48e70de064d5f144aa5949097d6c35e9d
Naman <namansingla830@gmail.com>
2026-07-06 13:26:10 +0530

    add observation of deletion of hed in dll

---
+++ Linked_List/Doubly_LinkedList/delete_of_DLL/01_delete_head.js
@@ -0,0 +1,13 @@
+/*
+Before deletion
+NULL <- [10] <-> [20] <-> [30] -> NULL
+        ^
+       head
+
+After deleting head
+NULL <- [20] <-> [30] -> NULL
+        ^
+       head
+
+The node 10 is removed.
+*/

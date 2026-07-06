add3b1083f5e18f9194b9370468c054b710178fc
Naman <namansingla830@gmail.com>
2026-07-06 13:40:36 +0530

    update observation

--- Linked_List/Doubly_LinkedList/delete_of_DLL/02_delete_tail.js
+++ Linked_List/Doubly_LinkedList/delete_of_DLL/02_delete_tail.js
@@ -1,7 +1,8 @@
 /*
 Key Observations
-A singly linked list only moves forward.
-We cannot directly reach the previous node of the tail.
-Therefore, we must stop at the second last node.
-Then make its next point to null.
+If the list is empty, return null.
+If the list has only one node, deleting the tail makes the list empty.
+Otherwise:
+    Move to the last node.
+    Disconnect it from its previous node.
 */

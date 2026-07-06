5b3aaeeb2c168aeb4a06b483c1be28287706e48a
Naman <namansingla830@gmail.com>
2026-07-06 13:26:44 +0530

    add key observation

--- Linked_List/Doubly_LinkedList/delete_of_DLL/01_delete_head.js
+++ Linked_List/Doubly_LinkedList/delete_of_DLL/01_delete_head.js
@@ -10,4 +10,11 @@ NULL <- [20] <-> [30] -> NULL
        head
 
 The node 10 is removed.
+
+Key Observations
+If the list is empty → return null.
+If only one node exists → deleting it makes the list empty.
+Otherwise:
+Move head to second node.
+Make new head's prev = null.
 */

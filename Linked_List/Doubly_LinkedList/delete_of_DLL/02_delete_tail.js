0812d2071b22782c4ab186d5f1cd03f83a1d37fc
Naman <namansingla830@gmail.com>
2026-07-06 13:36:55 +0530

    add observation for delting tail of dll

---
+++ Linked_List/Doubly_LinkedList/delete_of_DLL/02_delete_tail.js
@@ -0,0 +1,7 @@
+/*
+Key Observations
+A singly linked list only moves forward.
+We cannot directly reach the previous node of the tail.
+Therefore, we must stop at the second last node.
+Then make its next point to null.
+*/

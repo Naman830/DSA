18d72f3f100a26913228e1105e846069642dcedf
Naman <namansingla830@gmail.com>
2026-07-06 13:21:19 +0530

    add converting array into double ll

---
+++ Linked_List/Doubly_LinkedList/02_array_To_DD.js
@@ -0,0 +1,26 @@
+// Class representing a node in Doubly Linked List
+class Node {
+  // Constructor to initialize a node
+  constructor(data, next = null, prev = null) {
+    // Stores data of the node
+    this.data = data;
+
+    // Pointer to the next node
+    this.next = next;
+
+    // Pointer to the previous node
+    this.prev = prev;
+  }
+}
+
+// Initializing an array to create nodes
+let arr = [2, 5, 8, 7];
+
+// Creating the head node of the doubly linked list
+let head = new Node(arr[0]);
+
+// Printing the object representing head
+console.log(head);
+
+// Printing the data stored in head node
+console.log(head.data);

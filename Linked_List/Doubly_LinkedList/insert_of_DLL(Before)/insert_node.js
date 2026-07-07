8cbf9270555c4aee136af33edd6b5737122d3e6f
Naman <namansingla830@gmail.com>
2026-07-07 09:13:36 +0530

    add observation

---
+++ Linked_List/Doubly_LinkedList/insert_of_DLL(Before)/insert_node.js
@@ -0,0 +1,11 @@
+/*
+You are given a pointer/reference to a node in a Doubly Linked List. Insert a new node before that node.
+
+Since the given node is not the head, the head remains unchanged.
+
+2. Key Observations
+Every node has prev and next.
+We already have the node before which we need to insert.
+Update 4 pointers carefully.
+Head does not change because we're not inserting before the first node.
+*/

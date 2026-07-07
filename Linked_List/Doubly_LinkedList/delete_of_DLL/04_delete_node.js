0eddb0758f651170637cf5bc632aead100f0c904
Naman <namansingla830@gmail.com>
2026-07-07 08:42:53 +0530

    add observation

---
+++ Linked_List/Doubly_LinkedList/delete_of_DLL/04_delete_node.js
@@ -0,0 +1,14 @@
+/*
+Every node has:
+next
+prev
+
+While deleting a node:
+Previous node should point to the next node.
+Next node should point to the previous node.
+
+Special cases:
+Deleting the head
+Deleting the tail
+Deleting the only node
+*/

0e2d2709e406294a3d8b434f6b2f18495b6387d7
Naman <namansingla830@gmail.com>
2026-07-07 08:49:03 +0530

    add key observation for insert head in DLL

---
+++ Linked_List/Doubly_LinkedList/insert_of_DLL(Before)/insert_head.js
@@ -0,0 +1,9 @@
+/*
+Key Observations
+
+The new node becomes the first node.
+The new node's next should point to the old head.
+The old head's prev should point back to the new node.
+The new node's prev will always be null.
+If the list is empty, simply return the new node.
+*/

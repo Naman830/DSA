b8f71f594d32fe47ef49ea4f9d021e96dd4ead86
Naman <namansingla830@gmail.com>
2026-07-07 17:07:06 +0530

    add brute force solution

--- Array/Array_leetcode/1299_replace_with_greatest_element.js
+++ Array/Array_leetcode/1299_replace_with_greatest_element.js
@@ -22,3 +22,28 @@ The last element will always become -1.
 The array size does not change.
 Only the values are replaced.
 */
+
+// 1. Brute Force Approach
+
+function replaceElements(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    let max = -1;
+
+    for (let j = i + 1; j < n; j++) {
+      max = Math.max(max, arr[j]);
+    }
+
+    arr[i] = max;
+  }
+
+  return arr;
+}
+
+// Test Cases
+console.log(replaceElements([17, 18, 5, 4, 6, 1]));
+// [18, 6, 6, 6, 1, -1]
+
+console.log(replaceElements([400]));
+// [-1]

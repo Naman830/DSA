76079fe80064eb8e46c150eae3fcbdacba81b96f
Naman <namansingla830@gmail.com>
2026-07-07 08:49:17 +0530

    add diagram

--- Linked_List/Doubly_LinkedList/insert_of_DLL(Before)/insert_head.js
+++ Linked_List/Doubly_LinkedList/insert_of_DLL(Before)/insert_head.js
@@ -1,6 +1,13 @@
 /*
-Key Observations
+Before:
+NULL <- 20 <-> 30 <-> 40 -> NULL
 
+Insert 10
+
+After: 
+NULL <- 10 <-> 20 <-> 30 <-> 40 -> NULL
+
+Key Observations
 The new node becomes the first node.
 The new node's next should point to the old head.
 The old head's prev should point back to the new node.

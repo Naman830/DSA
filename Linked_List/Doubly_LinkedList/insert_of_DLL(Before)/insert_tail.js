5c9ce7e7af400c3946a07536aca0ce8794668b18
Naman <namansingla830@gmail.com>
2026-07-07 08:55:56 +0530

    add key observation

---
+++ Linked_List/Doubly_LinkedList/insert_of_DLL(Before)/insert_tail.js
@@ -0,0 +1,19 @@
+/*
+Before:
+10 ⇄ 20 ⇄ 30 ⇄ 40
+
+Insert 25 before the tail (40).
+
+After:
+10 ⇄ 20 ⇄ 30 ⇄ 25 ⇄ 40
+
+Key Observations
+Tail is the last node (next == null).
+We need the node before the tail.
+Since it's a DLL, updating both next and prev pointers is important.
+
+Edge cases:
+Empty list
+Only one node
+Two or more nodes
+*/

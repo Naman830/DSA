180e4e37429e561ef9264522921662d51b041c9b
Naman <namansingla830@gmail.com>
2026-07-08 08:40:06 +0530

    add explanation

---
+++ Linked_List/Problem_of_LL/02_Even_Odd.js
@@ -0,0 +1,26 @@
+/*
+EXPLANATION:-
+
+The problem is NOT about odd and even values.
+It is about odd and even positions (indices) of nodes.
+For example,
+
+Input:
+1 → 2 → 3 → 4 → 5
+
+Positions:
+1st  2nd  3rd  4th  5th
+
+Odd Position Nodes:
+1 → 3 → 5
+
+Even Position Nodes:
+2 → 4
+
+Output:
+1 → 3 → 5 → 2 → 4
+
+Notice:
+Node values don't matter.
+Only their positions matter.
+*/

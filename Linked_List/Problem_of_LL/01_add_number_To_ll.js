d1f52200de7cda8ba9ace80b5ef82760dc80e173
Naman <namansingla830@gmail.com>
2026-07-08 08:14:37 +0530

    add key observation

--- Linked_List/Problem_of_LL/01_add_number_To_ll.js
+++ Linked_List/Problem_of_LL/01_add_number_To_ll.js
@@ -17,5 +17,10 @@ These actually represent the numbers:
 The answer should also be stored in reverse order.
 7 → 0 → 8
 
-
+Key Observations
+Every node contains one digit (0-9).
+Addition is performed exactly like we do on paper.
+We must keep track of the carry.
+The linked lists can have different lengths.
+If a carry remains after the last nodes, create one more node.
 */

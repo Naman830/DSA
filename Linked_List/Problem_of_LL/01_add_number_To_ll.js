1e04c140373db6c185dab01729db7b0ab16d831c
Naman <namansingla830@gmail.com>
2026-07-08 08:14:15 +0530

    add explanation of add number to ll

---
+++ Linked_List/Problem_of_LL/01_add_number_To_ll.js
@@ -0,0 +1,21 @@
+/*
+EXPLANATION :-
+You are given two linked lists where each node contains a single digit
+The digits are stored in reverse order.
+
+Example:
+List 1: 2 → 4 → 3
+List 2: 5 → 6 → 4
+
+These actually represent the numbers:
+342
++
+465
+----
+807
+
+The answer should also be stored in reverse order.
+7 → 0 → 8
+
+
+*/

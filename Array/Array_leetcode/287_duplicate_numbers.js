9c1d3e67c27df1bd650467bd650aec17044240ee
Naman <namansingla830@gmail.com>
2026-07-10 15:16:11 +0530

    add js code

--- Array/Array_leetcode/287_duplicate_numbers.js
+++ Array/Array_leetcode/287_duplicate_numbers.js
@@ -9,3 +9,31 @@ Your task is to return the duplicate number without modifying the array.
 */
 
 // 1. Brute Force Approach
+// For every element, count how many times it appears.
+// If any count becomes greater than 1, return that element.
+
+/*
+Find the Duplicate Number
+Brute Force
+Time : O(n²)
+Space : O(1)
+*/
+
+function findDuplicate(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0;
+
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        count++;
+      }
+    }
+
+    if (count > 1) {
+      return nums[i];
+    }
+  }
+}
+
+console.log(findDuplicate([1, 3, 4, 2, 2])); // 2
+console.log(findDuplicate([3, 1, 3, 4, 2])); // 3

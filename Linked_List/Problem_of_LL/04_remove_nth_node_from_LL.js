d7e8c754cfaa6fb99a184acf4362474c5c54d176
Naman <namansingla830@gmail.com>
2026-07-10 08:45:21 +0530

    add steps and tc and sc

--- Linked_List/Problem_of_LL/04_remove_nth_node_from_LL.js
+++ Linked_List/Problem_of_LL/04_remove_nth_node_from_LL.js
@@ -20,3 +20,39 @@ We only know how to move forward in a singly linked list.
 We cannot move backwards.
 So how do we find the node from the end?
 */
+
+// 1. Approach 1 (Brute Force)
+
+/*
+Approach 1 (Brute Force)
+
+Step 1
+Count total nodes.
+
+1 → 2 → 3 → 4 → 5
+Length = 5
+
+Step 2
+Find which node to remove from the front.
+
+Formula
+Position from front = Length - n + 1
+
+Example
+Length = 5
+n = 2
+
+5 - 2 + 1 = 4
+
+Remove 4th node.
+1
+2
+3
+4 ❌
+5
+
+Step 3- connect the previous node to next next node 
+
+Time Complexity O(N) + O(N) = O(2N)
+Space: O(1)
+*/

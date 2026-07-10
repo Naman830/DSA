f6df19fac6c6209b1a266c81d26510b4c17d9ed2
Naman <namansingla830@gmail.com>
2026-07-10 15:14:56 +0530

    add explanation of duplicate numbers

---
+++ Array/Array_leetcode/287_duplicate_numbers.js
@@ -0,0 +1,9 @@
+/*
+You are given an array nums containing n + 1 integers.
+
+Every number is between 1 and n (inclusive).
+There is only one duplicate number.
+That duplicate number may appear more than twice.
+
+Your task is to return the duplicate number without modifying the array.
+*/

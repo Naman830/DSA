3284c4d841d2676965af8fd5fce0b449b07f419b
Naman <namansingla830@gmail.com>
2026-07-11 10:21:29 +0530

    add observation of recursive ll

---
+++ Linked_List/Problem_of_LL/reverse_LL/03_recursive_reverse_LL.js
@@ -0,0 +1,6 @@
+/*
+We cannot move backwards in a singly linked list.
+Recursion automatically stores previous nodes in the call stack.
+We first travel to the last node.
+While returning from recursion, we reverse every link.
+*/

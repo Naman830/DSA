57a6515329e98e71856339978b057b06f8004f45
Naman <namansingla830@gmail.com>
2026-07-11 10:21:55 +0530

    add pseudo code

--- Linked_List/Problem_of_LL/reverse_LL/03_recursive_reverse_LL.js
+++ Linked_List/Problem_of_LL/reverse_LL/03_recursive_reverse_LL.js
@@ -4,3 +4,22 @@ Recursion automatically stores previous nodes in the call stack.
 We first travel to the last node.
 While returning from recursion, we reverse every link.
 */
+
+/*
+Pseudocode
+
+reverse(head)
+
+If head is null
+    return null
+
+If head.next is null
+    return head
+
+newHead = reverse(head.next)
+
+head.next.next = head
+head.next = null
+
+return newHead
+*/

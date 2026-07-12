85bafdd8cd03ad9b4bacdf13a2f8f13cf05ccf96
Naman <namansingla830@gmail.com>
2026-07-12 07:37:41 +0530

    add algorithm

--- Linked_List/Problem_of_LL/05_check_If_LL_is_palindrome.js
+++ Linked_List/Problem_of_LL/05_check_If_LL_is_palindrome.js
@@ -2,3 +2,31 @@
 1 → 2 → 2 → 1 ✅ Palindrome
 1 → 2 → 3 → 4 ❌ Not a palindrome
 */
+
+// 1. Brute Force Approach (Using Extra Space)
+/*
+Traverse list
+↓
+
+Store every value in array
+↓
+
+Two pointers
+
+left = 0
+right = n-1
+↓
+
+Compare
+
+If mismatch
+return false
+
+Else
+left++
+right--
+↓
+
+Finished
+return true
+*/

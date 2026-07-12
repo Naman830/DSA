01b6c8626bd7a921acea6558451dc83f40ca6c70
Naman <namansingla830@gmail.com>
2026-07-12 07:40:40 +0530

    add observation of optimal solution

--- Linked_List/Problem_of_LL/05_check_If_LL_is_palindrome.js
+++ Linked_List/Problem_of_LL/05_check_If_LL_is_palindrome.js
@@ -35,3 +35,12 @@ return true
 Time : O(n)
 Space : O(n)
 */
+
+// 2. Optimal Approach (O(1) Space)
+// Reverse only the second half of the linked list.
+/*
+Step 1: Find the Middle [Use Slow and Fast Pointer.]
+Step 2: Reverse the Second Half
+Step 3: Compare Both Halves
+Step 4: Restore the list by reversing the second half again. [Some interviewers appreciate this because the original linked list remains unchanged.]
+*/

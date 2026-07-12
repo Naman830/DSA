ba78a3803f64167b1e8957616d3af6d8a77b880b
Naman <namansingla830@gmail.com>
2026-07-12 08:52:55 +0530

    add explanation of find intersection point of y ll

---
+++ Linked_List/Problem_of_LL/07_find_intersection_point_of_Y_ll.js
@@ -0,0 +1,17 @@
+/*
+You are given the heads of two singly linked lists.
+These two linked lists may merge at some point, forming a Y-shaped structure.
+Your task is to return the first common node where both linked lists intersect.
+
+Important: Two nodes are considered intersecting only if they are the exact s1ame node in memory, not because they have the same value.
+
+Example
+List A
+
+1 → 2 → 3
+          \
+           8 → 10 → 12
+          /
+      5 → 6
+List B
+*/

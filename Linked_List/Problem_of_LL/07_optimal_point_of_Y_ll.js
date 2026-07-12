a76ace6749d83935dbaad3c9efc719735cd17510
Naman <namansingla830@gmail.com>
2026-07-12 09:13:28 +0530

    approach epxlation

--- Linked_List/Problem_of_LL/07_optimal_point_of_Y_ll.js
+++ Linked_List/Problem_of_LL/07_optimal_point_of_Y_ll.js
@@ -1,5 +1,4 @@
 // Optimal Solution (Two Pointer Switching)
-
 /*
 Instead of calculating the lengths of both linked lists, we can make both pointers travel the same total distance.
 
@@ -13,4 +12,24 @@ Eventually:
 
 If the lists intersect, both pointers will meet at the intersection.
 If they don't intersect, both pointers will become null at the same time.
+*/
+
+/*
+When Pointer A finishes List A,
+instead of stopping,
+it starts from List B.
+Similarly,
+
+when Pointer B finishes List B,
+it starts from List A.
+Now look at the total distance.
+
+Pointer A travels
+
+List A + List B
+Pointer B also travels
+List B + List A
+Both travel exactly the same distance.
+
+Therefore, they become synchronized.
 */

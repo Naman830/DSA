eebf7dfbcaf82c11fcb9f6db969e238039b05e6d
Naman <namansingla830@gmail.com>
2026-07-12 07:53:25 +0530

    add js code

--- Linked_List/Problem_of_LL/05_check_If_LL_is_palindrome.js
+++ Linked_List/Problem_of_LL/05_check_If_LL_is_palindrome.js
@@ -40,7 +40,88 @@ Space : O(n)
 // Reverse only the second half of the linked list.
 /*
 Step 1: Find the Middle [Use Slow and Fast Pointer.]
+    For Even: fast.next.next
+    For Odd: fast.next
 Step 2: Reverse the Second Half
 Step 3: Compare Both Halves
 Step 4: Restore the list by reversing the second half again. [Some interviewers appreciate this because the original linked list remains unchanged.]
+
 */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Solution {
+  isPalindrome(head) {
+    // Empty list or single node is always a palindrome
+    if (head === null || head.next === null) return true;
+
+    // Step 1: Find the middle
+    let slow = head;
+    let fast = head;
+
+    while (fast.next !== null && fast.next.next !== null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    // Step 2: Reverse the second half
+    let secondHalf = this.reverse(slow.next);
+
+    // Step 3: Compare both halves
+    let firstHalf = head;
+    let temp = secondHalf;
+
+    while (temp !== null) {
+      if (firstHalf.value !== temp.value) {
+        return false;
+      }
+
+      firstHalf = firstHalf.next;
+      temp = temp.next;
+    }
+
+    return true;
+  }
+
+  // Helper function to reverse a linked list
+  reverse(head) {
+    let prev = null;
+    let curr = head;
+
+    while (curr !== null) {
+      let nextNode = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = nextNode;
+    }
+
+    return prev;
+  }
+  print(head) {
+    let current = head;
+    let result = [];
+    while (current !== null) {
+      result.push(current.value);
+      current = current.next;
+    }
+    return console.log(result);
+  }
+}
+
+const obj = new Solution();
+
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(2);
+head.next.next.next.next = new Node(1);
+
+const ans = obj.isPalindrome(head);
+
+console.log(ans);
+obj.print(head);

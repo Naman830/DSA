fca540b30f9887b528eec3e4da6cffb8688edefe
Naman <namansingla830@gmail.com>
2026-07-13 12:45:40 +0530

    add js code

--- Linked_List/Problem_of_LL/11_delete_middle_node_ll.js
+++ Linked_List/Problem_of_LL/11_delete_middle_node_ll.js
@@ -18,3 +18,60 @@ Then calculate the middle position.
 Traverse again until the node before the middle and remove the middle node.
 This requires two traversals.
 */
+
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+function deleteMiddle(head) {
+  // If only one node exists
+  if (head === null || head.next === null) {
+    return null;
+  }
+
+  // Count total nodes
+  let count = 0;
+  let temp = head;
+
+  while (temp !== null) {
+    count++;
+    temp = temp.next;
+  }
+
+  // Middle index (0-based)
+  let middle = Math.floor(count / 2);
+
+  temp = head;
+
+  // Reach node before middle
+  for (let i = 0; i < middle - 1; i++) {
+    temp = temp.next;
+  }
+
+  // Delete middle node
+  temp.next = temp.next.next;
+
+  return head;
+}
+// Helper function
+function printList(head) {
+  let result = [];
+  while (head) {
+    result.push(head.val);
+    head = head.next;
+  }
+  console.log(result.join(" -> "));
+}
+
+// Test Case
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+head = deleteMiddle(head);
+printList(head); // 1 -> 2 -> 4 -> 5

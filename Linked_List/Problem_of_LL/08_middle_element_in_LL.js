c436502a4a76c1c3ef563c6ff3988e5b4ee82c2d
Naman <namansingla830@gmail.com>
2026-07-13 11:44:51 +0530

    add explanation of middle element

---
+++ Linked_List/Problem_of_LL/08_middle_element_in_LL.js
@@ -0,0 +1,4 @@
+/*
+If the linked list has an odd number of nodes, there is only one middle node.
+If the linked list has an even number of nodes, most interview questions (including LeetCode 876) ask you to return the second middle node.
+*/

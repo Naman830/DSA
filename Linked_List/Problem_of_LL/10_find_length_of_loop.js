762b35cc2f645b9ef0b493e36c9ad3f04335087b
Naman <namansingla830@gmail.com>
2026-07-13 12:27:24 +0530

    add eplanation

---
+++ Linked_List/Problem_of_LL/10_find_length_of_loop.js
@@ -0,0 +1,5 @@
+// Similar as problem 09 in this --> After detecting it, we count how many nodes form the cycle.
+
+// 1. Brute Force (HashSet)
+// The repeated node is the starting point where counting begins.
+// Traverse once around the loop until you reach that node again.

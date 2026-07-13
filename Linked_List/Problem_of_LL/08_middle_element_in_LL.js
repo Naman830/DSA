53d988241004067cc343a9af69837cb48a3790aa
Naman <namansingla830@gmail.com>
2026-07-13 11:47:29 +0530

    add js code

--- Linked_List/Problem_of_LL/08_middle_element_in_LL.js
+++ Linked_List/Problem_of_LL/08_middle_element_in_LL.js
@@ -14,3 +14,44 @@ middle = Math.floor(length / 2)
 Then traverse again until that index.
 So this requires two traversals.
 */
+
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+function middleNode(head) {
+  // Count total nodes
+  let count = 0;
+  let temp = head;
+
+  while (temp !== null) {
+    count++;
+    temp = temp.next;
+  }
+
+  // Find middle index
+  let middle = Math.floor(count / 2);
+
+  // Traverse again
+  temp = head;
+
+  while (middle > 0) {
+    temp = temp.next;
+    middle--;
+  }
+
+  return temp;
+}
+
+// Test
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+head.next.next.next.next.next = new ListNode(6);
+
+console.log(middleNode(head).val); // 3

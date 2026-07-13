9e87ac79b9cb7b532ca9a355187286aac356291f
Naman <namansingla830@gmail.com>
2026-07-13 11:46:14 +0530

    explanation of brute

--- Linked_List/Problem_of_LL/08_middle_element_in_LL.js
+++ Linked_List/Problem_of_LL/08_middle_element_in_LL.js
@@ -1,4 +1,16 @@
 /*
 If the linked list has an odd number of nodes, there is only one middle node.
+
 If the linked list has an even number of nodes, most interview questions (including LeetCode 876) ask you to return the second middle node.
+*/
+
+// Approach 1 — Brute Force (Count Nodes)
+/*
+First count the total number of nodes.
+Once we know the length, calculate
+
+middle = Math.floor(length / 2)
+
+Then traverse again until that index.
+So this requires two traversals.
 */

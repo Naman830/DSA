4dc68af94f4b1cc482d2e9b7ebbf163ed93c1c2b
Naman <namansingla830@gmail.com>
2026-07-13 12:42:03 +0530

    add explanation of delete midd node

---
+++ Linked_List/Problem_of_LL/11_delete_middle_node_ll.js
@@ -0,0 +1,5 @@
+/*
+You are given the head of a singly linked list.
+Your task is to delete the middle node and return the updated linked list.
+If the linked list has only one node, deleting the middle node means the list becomes empty (null).
+*/

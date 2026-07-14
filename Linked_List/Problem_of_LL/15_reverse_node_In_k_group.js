483352631b6e3ddbe995cd62a5c198cfbf9c6c1b
Naman <namansingla830@gmail.com>
2026-07-14 09:12:34 +0530

    add js code of reverse node

--- Linked_List/Problem_of_LL/15_reverse_node_In_k_group.js
+++ Linked_List/Problem_of_LL/15_reverse_node_In_k_group.js
@@ -104,3 +104,114 @@ while curr exists
 
 return dummy.next
 */
+
+/**
+ * Problem:
+ * Reverse every group of k nodes in a linked list.
+ * If remaining nodes are fewer than k, leave them unchanged.
+ */
+
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// Find the kth node starting from current node
+function getKthNode(curr, k) {
+  while (curr !== null && k > 1) {
+    curr = curr.next;
+    k--;
+  }
+
+  return curr;
+}
+
+// Reverse a linked list
+function reverse(head) {
+  let prev = null;
+  let curr = head;
+
+  while (curr !== null) {
+    let nextNode = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextNode;
+  }
+
+  return prev;
+}
+
+function reverseKGroup(head, k) {
+  let dummy = new ListNode(0);
+  dummy.next = head;
+
+  let prevGroup = dummy;
+  let curr = head;
+
+  while (curr !== null) {
+    // Find kth node
+    let kthNode = getKthNode(curr, k);
+
+    // Less than k nodes left
+    if (kthNode === null) break;
+
+    // Save next group's starting node
+    let nextGroup = kthNode.next;
+
+    // Break current group
+    kthNode.next = null;
+
+    // Reverse current group
+    let reversedHead = reverse(curr);
+
+    // Connect previous group
+    prevGroup.next = reversedHead;
+
+    // Current becomes tail after reversing
+    curr.next = nextGroup;
+
+    // Move prevGroup to current tail
+    prevGroup = curr;
+
+    // Start next group
+    curr = nextGroup;
+  }
+
+  return dummy.next;
+}
+// Helper: create linked list
+function createList(arr) {
+  let dummy = new ListNode(0);
+  let temp = dummy;
+
+  for (let num of arr) {
+    temp.next = new ListNode(num);
+    temp = temp.next;
+  }
+
+  return dummy.next;
+}
+
+// Helper: print linked list
+function printList(head) {
+  let result = [];
+
+  while (head) {
+    result.push(head.val);
+    head = head.next;
+  }
+
+  console.log(result.join(" -> "));
+}
+
+// Test Case
+let head = createList([1, 2, 3, 4, 5]);
+
+let ans = reverseKGroup(head, 2);
+
+printList(ans);
+
+// Output:
+// 2 -> 1 -> 4 -> 3 -> 5

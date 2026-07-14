218d95abb29f8ea4f0475dce96216e248a4a726f
Naman <namansingla830@gmail.com>
2026-07-14 08:20:13 +0530

    add pseudo code of brute for sort ll

--- Linked_List/Problem_of_LL/13_sort_LL.js
+++ Linked_List/Problem_of_LL/13_sort_LL.js
@@ -9,6 +9,24 @@ Algorithms like Quick Sort are not ideal for Linked Lists.
 Merge Sort is the best sorting algorithm for Linked Lists.
 
 Why?
-
 Because Merge Sort only needs sequential access.
+*/
+
+// Approach 1 — Brute Force
+/*
+Traverse the linked list.
+Store every value inside an array.
+Sort the array.
+Traverse the linked list again.
+
+Replace every node's value.
+We're not changing nodes, only changing values.
+*/
+/*
+Pseudocode
+
+Take all values into array
+Sort array
+Traverse linked list
+Replace values one by one
 */

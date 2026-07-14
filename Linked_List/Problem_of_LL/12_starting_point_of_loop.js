d3aec549ebb56a5cb09d4ccb0bbb8f278fa699b7
Naman <namansingla830@gmail.com>
2026-07-14 07:52:44 +0530

    add explanation and observation

---
+++ Linked_List/Problem_of_LL/12_starting_point_of_loop.js
@@ -0,0 +1,9 @@
+// Goal: Given a linked list that contains a cycle, return the node where the cycle begins. If there is no cycle, return null.
+
+/*
+Key Observations
+If there is no cycle, answer is null.
+First detect whether a cycle exists.
+Then find the first node where the cycle starts.
+We should solve it in O(N) time and O(1) space.
+*/

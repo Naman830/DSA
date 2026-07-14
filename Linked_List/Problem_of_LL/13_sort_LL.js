5dc955b392647596a70f4b8928ad454b662764d8
Naman <namansingla830@gmail.com>
2026-07-14 08:19:15 +0530

    add key observation

--- Linked_List/Problem_of_LL/13_sort_LL.js
+++ Linked_List/Problem_of_LL/13_sort_LL.js
@@ -1,5 +1,14 @@
 /*
 You are given the head of a singly linked list.
-
 Your task is to sort the linked list in ascending order and return the new head.
+
+Key Observations
+Arrays can be sorted easily because we have random access.
+Linked Lists don't allow random access.
+Algorithms like Quick Sort are not ideal for Linked Lists.
+Merge Sort is the best sorting algorithm for Linked Lists.
+
+Why?
+
+Because Merge Sort only needs sequential access.
 */

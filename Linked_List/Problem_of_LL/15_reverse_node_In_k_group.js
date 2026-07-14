6ee6b529db917ab95b9b3810f08da98df4c63188
Naman <namansingla830@gmail.com>
2026-07-14 09:03:59 +0530

    add explanation

---
+++ Linked_List/Problem_of_LL/15_reverse_node_In_k_group.js
@@ -0,0 +1,22 @@
+/*
+EXPLANATION
+You are given the head of a linked list and an integer k.
+Reverse every group of k nodes.
+If the last group contains fewer than k nodes, leave it as it is
+
+Input:
+1 → 2 → 3 → 4 → 5
+k = 2
+
+Output:
+2 → 1 → 4 → 3 → 5
+
+Explanation
+
+(1,2) -> reverse -> (2,1)
+
+(3,4) -> reverse -> (4,3)
+
+(5) -> less than k
+don't reverse
+*/

7fe1434164e6025854dbc107f7954d2ad78951ba
Naman <namansingla830@gmail.com>
2026-07-15 07:16:36 +0530

    add explanation of rotate a LL

---
+++ Linked_List/Problem_of_LL/16_Rotate_a_LL.js
@@ -0,0 +1,20 @@
+/*
+You are given the head of a linked list and an integer k
+Your task is to rotate the linked list to the right by k positions.
+
+When we rotate once to the right:
+The last node becomes the first node and Everything else shifts one position to the right.
+
+Input:
+1 → 2 → 3 → 4 → 5
+k = 2
+
+Rotate once:
+5 → 1 → 2 → 3 → 4
+
+Rotate twice:
+4 → 5 → 1 → 2 → 3
+
+Output:
+4 → 5 → 1 → 2 → 3
+*/

72c670cb72f30ddb7a134dbf8d077be9defd570e
Naman <namansingla830@gmail.com>
2026-07-16 10:00:37 +0530

    explanation of optimal approach

---
+++ Linked_List/Problem_of_LL/18_Optimal_copy_LL_with_random&nextPointers.js
@@ -0,0 +1,12 @@
+/*
+Instead of storing original → copied node mapping in a HashMap,
+we temporarily insert every copied node just after its original node.
+
+Original List
+1 → 2 → 3
+
+After inserting copied nodes
+1 → 1' → 2 → 2' → 3 → 3'
+
+Now every copied node is immediately after its original node.
+*/

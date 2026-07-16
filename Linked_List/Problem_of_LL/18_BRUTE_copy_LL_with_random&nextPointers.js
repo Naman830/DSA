2266409cb6a8c9040483ce932116f54f07c474f2
Naman <namansingla830@gmail.com>
2026-07-16 09:11:57 +0530

    add key observation

--- Linked_List/Problem_of_LL/18_BRUTE_copy_LL_with_random&nextPointers.js
+++ Linked_List/Problem_of_LL/18_BRUTE_copy_LL_with_random&nextPointers.js
@@ -7,4 +7,10 @@ random → points to any node in the list (or null).
 
 TASK: Create a deep copy of the linked list.
 Deep Copy: Create completely new nodes. The copied list should not share any node with the original list.
+
+Key Observations:
+Every original node must have a newly created node.
+next pointers are easy to copy.
+random pointers are difficult because they can point anywhere.
+We need a way to quickly find the copied version of any original node.
 */

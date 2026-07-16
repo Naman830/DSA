034e7ec57a535dec3faf67afc542b23332184a13
Naman <namansingla830@gmail.com>
2026-07-16 09:11:20 +0530

    add explanation of brute copy randowm pointer

---
+++ Linked_List/Problem_of_LL/18_BRUTE_copy_LL_with_random&nextPointers.js
@@ -0,0 +1,10 @@
+// HASHMAP SOLUTION
+
+/*
+EXPLANATION: You are given a linked list where each node contains:
+next → points to the next node.
+random → points to any node in the list (or null).
+
+TASK: Create a deep copy of the linked list.
+Deep Copy: Create completely new nodes. The copied list should not share any node with the original list.
+*/

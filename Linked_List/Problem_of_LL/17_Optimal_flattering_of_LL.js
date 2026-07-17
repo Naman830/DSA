b8ef4aa88696b02ee9b2872734f16d9f31b4d2e0
Naman <namansingla830@gmail.com>
2026-07-17 09:55:30 +0530

    merge pseudo code

--- Linked_List/Problem_of_LL/17_Optimal_flattering_of_LL.js
+++ Linked_List/Problem_of_LL/17_Optimal_flattering_of_LL.js
@@ -4,7 +4,6 @@ Since every bottom list is already sorted, we merge them just like Merge Sort.
 We recursively flatten from the right side.
 
 Pseudocode
-
 flatten(head)
 
 if head == null OR head.next == null
@@ -15,4 +14,15 @@ right = flatten(head.next)
 head = merge(head, right)
 
 return head
+
+Merge Pseudocode
+merge(list1, list2)
+
+dummy
+
+while both exist
+    smaller node becomes next
+attach remaining nodes
+
+return dummy.bottom
 */

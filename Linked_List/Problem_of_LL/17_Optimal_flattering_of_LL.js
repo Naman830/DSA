cf338641a809634c927815e4f1b4fddb3732ee14
Naman <namansingla830@gmail.com>
2026-07-17 09:56:23 +0530

    format it

--- Linked_List/Problem_of_LL/17_Optimal_flattering_of_LL.js
+++ Linked_List/Problem_of_LL/17_Optimal_flattering_of_LL.js
@@ -2,7 +2,9 @@
 Instead of sorting everything,
 Since every bottom list is already sorted, we merge them just like Merge Sort.
 We recursively flatten from the right side.
+*/
 
+/*
 Pseudocode
 flatten(head)
 
@@ -15,6 +17,9 @@ head = merge(head, right)
 
 return head
 
+*/
+
+/*
 Merge Pseudocode
 merge(list1, list2)
 

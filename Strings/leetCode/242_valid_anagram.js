e15f15a3debf4746d309e704514019dbe032589a
Naman <namansingla830@gmail.com>
2026-07-18 08:05:54 +0530

    add explanatiion

---
+++ Strings/leetCode/242_valid_anagram.js
@@ -0,0 +1,11 @@
+/*
+What is an Anagram?
+
+Two strings are anagrams if:
+
+They contain exactly the same characters
+Each character appears the same number of times
+The order does not matter.
+
+
+*/

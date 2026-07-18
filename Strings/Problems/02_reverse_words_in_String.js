be565808e20cc4987eb46dc6f7c58ca219ac7b6d
Naman <namansingla830@gmail.com>
2026-07-18 08:45:59 +0530

    add key observation

--- Strings/Problems/02_reverse_words_in_String.js
+++ Strings/Problems/02_reverse_words_in_String.js
@@ -9,6 +9,11 @@ Not
 tac god drib
 
 That would be reversing letters, which is a completely different problem.
-We didn't change the letters inside the words.
-We only changed the position of the words.
+
+KEY OBSERVATION
+A word is a group of characters separated by spaces.
+Reverse the order of words, not the letters.
+Ignore extra spaces.
+Final answer should have only one space between words.
+Don't worry about coding yet—first understand that you're simply rearranging whole words.
 */

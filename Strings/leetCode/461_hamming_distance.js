b8ed10b5d77c9cb9e27c1958e309c6279bac26d5
Naman <namansingla830@gmail.com>
2026-07-18 07:53:17 +0530

    add question explanation of hamming distance

---
+++ Strings/leetCode/461_hamming_distance.js
@@ -0,0 +1,12 @@
+// Brian Kernighan's Algorithm
+// Count the number of set bits (1s) in a binary number efficiently.
+/*
+Suppose you have a number.
+13
+
+Binary representation:
+1101
+
+There are 3 ones.
+Answer = 3
+*/

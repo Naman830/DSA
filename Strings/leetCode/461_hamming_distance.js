bf8d53c1411ac240789e0002abbd39f8eff874ef
Naman <namansingla830@gmail.com>
2026-07-18 07:55:21 +0530

    explanation of  Brian Kernighan's Algorithm

--- Strings/leetCode/461_hamming_distance.js
+++ Strings/leetCode/461_hamming_distance.js
@@ -10,3 +10,42 @@ Binary representation:
 There are 3 ones.
 Answer = 3
 */
+
+/*
+Brian Kernighan's Observation
+Instead of checking every bit,
+remove one set bit at a time.
+
+The trick is
+n & (n - 1)
+
+This removes the rightmost set bit.
+Why does it work?
+
+Let's understand carefully.
+
+Take
+n = 12
+
+Binary
+1100
+
+Subtract 1
+1100
+   -
+0001
+= 1011
+
+Now perform AND
+
+1100
+1011
+----
+100
+See what happened?
+
+1100
+↓
+1000
+The last 1 disappeared.
+*/

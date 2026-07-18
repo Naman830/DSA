b339a1fe74cd53c94a2c50bcc562067bf460fe3d
Naman <namansingla830@gmail.com>
2026-07-18 08:06:07 +0530

    add key observation

--- Strings/leetCode/242_valid_anagram.js
+++ Strings/leetCode/242_valid_anagram.js
@@ -7,5 +7,8 @@ They contain exactly the same characters
 Each character appears the same number of times
 The order does not matter.
 
-
+Key Observations
+Length must be the same.
+Every character count must match.
+Order doesn't matter.
 */

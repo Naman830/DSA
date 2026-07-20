aa13dcf3d336dd1da35113a0c986cd9ae46069a7
Naman <namansingla830@gmail.com>
2026-07-20 08:20:54 +0530

    add observation

---
+++ Strings/Problems/08_max_nesting_depth_of_parenthesis.js
@@ -0,0 +1,8 @@
+// Your task is to find the deepest level of nested parentheses.
+/*
+KEY OBSERVATION
+Every ( means we are going one level deeper.
+Every ) means we are coming one level back.
+We only care about the highest depth reached.
+Other characters don't matter.
+*/

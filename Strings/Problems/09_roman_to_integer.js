0638fc2ca4558e5c3f9c4a8ad3a01740fc740eb4
Naman <namansingla830@gmail.com>
2026-07-20 08:35:36 +0530

    add js code

--- Strings/Problems/09_roman_to_integer.js
+++ Strings/Problems/09_roman_to_integer.js
@@ -25,7 +25,6 @@ Create a map of Roman values
 answer = 0
 
 Loop through string
-
     current = value of current character
     next = value of next character
 
@@ -36,3 +35,50 @@ Loop through string
 
 Return answer
 */
+
+function romanToInteger(roman) {
+  // Store Roman numeral values
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  // Final answer
+  let result = 0;
+
+  // Traverse the string
+  for (let i = 0; i < roman.length; i++) {
+    // Current Roman numeral value
+    const current = values[roman[i]];
+
+    // Next Roman numeral value (0 if no next character)
+    const next = values[roman[i + 1]] || 0;
+
+    // If current value is smaller than next,
+    // it should be subtracted.
+    if (current < next) {
+      result -= current;
+    } else {
+      // Otherwise simply add it.
+      result += current;
+    }
+  }
+
+  return result;
+}
+
+// ------------------------
+// Test Cases
+// ------------------------
+console.log(romanToInteger("III")); // 3
+console.log(romanToInteger("LVIII")); // 58
+console.log(romanToInteger("MCMXCIV")); // 1994
+console.log(romanToInteger("IX")); // 9
+console.log(romanToInteger("XL")); // 40
+console.log(romanToInteger("CD")); // 400
+console.log(romanToInteger("MMXXVI")); // 2026

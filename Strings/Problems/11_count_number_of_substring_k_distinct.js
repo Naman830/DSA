a4473195deaecad23e548a3f4f5d0613558264a4
Naman <namansingla830@gmail.com>
2026-07-20 09:18:27 +0530

    add optimal js code

--- Strings/Problems/11_count_number_of_substring_k_distinct.js
+++ Strings/Problems/11_count_number_of_substring_k_distinct.js
@@ -54,3 +54,66 @@ console.log(countSubstringsBruteForce("abc", 2)); // 2
 console.log(countSubstringsBruteForce("pqpqs", 2)); // 7
 
 // TC: O(n²) && SC: O(1)
+
+// 2. Optimal Approach
+/*
+First Understand "At Most K"
+Instead of findinds
+
+Exactly K
+we first solve
+At Most K
+
+because Sliding Window handles this naturally.
+
+Pseudocode
+function atMost(K)
+
+left = 0
+for right
+    add current character
+
+    while distinct > K
+        remove left character
+        move left
+    
+    sanswer += right-left+1
+
+return answer
+
+Exactly(K) = AtMost(K) - AtMost(K-1)
+*/
+
+function countAtMostKDistinct(s, k) {
+  const freq = new Map();
+  let left = 0;
+  let count = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    freq.set(s[right], (freq.get(s[right]) || 0) + 1);
+    while (freq.size > k) {
+      freq.set(s[left], freq.get(s[left]) - 1);
+
+      if (freq.get(s[left]) === 0) {
+        freq.delete(s[left]);
+      }
+
+      left++;
+    }
+
+    // Count all valid substrings ending at 'right'
+    count += right - left + 1;
+  }
+
+  return count;
+}
+
+// Function to count substrings with exactly K distinct characters
+function countExactlyKDistinct(s, k) {
+  return countAtMostKDistinct(s, k) - countAtMostKDistinct(s, k - 1);
+}
+
+console.log(countExactlyKDistinct("abc", 2)); // 2
+console.log(countExactlyKDistinct("pqpqs", 2)); // 7
+console.log(countExactlyKDistinct("aa", 1)); // 3
+console.log(countExactlyKDistinct("aba", 2)); // 3

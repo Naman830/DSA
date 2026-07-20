0073a73e23cdcb313a1479f019e4b7cf459d5dd8
Naman <namansingla830@gmail.com>
2026-07-20 06:56:26 +0530

    add key observation

---
+++ Strings/Problems/04_longest_prefix_sum.js
@@ -0,0 +1,9 @@
+/*
+Key Observations (Short)
+
+Prefix always starts from the beginning of the string.
+Every string must contain that prefix.
+As soon as one character differs, stop.
+If there is no common first character, return "".
+The answer can never be longer than the shortest string.
+*/

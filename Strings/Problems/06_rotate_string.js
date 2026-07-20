9e32aca13cbe0285dc0f2b19a6128a9673d378a6
Naman <namansingla830@gmail.com>
2026-07-20 07:40:13 +0530

    add explanation

---
+++ Strings/Problems/06_rotate_string.js
@@ -0,0 +1,15 @@
+/*
+s = "abcde"
+goal = "cdeab"
+
+Rotations:
+abcde
+↓
+bcdea
+↓
+cdeab ✅
+
+Answer: true
+
+We have to check that if we rotate s some time until reach too goal s % length if not return false 
+*/

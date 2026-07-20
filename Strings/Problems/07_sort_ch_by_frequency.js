b217b9ad9236c66d1f0b99b347c831ac8d1ca7e7
Naman <namansingla830@gmail.com>
2026-07-20 07:55:17 +0530

    add key observation

---
+++ Strings/Problems/07_sort_ch_by_frequency.js
@@ -0,0 +1,14 @@
+/*
+Your task is to arrange the characters in decreasing order of how many times they appear.
+
+The character that appears the most should come first.
+Then the second most frequent character.
+Continue until all characters are placed.
+
+2. Key Observations (Short)
+Count how many times every character appears.
+Characters with higher frequency come first.
+Same frequency can appear in any order.
+Uppercase and lowercase are different.
+Spaces and symbols are also characters if present.
+*/

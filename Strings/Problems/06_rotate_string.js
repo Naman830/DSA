23a0f0335333ccabb333b3023a8ee02a7c275e0e
Naman <namansingla830@gmail.com>
2026-07-20 07:43:07 +0530

    add key observation

--- Strings/Problems/06_rotate_string.js
+++ Strings/Problems/06_rotate_string.js
@@ -12,4 +12,10 @@ cdeab ✅
 Answer: true
 
 We have to check that if we rotate s some time until reach too goal s % length if not return false 
+
+Key Observations
+Both strings must have the same length.
+Rotation never changes characters.
+Only changes their positions.
+Every possible rotation should be checked.
 */

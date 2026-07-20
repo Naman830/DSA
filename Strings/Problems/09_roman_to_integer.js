7393ab8dc60cc87e77ddbb09f0568eaa17fce927
Naman <namansingla830@gmail.com>
2026-07-20 08:31:32 +0530

    add key observation'

---
+++ Strings/Problems/09_roman_to_integer.js
@@ -0,0 +1,8 @@
+/*
+Key Observations (Interview)
+Convert every Roman character into its numeric value.
+Normally add every value.
+If current value is smaller than next value, subtract it.
+Otherwise, add it.
+Traverse the string once.
+*/

afafff7fc4bb5e3ed484bee7258d3755e58fd78f
Naman <namansingla830@gmail.com>
2026-07-20 08:45:04 +0530

    explanation of atoi

---
+++ Strings/Problems/10_string_to_integer[ATOI].js
@@ -0,0 +1,25 @@
+/*
+1. Easy Explanation
+
+Imagine someone gives you a string like:
+"   -42"
+
+Your job is to convert it into an integer.
+
+But the string may contain:
+Spaces before the number
+A + or - sign
+
+Numbers
+
+Extra characters after the number
+You must read only the valid part and ignore the rest.
+
+For example
+"42"          → 42
+"   -42"      → -42
+"4193abc"     → 4193
+"words123"    → 0
+
+This is exactly what the C/C++ function atoi() does.
+*/

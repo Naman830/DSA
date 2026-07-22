2ba06281dd7e2aa24461038434beaeca35815d79
Naman <namansingla830@gmail.com>
2026-07-22 08:44:06 +0530

    add tc and sc

--- Array/matrix/set_matrix_zeros/02_optimal.js
+++ Array/matrix/set_matrix_zeros/02_optimal.js
@@ -6,3 +6,12 @@ First column stores which rows should become zero.
 
 Since matrix[0][0] belongs to both the first row and first column, use an extra variable (firstColumnHasZero) to remember whether the first column needs to be zeroed.
 */
+
+/*
+| Case    | Time   | Space |
+| ------- | ------ | ----- |
+| Best    | O(M×N) | O(1)  |
+| Average | O(M×N) | O(1)  |
+| Worst   | O(M×N) | O(1)  |
+
+*/

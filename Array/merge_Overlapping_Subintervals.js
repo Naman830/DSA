9b4e8c9b399305e6899b3a41800aeadc263ff262
Naman <namansingla830@gmail.com>
2026-07-22 08:59:50 +0530

    remove uncessary line

--- Array/merge_Overlapping_Subintervals.js
+++ Array/merge_Overlapping_Subintervals.js
@@ -24,32 +24,6 @@ Result:
   [15,18]
 ]
 
-Input
-Array of intervals:
-[[start1,end1],[start2,end2],...]
-
-Output
-Merged intervals.
-Hidden Observation
-Two intervals overlap if:
-
-currentStart <= previousEnd
-
-Example:
-[1,3]
-[2,6]
-
-2 <= 3
-
-=> Overlap exists
-
-Merge:
-start = min(1,2) = 1
-end   = max(3,6) = 6
-
-=> [1,6]
-2. Intuition
-
 Imagine drawing intervals on a line:
 [1-----3]
      [2--------6]
@@ -57,18 +31,15 @@ Both touch each other.
 
 Instead of keeping both:
 [1-------------6]
-
 */
 
 // 1. Brute Force Solution
 // For every interval: Check all remaining intervals.
 // If overlap exists: Merge them. [Repeat until no merges are possible.]
-
 // TC: O(n²) & O(n)
 
 /*
 Pseudocode
-
 for every interval i
 
     for every interval j
@@ -77,6 +48,7 @@ for every interval i
 
             merge
 */
+
 function mergeIntervalsBrute(intervals) {
   // Sort first so overlapping intervals come together
   intervals.sort((a, b) => a[0] - b[0]);

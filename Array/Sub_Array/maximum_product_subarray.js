8cb00227466c9757857809d1ddd3c800a0ee4c56
Naman <namansingla830@gmail.com>
2026-07-25 08:18:05 +0530

    add explanation

---
+++ Array/Sub_Array/maximum_product_subarray.js
@@ -0,0 +1,16 @@
+/*
+Your task is to find the contiguous subarray (elements next to each other) that has the maximum product, and return that product.
+
+Example
+Input: [2,3,-2,4]
+
+Possible Products:
+[2] = 2
+[2,3] = 6 ✅
+[2,3,-2] = -12
+[3,-2,4] = -24
+[4] = 4
+
+Output:
+6
+*/

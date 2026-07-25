4585eddbd33788d8eacf9498d996d8b0dc38bf73
Naman <namansingla830@gmail.com>
2026-07-25 10:42:45 +0530

    add example

---
+++ Array/reverse_pairs.js
@@ -0,0 +1,18 @@
+/*
+Explanation
+You are given an array nums.
+A reverse pair is a pair of indices (i, j) such that:
+
+i < j
+nums[i] > 2 * nums[j]
+
+Return the total number of reverse pairs.
+Example
+Input: [1,3,2,3,1]
+
+Pairs:
+(3,1) → 3 > 2×1 ✅
+(3,1) → another 3 > 2×1 ✅
+
+Output: 2
+*/

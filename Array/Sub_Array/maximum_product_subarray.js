a931dec3cf13b90b23b779c8d0f31b39d66d4dc6
Naman <namansingla830@gmail.com>
2026-07-25 08:18:41 +0530

    add key observation

--- Array/Sub_Array/maximum_product_subarray.js
+++ Array/Sub_Array/maximum_product_subarray.js
@@ -13,4 +13,12 @@ Possible Products:
 
 Output:
 6
+
+Key Observations (Short)
+Product changes differently than sum.
+A negative × negative = positive.
+A zero breaks the product, so we start again after zero.
+At every index we need:
+Maximum product ending here.
+Minimum product ending here (because it may become maximum after multiplying by a negative).
 */

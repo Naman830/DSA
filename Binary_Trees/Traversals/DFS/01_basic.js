9160785d1d89cbb527ba86d81db26712ba479507
Naman <namansingla830@gmail.com>
2026-08-01 09:20:14 +0530

    add basics

---
+++ Binary_Trees/Traversals/DFS/01_basic.js
@@ -0,0 +1,12 @@
+// Binary Tree DFS (Depth First Search)
+// Go as deep as possible into one branch before moving to another branch.
+
+// There are three types
+// 1. Preorder Traversal        [Root → Left → Right]
+// LOOK Word Pre [Root first]
+
+// 2. Inorder Traversal         [Left → Root → Right]
+// LOOK Word In [Root in center]
+
+// 3. Postorder Traversal       [Left → Right → Root]
+// LOOK Word Post [Root in Last]

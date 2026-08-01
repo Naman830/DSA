6dd8fe7bea0d61a6408374e6f3c4512ff21032f8
Naman <namansingla830@gmail.com>
2026-08-01 09:23:26 +0530

    explain bfs

---
+++ Binary_Trees/Traversals/BFS/01_basic.js
@@ -0,0 +1,16 @@
+/*
+Binary Tree Traversal (BFS - Breadth First Search)
+
+Easy Definition:
+BFS (Breadth First Search) visits a binary tree level by level, starting from the root. It first visits every node of Level 1, then Level 2, then Level 3, and so on.
+*/
+
+/*
+          1      --- level-1
+        /   \    
+       2     3   --- level-2
+      / \   / \
+     4   5 6   7 --- level-3
+
+1 → 2 → 3 → 4 → 5 → 6 → 7
+*/

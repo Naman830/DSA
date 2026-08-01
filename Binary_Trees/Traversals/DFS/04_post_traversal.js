b1d9fd86fc03358c895af1e0f830b59c68fc9a22
Naman <namansingla830@gmail.com>
2026-08-01 09:44:48 +0530

    add js code

---
+++ Binary_Trees/Traversals/DFS/04_post_traversal.js
@@ -0,0 +1,58 @@
+/*
+In Postorder Traversal, we visit:
+
+Left subtree
+Right subtree
+Root node
+
+The root is always visited last.
+*/
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function postorderTraversal(root) {
+  const result = [];
+
+  function dfs(node) {
+    // Base case
+    if (node === null) return;
+
+    // Visit Left
+    dfs(node.left);
+
+    // Visit Right
+    dfs(node.right);
+
+    // Visit Root
+    result.push(node.val);
+  }
+
+  dfs(root);
+
+  return result;
+}
+
+/*
+        1
+       / \
+      2   3
+     / \
+    4   5
+*/
+
+const root = new TreeNode(1);
+
+root.left = new TreeNode(2);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+root.right = new TreeNode(3);
+
+// Test
+console.log(postorderTraversal(root)); // [4, 5, 2, 3, 1]

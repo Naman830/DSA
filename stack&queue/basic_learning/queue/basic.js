47d41ed5fcf21ae1a7e2552709b7e3f65b569020
Naman <namansingla830@gmail.com>
2026-08-01 10:42:35 +0530

    meaning of queue

---
+++ stack&queue/basic_learning/queue/basic.js
@@ -0,0 +1,15 @@
+/*
+Queue
+A Queue is a linear data structure that follows the FIFO principle.
+
+FIFO = First In, First Out
+The element inserted first is removed first.
+
+Ticket Counter 🎫
+A → B → C  →  D
+^             ^
+Front       Rear
+
+Front = Where people leave.
+Rear = Where new people join.
+*/

296c0c617b4bcdfe6dc08ac62537834ec0635fb5
Naman <namansingla830@gmail.com>
2026-08-01 10:36:16 +0530

    meaning of stack

---
+++ stack&queue/basic_learning/stack/01_basic.js
@@ -0,0 +1,20 @@
+/*
+Stack
+
+A Stack is a linear data structure that follows the LIFO principle.
+LIFO = Last In, First Out
+
+    ┌─────┐  ← 4 (Top)
+    │  4  │
+    ├─────┤
+    │  3  │
+    ├─────┤
+    │  2  │
+    ├─────┤
+    │  1  │
+    └─────┘
+
+If you want to remove a plate, you must remove the top one first.
+Remove order:
+4 → 3 → 2 → 1
+*/

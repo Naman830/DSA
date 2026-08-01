3d8fd170b312bcac1ac230f26fb2a40665a48160
Naman <namansingla830@gmail.com>
2026-08-01 10:38:08 +0530

    operations in stack

--- stack&queue/basic_learning/stack/01_basic.js
+++ stack&queue/basic_learning/stack/01_basic.js
@@ -18,3 +18,12 @@ If you want to remove a plate, you must remove the top one first.
 Remove order:
 4 → 3 → 2 → 1
 */
+
+/*
+Operations in Stack
+
+1. Push (Insert)        [Adds an element on the top.]
+2. Pop (Remove)         [Removes the top element.]
+3. Peek / Top           [Returns the top element without removing it.]
+4. isEmpty()            [Checks whether the stack is empty.]
+*/

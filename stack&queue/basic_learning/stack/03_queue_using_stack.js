456b5b0c30c5be5fab5fb2d33b6ad6f81653307d
Naman <namansingla830@gmail.com>
2026-08-03 09:04:11 +0530

    js code enque and deque

---
+++ stack&queue/basic_learning/stack/03_queue_using_stack.js
@@ -0,0 +1,30 @@
+/*
+Maintain two stacks:
+
+stack1 → Used for inserting elements.
+stack2 → Used for removing elements.
+*/
+
+class QueueUsingStack {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
+  }
+
+  enqueue(value) {
+    this.stack1.push(value);
+  }
+
+  dequeue() {
+    if (this.stack2.length === 0) {
+      while (this.stack1.length > 0) {
+        this.stack2.push(this.stack1.pop());
+      }
+    }
+
+    if (this.stack2.length === 0) {
+      return "Queue is empty";
+    }
+    return this.stack2.pop();
+  }
+}

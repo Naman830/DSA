280632283985925c1fbd9c44a157948a83c4e009
Naman <namansingla830@gmail.com>
2026-08-03 09:38:03 +0530

    add js code

---
+++ stack&queue/basic_learning/stack/queue_using_stack/02_approach.js
@@ -0,0 +1,55 @@
+/*
+Whenever we want to dequeue, we keep popping elements recursively until only one element remains.
+
+That last remaining element is actually the front of the queue.
+
+After removing it, we push back every previous element while returning from recursion.
+*/
+class Queue {
+  constructor() {
+    this.stack = [];
+  }
+
+  // Add element to queue
+  enqueue(value) {
+    this.stack.push(value);
+  }
+
+  dequeue() {
+    if (this.stack.length === 0) {
+      return "QUEUE IS EMPTY";
+    }
+
+    if (this.stack.length === 1) {
+      return this.stack.pop();
+    }
+
+    const top = this.stack.pop();
+    const front = this.dequeue();
+
+    this.stack.push(top);
+    return top;
+  }
+
+  // Show queue
+  display() {
+    console.log([...this.stack].reverse());
+  }
+}
+
+const q = new Queue();
+
+q.enqueue(10);
+q.enqueue(20);
+q.enqueue(30);
+q.enqueue(40);
+
+q.display();
+
+console.log(q.dequeue()); // 10
+
+q.display();
+
+console.log(q.dequeue()); // 20
+
+q.display();

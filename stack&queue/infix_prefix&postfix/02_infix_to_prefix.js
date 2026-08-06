320849640b1b9071339b1c54bdc85c857aab88d9
Naman <namansingla830@gmail.com>
2026-08-06 09:24:28 +0530

    add algorith

--- stack&queue/infix_prefix&postfix/02_infix_to_prefix.js
+++ stack&queue/infix_prefix&postfix/02_infix_to_prefix.js
@@ -13,3 +13,13 @@ Parentheses have the highest priority.
 Prefix scans from right → left (or we can reverse the infix and use postfix logic).
 We use a stack to temporarily store operators.
 */
+
+/*
+Algorithm
+
+1. Reverse infix
+2. Swap brackets ( and ) 
+3. Convert to postfix
+4. Reverse postfix
+Done
+*/

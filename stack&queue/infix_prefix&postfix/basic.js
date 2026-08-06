5f53d5f9c98e3a209c195b702597f48ffefaa7bf
Naman <namansingla830@gmail.com>
2026-08-06 08:29:29 +0530

    add operands

---
+++ stack&queue/infix_prefix&postfix/basic.js
@@ -0,0 +1,15 @@
+/*
+These are three different ways to write mathematical expressions.
+
+
+What is an Operand?
+An operand is the value or variable on which an operation is performed.
+
+Example	   Operands
+5 + 3	    5, 3
+10 - 4	    10, 4
+A * B	    A, B
+x / y	    x, y
+
+Numbers or variables = Operands
+*/

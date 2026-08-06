364ac7e06660076273ad18b6e9da44a15c31e2df
Naman <namansingla830@gmail.com>
2026-08-06 08:56:52 +0530

    add js basic condition code

--- stack&queue/infix_prefix&postfix/01_infix_to_postfix.js
+++ stack&queue/infix_prefix&postfix/01_infix_to_postfix.js
@@ -1,7 +1,6 @@
 /*
 a + b * ( c ^ d - c )
 
-
 | Step | Scan | Action                                               | Stack        | Postfix     |
 | ---- | ---- | ---------------------------------------------------- | ------------ | ----------- |
 | 1    | `a`  | Operand → Add to postfix                             |              | `a`         |
@@ -17,5 +16,31 @@ a + b * ( c ^ d - c )
 | 11   | `)`  | Pop until `(`                                        | `+  *`       | `abcd^c-`   |
 | 12   | End  | Pop `*`                                              | `+`          | `abcd^c-*`  |
 | 13   | End  | Pop `+`                                              |              | `abcd^c-*+` |
-
 */
+
+function precedence(ch) {
+  if (ch === "^") return 3;
+  if (ch === "*" || ch === "/") return 2;
+  if (ch === "+" || ch === "-") return 1;
+  return 0;
+}
+
+function infixToPostix(expression) {
+  const stack = [];
+  let postfix = "";
+
+  for (const ch of expression) {
+    if (ch === " ") continue;
+
+    if ((ch >= "a" && ch <= "z") || (ch >= "A" && ch <= "Z")) {
+      postfix += ch;
+    } else if (ch === "(") {
+      stack.push(ch);
+    } else if (ch === ")") {
+      while (stack[stack.length - 1] !== "(") {
+        postfix += stack.pop;
+      }
+      stack.pop();
+    }
+  }
+}

d29dd7eed5da521e9c135dfff33290b7d8c8ea37
Naman <namansingla830@gmail.com>
2026-08-07 09:59:47 +0530

    explain matrix method using digrams

--- Graph/Learning/02_graph_representation.js
+++ Graph/Learning/02_graph_representation.js
@@ -5,3 +5,24 @@ There are 2 common ways to represent a graph:
 1. Adjacency Matrix
 2. Adjacency List ✅ (Most commonly used)
 */
+
+/*
+1. Adjacency Matrix
+Create a 2D array (matrix) of size V × V.
+
+matrix[i][j] = 1 → Edge exists
+matrix[i][j] = 0 → No edge
+
+Graph          Edge
+0 ----- 1 ---> 0-1
+|       | ---> 0-2
+|       | ---> 1-3
+2 ----- 3 ---> 2-3
+
+     Matrix
+     0 1 2 3
+0 -> 0 1 1 0
+1 -> 1 0 0 1
+2 -> 1 0 0 1
+3 -> 0 1 1 0
+*/

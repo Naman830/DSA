aec6f9c18c723da56f9cd3847f2466bd698f4a8b
Naman <namansingla830@gmail.com>
2026-08-07 09:56:33 +0530

    add meaning of graph representation

---
+++ Graph/Learning/02_graph_representation.js
@@ -0,0 +1,7 @@
+/*
+A graph representation is the way we store a graph in memory so that we can easily perform operations like traversal (BFS, DFS), finding paths, etc.
+
+There are 2 common ways to represent a graph:
+1. Adjacency Matrix
+2. Adjacency List ✅ (Most commonly used)
+*/

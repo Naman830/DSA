1be7d60e461ca73106d360f5f8a8de5c57e83f87
Naman <namansingla830@gmail.com>
2026-08-07 09:18:30 +0530

    add meaning of node and vertices

---
+++ Graph/Learning/introduction_to_graphs.js
@@ -0,0 +1,14 @@
+/*
+A Graph is a non-linear data structure that represents connections (relationships) between different objects.
+
+The objects are called Vertices (Nodes).
+The connections between them are called Edges.
+
+Alice -------- Bob
+  |              |
+  |              |
+Charlie ------ David
+
+Alice, Bob, Charlie, David → Vertices (Nodes)
+Friendship between them → Edges
+*/

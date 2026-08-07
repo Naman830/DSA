d448949ac585861cf4f99811c4741adca0749c98
Naman <namansingla830@gmail.com>
2026-08-07 09:23:15 +0530

    add meaning of undirected graph

--- Graph/Learning/introduction_to_graphs.js
+++ Graph/Learning/introduction_to_graphs.js
@@ -4,11 +4,32 @@ A Graph is a non-linear data structure that represents connections (relationship
 The objects are called Vertices (Nodes).
 The connections between them are called Edges.
 
-Alice -------- Bob
-  |              |
+Alice -------- Bob ---> this is NODE/VERTEX
+  |              | ---> this is edge
   |              |
 Charlie ------ David
 
 Alice, Bob, Charlie, David → Vertices (Nodes)
 Friendship between them → Edges
+*/
+
+/*
+This is undirected graph Edges work in both directions.
+      A
+     / \ 
+    B---C
+     \
+      D
+
+Vertex (Node) = A, B, C, D
+
+Degree = Number of edges connected to a vertex.
+A → 2
+B → 3
+C → 2
+D → 1
+
+In undirected graph it is called undirected graph degree
+Property always 
+Total Degree = 2 x edges [every word contain two edges]
 */

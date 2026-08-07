217a422a02e53ccd09f063ad7a09dc3da0e0d34a
Naman <namansingla830@gmail.com>
2026-08-07 10:29:13 +0530

    explain connect component using dugram

---
+++ Graph/Learning/02_connected_components.js
@@ -0,0 +1,27 @@
+/*
+A Connected Component is a group of vertices (nodes) in an undirected graph where:
+
+Every node can reach every other node in the group.
+There is no connection to nodes in another group.
+
+Think of it as separate islands inside one graph.
+
+Example:-
+
+      0
+     / \
+    1---2
+
+      3
+      |
+      4
+
+      5
+
+Components are:
+{0,1,2}
+{3,4}
+{5}
+
+Answer = 3 Connected Components
+*/

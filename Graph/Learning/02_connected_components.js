ac17901730c058f98c544abbee2bc90d733d0ed0
Naman <namansingla830@gmail.com>
2026-08-07 10:30:36 +0530

    add a way to solve the question

--- Graph/Learning/02_connected_components.js
+++ Graph/Learning/02_connected_components.js
@@ -32,4 +32,14 @@ Works for Undirected Graphs.
 Every node belongs to exactly one connected component.
 If the graph has only one connected component, the graph is Connected.
 If it has more than one, it is Disconnected.
+
+Simple Algorithm or way
+1. Keep a visited array.
+2. Loop through every node.
+
+3. If a node is not visited:
+    Start DFS or BFS from it.
+    Mark every reachable node.
+    Increase the component count.
+Every new DFS/BFS means we found a new connected component.
 */

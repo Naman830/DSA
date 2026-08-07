01936755f4cbd4055ac77536d1de56489fa987b5
Naman <namansingla830@gmail.com>
2026-08-07 10:07:38 +0530

    add js code

--- Graph/Learning/graph_representation/02_list_method.js
+++ Graph/Learning/graph_representation/02_list_method.js
@@ -15,3 +15,23 @@ Adjacent list
   2 → [0,3]
   3 → [1,2]
 */
+
+const V = 4;
+
+// Create adjacency list
+const graph = Array.from({ length: V }, () => []);
+
+// Add edges
+graph[0].push(1);
+graph[1].push(0);
+
+graph[0].push(2);
+graph[2].push(0);
+
+graph[1].push(3);
+graph[3].push(1);
+
+graph[2].push(3);
+graph[3].push(2);
+
+console.log(graph);

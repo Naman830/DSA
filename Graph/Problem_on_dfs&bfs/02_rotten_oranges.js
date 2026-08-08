0e5dbdd3921b39f0da5a95ced7837aa355ad3b97
Naman <namansingla830@gmail.com>
2026-08-08 10:37:48 +0530

    add all rotten orange in queeue

--- Graph/Problem_on_dfs&bfs/02_rotten_oranges.js
+++ Graph/Problem_on_dfs&bfs/02_rotten_oranges.js
@@ -48,3 +48,58 @@ Why BFS?
 Because BFS explores nodes based on their [distance] from the starting points.
 Here: Distance = Time
 */
+
+function orangesRotting(grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  const queue = [];
+  let fresh = 0;
+
+  // Add all rotten oranges to the queue.
+  // Also count the total fresh oranges.
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (grid[r][c] === 2) {
+        queue.push([r, c]);
+      } else if (grid[r][c] === 1) {
+        fresh++;
+      }
+    }
+  }
+
+  let minute = 0;
+  let front = 0;
+
+  // Four possible directions: up, down, left, right
+  const direction = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+}
+
+// Test Case 1
+console.log(
+  orangesRotting([
+    [2, 1, 1],
+    [1, 1, 0],
+    [0, 1, 1],
+  ]),
+);
+// Output: 4
+
+// Test Case 2
+console.log(
+  orangesRotting([
+    [2, 1, 1],
+    [0, 1, 1],
+    [1, 0, 1],
+  ]),
+);
+// Output: -1
+
+// Test Case 3
+console.log(orangesRotting([[0, 2]]));
+// Output: 0

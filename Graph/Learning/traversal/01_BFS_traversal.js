6bb3ce3fffe380ef416262df11ab7d7a3c2c3ad5
Naman <namansingla830@gmail.com>
2026-08-08 09:23:24 +0530

    traversal technique

--- Graph/Learning/traversal/01_BFS_traversal.js
+++ Graph/Learning/traversal/01_BFS_traversal.js
@@ -13,3 +13,13 @@ BFS visits a graph level by level. It first visits the starting node, then all i
 
 BFS = 0 → 1 → 2 → 3 → 4 → 5
 */
+
+/*
+How traversal work
+
+1. Put starting node in queue [It follow FIFO rule]
+2. Remove front node
+3. Visit its unvisited neighbors [It mark down that this number is visited]
+4. Add those neighbors to queue
+5. Repeat until queue is empty
+*/

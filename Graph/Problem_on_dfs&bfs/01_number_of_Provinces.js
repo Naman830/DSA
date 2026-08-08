d876a7868c85afcfbd603e1928a488dea94bd314
Naman <namansingla830@gmail.com>
2026-08-08 10:00:43 +0530

    explanation of provinces

---
+++ Graph/Problem_on_dfs&bfs/01_number_of_Provinces.js
@@ -0,0 +1,18 @@
+/*
+EXPLANATION:-
+
+You are given n cities and an n × n matrix isConnected.
+
+1. isConnected[i][j] = 1 → city i and city j are directly connected.
+2. isConnected[i][j] = 0 → they are not directly connected.
+
+A province is a group of cities connected directly or indirectly.
+
+example:-
+1 --- 2      3 --- 4
+      |
+      5
+
+Province 1 → {1, 2, 5}
+Province 2 → {3, 4}
+*/

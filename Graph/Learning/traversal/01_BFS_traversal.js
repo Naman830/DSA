aa249ed4595646f40fb3cbe3cfc04ae29c100735
Naman <namansingla830@gmail.com>
2026-08-08 09:22:14 +0530

    explain bfs

---
+++ Graph/Learning/traversal/01_BFS_traversal.js
@@ -0,0 +1,15 @@
+/*
+BFS = Breadth First Search
+
+BFS visits a graph level by level. It first visits the starting node, then all its direct neighbors, then their neighbors, and so on.
+*/
+
+/*
+        0   ---> Level 0 = 0
+       / \
+      1   2 ----> Level 1 = 1, 2
+     / \   \
+    3   4   5 ---> Level 2 = 3, 4, 5
+
+BFS = 0 → 1 → 2 → 3 → 4 → 5
+*/

f0a96d729888d1f0786e0adb20e47561f124f2b9
Naman <namansingla830@gmail.com>
2026-08-14 10:15:06 +0530

    add monotonic stack meaning

---
+++ stack&queue/monotonic_stack&queue/01_next_greater_element.js
@@ -0,0 +1,12 @@
+/*
+Monotonic simply means:
+Something keeps moving in one direction — increasing or decreasing — without breaking the order.
+
+What is a Monotonic Stack?
+A monotonic stack is just a normal stack with one extra rule:
+| Type                       | Stack order   |
+| -------------------------- | ------------- |
+| Monotonic Increasing Stack | Small → Large |
+| Monotonic Decreasing Stack | Large → Small |
+
+*/

62ccd2e51ccfcc736854e265bfd41d71a9c35cd7
Naman <namansingla830@gmail.com>
2026-08-14 08:44:55 +0530

    add example

---
+++ stack&queue/infix_prefix&postfix/04_prefix_to_infix.js
@@ -0,0 +1,17 @@
+/*
+Exaplanation:-
+Prefix → Infix = Scan right to left + Stack [similar as postfix to infix]
+
+Prefix: 
+Infix:   (A + B) * C
+
+Example:-
+* + A B C
+| Step | Character | Stack        | Action             |
+| ---- | --------- | ------------ | ------------------ |
+| 1    | `C`       | `[C]`        | Operand → push     |
+| 2    | `B`       | `[C, B]`     | Operand → push     |
+| 3    | `A`       | `[C, B, A]`  | Operand → push     |
+| 4    | `+`       | `[C, (A+B)]` | Pop A, B → `(A+B)` |
+| 5    | `*`       | `[(A+B)*C]`  | Pop `(A+B)`, C     |
+*/

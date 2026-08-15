df1f024e4886de7c396c940cda42357a65c22c0e
Naman <namansingla830@gmail.com>
2026-08-15 13:30:30 +0530

    eexplain what is trappig the rain water

---
+++ stack&queue/monotonic_stack&queue/05_trapping_raiwater/01_problem_understanding.js
@@ -0,0 +1,25 @@
+/*
+Explanation:-
+
+You are given an array where each number represents the height of a vertical bar.
+height = [4, 2, 0, 3, 2, 5]
+
+5                 █
+4   █             █
+3   █       █     █
+2   █   █   █ █   █
+1   █   █   █ █   █
+    ─────────────────
+    4   2   0 3 2 5
+
+Now imagine rain falls from above.
+Water can get trapped between taller bars.
+
+5                 █
+4   █~~~~~~~~~~~~~█
+3   █~~~~~~~█~~~~~█
+2   █~~~█~~~█~█~~~█
+1   █~~~█~~~█~█~~~█
+    ─────────────────
+The ~ represents trapped water.
+*/

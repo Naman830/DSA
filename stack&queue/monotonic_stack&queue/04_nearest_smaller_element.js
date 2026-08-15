a21b4326d882de243e9d996f4810442e90cdf71f
Naman <namansingla830@gmail.com>
2026-08-15 12:26:59 +0530

    explaination

---
+++ stack&queue/monotonic_stack&queue/04_nearest_smaller_element.js
@@ -0,0 +1,14 @@
+/*
+The Next Smaller Element (NSE) problem asks us to find, for every element, the first element to its right that is smaller than it.
+
+arr = [4, 8, 5, 2, 25]
+
+Next Smaller:
+4  → 2
+8  → 5
+5  → 2
+2  → -1
+25 → -1
+
+Answer = [2, 5, 2, -1, -1] 
+*/

35a4d16d535d3a900c2cc1de7b0528f3f4204b7c
Naman <namansingla830@gmail.com>
2026-08-15 11:59:40 +0530

    add circular way formula

--- stack&queue/monotonic_stack&queue/03_II_next_greater_element.js
+++ stack&queue/monotonic_stack&queue/03_II_next_greater_element.js
@@ -17,4 +17,18 @@ Every element can potentially look at elements after the last index.
 If no greater element exists → return -1.
 We can use a monotonic decreasing stack.
 To simulate the circular array, we process the array twice.
+
+======================================================================
+======================================================================
+
+Formula for circular way checking:- (index + 1) % n
+
+Example:
+[1, 2, 1]
+       ↑
+       index 2
+next index = (2 + 1) % 3 = 0
 */
+
+// 1. Brute Force
+// For every element, keep moving right and check elements one by one.

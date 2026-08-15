4397cc552f7327de8360a3839febe08b0b9d99f2
Naman <namansingla830@gmail.com>
2026-08-15 11:00:49 +0530

    add log

--- stack&queue/monotonic_stack&queue/01_next_greater_element.js
+++ stack&queue/monotonic_stack&queue/01_next_greater_element.js
@@ -89,6 +89,7 @@ console.log(nextGreaterElementOptimal([1, 3, 2, 4]));
 console.log(nextGreaterElementOptimal([5, 4, 3, 2, 1]));
 // [-1, -1, -1, -1, -1]
 
+console.log(nextGreaterElementOptimal([4, 12, 5, 3, 1, 2, 5, 3, 1, 2, 4, 6]));
 /*
 | Case    | Time | Space |
 | ------- | ---: | ----: |

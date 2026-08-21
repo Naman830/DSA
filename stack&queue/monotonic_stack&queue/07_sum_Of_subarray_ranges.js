f668374df1531a1aeb7014fe316233469089c73e
Naman <namansingla830@gmail.com>
2026-08-21 14:01:24 +0530

    add js code and time complexity for brute force

--- stack&queue/monotonic_stack&queue/07_sum_Of_subarray_ranges.js
+++ stack&queue/monotonic_stack&queue/07_sum_Of_subarray_ranges.js
@@ -17,3 +17,33 @@ Answer = 0 + 0 + 0 + 1 + 1 + 2 = 4
 The key idea is:
 Sum of ranges = Sum of all subarray maximums − Sum of all subarray minimums.
 */
+
+/*
+1. Brute Force
+Generate every subarray and keep track of its minimum and maximum.
+
+| Case    |  Time | Space |
+| ------- | ----: | ----: |
+| Best    | O(n²) |  O(1) |
+| Average | O(n²) |  O(1) |
+| Worst   | O(n²) |  O(1) |
+*/
+
+function subArrayRanges(nums) {
+  let total = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let min = nums[i];
+    let max = nums[i];
+
+    for (let j = i + 1; j < nums.length; j++) {
+      min = Math.min(min, nums[j]);
+      max = Math.max(max, nums[j]);
+
+      total += max - min;
+    }
+  }
+  return total;
+}
+
+console.log(subArrayRanges([1, 2, 3])); // 4
+console.log(subArrayRanges([1, 3, 2])); // 5

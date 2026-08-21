f12743d34990eea6c686cf35477ca112051d5b1b
Naman <namansingla830@gmail.com>
2026-08-21 09:29:38 +0530

    add js code

--- stack&queue/monotonic_stack&queue/06_asteriod_collision.js
+++ stack&queue/monotonic_stack&queue/06_asteriod_collision.js
@@ -30,3 +30,58 @@ For every asteroid:-
 1. If there is no possible collision, simply push it.
 2. If there is a collision compare their sizes.
 */
+
+function asteroidCollision(asteroids) {
+  const stack = [];
+
+  for (const asteroid of asteroids) {
+    let alive = true;
+
+    // Collision is possible only when:
+    // stack top is moving right (+)
+    // current asteroid is moving left (-)
+    while (
+      alive &&
+      stack.length > 0 &&
+      stack[stack.length - 1] > 0 &&
+      asteroid < 0
+    ) {
+      const top = stack[stack.length - 1];
+
+      if (top < Math.abs(asteroid)) {
+        // Stack asteroid is smaller -> it explodes
+        stack.pop();
+      } else if (top === Math.abs(asteroid)) {
+        // Both have same size -> both explode
+        stack.pop();
+        alive = false;
+      } else {
+        // Current asteroid is smaller -> it explodes
+        alive = false;
+      }
+    }
+
+    // If current asteroid survived, add it to stack
+    if (alive) {
+      stack.push(asteroid);
+    }
+  }
+
+  return stack;
+}
+
+// Test cases
+console.log(asteroidCollision([5, 10, -5]));
+// [5, 10]
+
+console.log(asteroidCollision([8, -8]));
+// []
+
+console.log(asteroidCollision([10, 2, -5]));
+// [10]
+
+console.log(asteroidCollision([-2, -1, 1, 2]));
+// [-2, -1, 1, 2]
+
+console.log(asteroidCollision([5, 10, -15]));
+// []

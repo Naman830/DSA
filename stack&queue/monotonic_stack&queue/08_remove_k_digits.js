a5904bf733eb9d8e8d138c17a19f7f5f8772530a
Naman <namansingla830@gmail.com>
2026-08-21 14:27:04 +0530

    add explanation of remove k digits

---
+++ stack&queue/monotonic_stack&queue/08_remove_k_digits.js
@@ -0,0 +1,15 @@
+/*
+You are given a number as a string and an integer k.
+You must remove exactly k digits so that the remaining number is as small as possible.
+
+Example:
+num = "1432219"
+k = 3
+
+Remove 4, 3, and 2:
+1432219
+ ↓↓↓
+1219
+
+Answer: 1219
+*/

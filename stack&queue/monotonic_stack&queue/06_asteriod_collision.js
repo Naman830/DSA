08080a8271fc5589de043b665c785d27b1b99763
Naman <namansingla830@gmail.com>
2026-08-21 09:25:33 +0530

    add optimal solution explanation

--- stack&queue/monotonic_stack&queue/06_asteriod_collision.js
+++ stack&queue/monotonic_stack&queue/06_asteriod_collision.js
@@ -22,3 +22,11 @@ Only then can they collide.
 // 1. Brute Force
 // The problem is that after every collision, we may need to scan the array again.
 // Time:  O(n²) && Space: O(n)
+
+// 2. Optimal Approach — Monotonic Stack
+/*
+For every asteroid:-
+
+1. If there is no possible collision, simply push it.
+2. If there is a collision compare their sizes.
+*/

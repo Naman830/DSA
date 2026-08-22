87c224b00ab7a4c02c90ddd3b4ed43eb56504aa0
Naman <namansingla830@gmail.com>
2026-08-22 06:46:30 +0530

    add brute force explanation'

---
+++ stack&queue/monotonic_stack&queue/09_larget_rectangle_in_histogram'/02_solution.js
@@ -0,0 +1,13 @@
+/*
+1. Brute Force
+
+For every bar:
+i. Take its height.
+ii. Expand left until you find a smaller bar.
+iii. Expand right until you find a smaller bar.
+iv. Calculate the area.
+
+This checks many bars repeatedly.
+Time: O(n²)
+Space: O(1)
+*/

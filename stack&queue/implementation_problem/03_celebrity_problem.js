b4eff421ac3770e6d677426200c0b03b2eaaf8af
Naman <namansingla830@gmail.com>
2026-08-23 07:54:48 +0530

    add explanation

---
+++ stack&queue/implementation_problem/03_celebrity_problem.js
@@ -0,0 +1,19 @@
+/*
+You are given n people and a matrix knows[i][j].
+
+knows[i][j] = 1 means person i knows person j.
+
+A celebrity must satisfy both:
+Celebrity knows nobody → their entire row is 0.
+Everybody knows the celebrity → their entire column is 1 except themselves.
+
+Example:
+      0  1  2
+0     0  1  0
+1     0  0  0
+2     1  1  0
+
+Person 1 is celebrity because:
+Person 1 knows nobody → row 1 = [0,0,0]
+Everybody knows person 1 → column 1 = [1,0,1]
+*/

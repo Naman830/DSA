5f1d5816f5044b1ddbaa7596bf1490652739cdb1
Naman <namansingla830@gmail.com>
2026-08-23 07:18:10 +0530

    add explanation

---
+++ stack&queue/implementation_problem/02_stock_span.js
@@ -0,0 +1,16 @@
+/*
+For each day's stock price, find how many consecutive days before today had a price ≤ today's price.
+
+Example:
+Prices:  [100, 80, 60, 70, 60, 75, 85]
+Span:    [  1,  1,  1,  2,  1,  4,  6]
+
+For 75:
+
+60 ≤ 75 ✅
+70 ≤ 75 ✅
+60 ≤ 75 ✅
+80 > 75 ❌ stop
+
+Span = 4
+*/

334579798b15984217ecfdeeae2144bbf22bfa36
Naman <namansingla830@gmail.com>
2026-08-23 08:18:20 +0530

    add menaing of lru cache

---
+++ stack&queue/implementation_problem/04_LRU_cache/01_basic.js
@@ -0,0 +1,7 @@
+/*
+Meaning:-
+
+LRU = Least Recently Used
+
+An LRU Cache is a cache that has a fixed capacity. When the cache becomes full and we need to insert something new, we remove the item that has not been used for the longest time.
+*/

1ac7185e5a67ca499a21cb1bffed4fc0baf5c4b5
Naman <namansingla830@gmail.com>
2026-08-23 06:46:13 +0530

    explaain deque

---
+++ stack&queue/implementation_problem/01_sliding_window_maximum/02_optimal_solution.js
@@ -0,0 +1,18 @@
+/*
+Optimal — Monotonic Deque
+
+A Deque (Double-Ended Queue) is a data structure where you can add and remove elements from both ends.
+
+        ← remove / add
+      ┌───────────────┐
+Front │  1  2  3  4  │ Rear
+      └───────────────┘
+        add / remove →
+Main operations
+pushFront() → add at front
+pushBack() → add at back
+popFront() → remove from front
+popBack() → remove from back
+
+Think: Deque = Queue + ability to work from both ends
+*/
---
+++ stack&queue/implementation_problem/01_sliding_window_maximum/dequeue.js
@@ -0,0 +1,16 @@
+/*
+A Deque (Double-Ended Queue) is a data structure where you can add and remove elements from both ends.
+
+        ← remove / add
+      ┌───────────────┐
+Front │  1  2  3  4  │ Rear
+      └───────────────┘
+        add / remove →
+Main operations
+pushFront() → add at front
+pushBack() → add at back
+popFront() → remove from front
+popBack() → remove from back
+
+Think: Deque = Queue + ability to work from both ends
+*/

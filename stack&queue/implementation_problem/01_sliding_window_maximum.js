12ece8c319796060853af9121eda3620c470dd5c
Naman <namansingla830@gmail.com>
2026-08-23 06:42:58 +0530

    add better approach

--- stack&queue/implementation_problem/01_sliding_window_maximum.js
+++ stack&queue/implementation_problem/01_sliding_window_maximum.js
@@ -18,6 +18,7 @@ Answer = [3, 3, 5, 5, 6, 7]
 */
 
 /*
+1. BRUTE FORCE
 For every window, simply scan all k elements and find the maximum.
 [1, 3, -1] → scan → 3
 [3, -1, -3] → scan → 3
@@ -27,4 +28,21 @@ Time: O(n * k)
 Space: O(1)
 
 This is too slow when n and k are large.
+*/
+
+/*
+2. Better Approach — Max Heap
+We can use a Max Heap to keep the largest element.
+
+Each time the window moves:
+
+i. Add the new element.
+ii. Remove elements outside the window.
+iii. Maximum is at the top of heap.
+
+Complexity
+Time: O(n log k)
+Space: O(k)
+
+Better, but we can do O(n).
 */

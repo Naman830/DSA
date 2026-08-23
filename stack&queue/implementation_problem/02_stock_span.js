9f849735dd92efe10505039f2119bb12b535d83c
Naman <namansingla830@gmail.com>
2026-08-23 07:19:23 +0530

    add key observation

--- stack&queue/implementation_problem/02_stock_span.js
+++ stack&queue/implementation_problem/02_stock_span.js
@@ -13,4 +13,12 @@ For 75:
 80 > 75 ❌ stop
 
 Span = 4
+
+Key observation:-
+We need to find the Previous Greater Element (PGE).
+
+i. If PGE exists at index j:            span = i - j
+ii. If there is no greater element:     span = i + 1
+
+So Stock Span is basically a Previous Greater Element problem.
 */

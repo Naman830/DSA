6ab574894410d0437d76b7251f436c02ffa4788d
Naman <namansingla830@gmail.com>
2026-08-23 06:40:46 +0530

    add explanation

---
+++ stack&queue/implementation_problem/01_sliding_window_maximum.js
@@ -0,0 +1,18 @@
+/*
+You are given an array and a window of size k.
+The window moves from left → right. For every window, find the maximum element.
+
+Example:
+arr = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+
+Windows:
+[1,  3, -1] → 3
+[3, -1, -3] → 3
+[-1,-3,  5] → 5
+[-3, 5,  3] → 5
+[5,  3,  6] → 6
+[3,  6,  7] → 7
+
+Answer = [3, 3, 5, 5, 6, 7]
+*/

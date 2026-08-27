c0005510a3bb414cc4f1dc3e62e10c670f6cd7b3
Naman <namansingla830@gmail.com>
2026-08-27 07:31:50 +0530

    add explanation

---
+++ Recursion/Get_a_strong_hold/02_pow(x,y).js
@@ -0,0 +1,16 @@
+/*
+You are given:
+x = 2
+n = 10
+
+You need to calculate:
+2¹⁰ = 1024
+
+In other words:
+Return x raised to the power n.
+
+Examples:
+myPow(2, 10)   → 1024
+myPow(2.1, 3)  → 9.261
+myPow(2, -2)   → 0.25 ----> if number is negative 1 / x
+*/

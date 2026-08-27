e22b974a01099375bf7648f67faf24c9b132ebe7
Naman <namansingla830@gmail.com>
2026-08-27 07:38:33 +0530

    add brute force solution

--- Recursion/Get_a_strong_hold/02_pow(x,y).js
+++ Recursion/Get_a_strong_hold/02_pow(x,y).js
@@ -18,3 +18,26 @@ myPow(2, -2)   → 0.25 ----> if number is negative  1 / x
 // Brute Force
 // TC: O(n) && SC: O(1)
 // Simply multiply x by itself n times.
+
+function myPowBrute(x, n) {
+  // Base case 1:- If our n value is equal to 0 or 1 return 1
+  if (n === 0 || n === 1) return 1;
+
+  // Check does number is negative
+  let temp = n;
+  if (n < 0) {
+    x = 1 / x;
+    temp = -n;
+  }
+
+  // Simply now multiply the x by itself and return it
+  let ans = 1;
+
+  for (let i = 0; i < n.length; i++) {
+    ans *= x;
+  }
+
+  return ans;
+}
+
+console.log(myPowBrute(2.0, 10));

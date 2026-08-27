3d19c63d75d1ba55e6abefc9e8e2f2fcffa50cf4
Naman <namansingla830@gmail.com>
2026-08-27 08:31:28 +0530

    add explanation of better approach

---
+++ Recursion/Get_a_strong_hold/03_count_good_number/03_better.js
@@ -0,0 +1,11 @@
+/*
+Direct Multiplication
+
+We don't need to actually generate the numbers.
+We already know:
+
+even index → 5 choices
+odd index  → 4 choices
+
+So we simply multiply the number of choices for every position.
+*/

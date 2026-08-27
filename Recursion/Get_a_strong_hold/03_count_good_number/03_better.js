4a8f3737ba9bcfdb5789ec95243fd2b30077f6a5
Naman <namansingla830@gmail.com>
2026-08-27 08:33:23 +0530

    add js doe

--- Recursion/Get_a_strong_hold/03_count_good_number/03_better.js
+++ Recursion/Get_a_strong_hold/03_count_good_number/03_better.js
@@ -9,3 +9,29 @@ odd index  → 4 choices
 
 So we simply multiply the number of choices for every position.
 */
+
+function countGoodNumbersBetter(n) {
+  const MOD = 1e7 + 7;
+
+  let answer = 1;
+
+  // Go through every position
+  for (let i = 0; i < n; i++) {
+    // Even index → 5 choices
+    if (i % 2 === 0) {
+      answer = (answer * 5) % MOD;
+    }
+
+    // Odd index → 4 choices
+    else {
+      answer = (answer * 4) % MOD;
+    }
+  }
+
+  return answer;
+}
+
+console.log(countGoodNumbersBetter(1)); // 5
+console.log(countGoodNumbersBetter(2)); // 20
+console.log(countGoodNumbersBetter(4)); // 400
+console.log(countGoodNumbersBetter(5)); // 2000

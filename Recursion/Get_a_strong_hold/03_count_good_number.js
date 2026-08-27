9e5e414e5e200c17704d4b799e9c06fedc38ca5a
Naman <namansingla830@gmail.com>
2026-08-27 07:59:57 +0530

    add explanation

---
+++ Recursion/Get_a_strong_hold/03_count_good_number.js
@@ -0,0 +1,21 @@
+/*
+The actual problem becomes:
+
+I have n positions.
+
+At every even position I have 5 choices.
+At every odd position I have 4 choices.
+
+How many different strings can I make?
+
+For example:
+n = 5
+
+positions:
+0     1     2     3     4
+↓     ↓     ↓     ↓     ↓
+5     4     5     4     5 choices
+
+Answer:
+5 × 4 × 5 × 4 × 5
+*/

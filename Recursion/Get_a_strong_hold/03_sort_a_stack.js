5575df201b0dd2fb2f51d05c72a5f4cf346d9860
Naman <namansingla830@gmail.com>
2026-08-29 06:25:54 +0530

    explain how to sort

---
+++ Recursion/Get_a_strong_hold/03_sort_a_stack.js
@@ -0,0 +1,12 @@
+/*
+The goal is to sort all elements of a stack using recursion, without using another explicit stack/data structure.
+
+for example:-
+Before:        After:
+
+   1              3
+   3              2
+   2              1
+   4              4
+  ---            ---
+*/

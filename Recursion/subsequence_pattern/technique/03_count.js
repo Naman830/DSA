f66c70872e0bf2c6cee85be50e87163637c1c2e1
Naman <namansingla830@gmail.com>
2026-08-29 07:32:57 +0530

    add explanation

---
+++ Recursion/subsequence_pattern/technique/03_count.js
@@ -0,0 +1,13 @@
+/*
+Number of answers = answers when we TAKE + answers when we DON'T TAKE
+                 COUNT
+                /     \
+             TAKE    NOT TAKE
+               ↓         ↓
+             count     count
+                \       /
+                 \     /
+                ADD THEM
+                   ↓
+             final count
+*/

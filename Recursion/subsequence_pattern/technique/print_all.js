8d14834159d85b312aadeda6752c81beee4b01d8
Naman <namansingla830@gmail.com>
2026-08-29 07:00:38 +0530

    add js code

--- Recursion/subsequence_pattern/technique/print_all.js
+++ Recursion/subsequence_pattern/technique/print_all.js
@@ -116,3 +116,50 @@ function solve(index, current) {
     solve(index + 1, current);
 }
 */
+
+/*
+Problem:
+Given an array, print all possible subsequences.
+
+Example:
+Input:  [3, 1]
+
+Output:
+[3, 1]
+[3]
+[1]
+[]
+*/
+
+function printAllSubsequences(index, arr, current) {
+  // Base case:
+  // We have processed all elements.
+  if (index === arr.length) {
+    console.log(current);
+    return;
+  }
+
+  // --------------------------------
+  // 1. TAKE the current element
+  // --------------------------------
+  current.push(arr[index]);
+
+  // 2. RECURSE
+  // Move to the next element
+  printAllSubsequences(index + 1, arr, current);
+
+  // --------------------------------
+  // 3. BACKTRACK / UNDO
+  // --------------------------------
+  // Remove the element so that we
+  // can explore the "don't take" branch.
+  current.pop();
+
+  // --------------------------------
+  // 4. DON'T TAKE the current element
+  // --------------------------------
+  printAllSubsequences(index + 1, arr, current);
+}
+
+// Test
+printAllSubsequences(0, [3, 1], []);

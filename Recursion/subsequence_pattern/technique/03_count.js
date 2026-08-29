59cb20d08477de352c0b91ee9a73ed70392e22ed
Naman <namansingla830@gmail.com>
2026-08-29 07:33:46 +0530

    addd js code

--- Recursion/subsequence_pattern/technique/03_count.js
+++ Recursion/subsequence_pattern/technique/03_count.js
@@ -11,3 +11,39 @@ Number of answers = answers when we TAKE + answers when we DON'T TAKE
                    ↓
              final count
 */
+
+function countSubsets(index, arr, sum, target) {
+  // Base case:
+  // We have processed every element
+  if (index === arr.length) {
+    // If current subset has target sum,
+    // count this as ONE valid answer
+    if (sum === target) {
+      return 1;
+    }
+
+    // This path does not give a valid answer
+    return 0;
+  }
+
+  // -------------------------
+  // 1. TAKE current element
+  // -------------------------
+
+  let take = countSubsets(index + 1, arr, sum + arr[index], target);
+
+  // -------------------------
+  // 2. DON'T TAKE current element
+  // -------------------------
+
+  let notTake = countSubsets(index + 1, arr, sum, target);
+
+  // Total answers from both choices
+  return take + notTake;
+}
+
+// Test
+console.log(countSubsets(0, [1, 2, 3], 0, 3));
+
+// Output:
+// 2

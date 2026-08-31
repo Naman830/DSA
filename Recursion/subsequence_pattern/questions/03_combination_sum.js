9014302a8eb060b10455bbdad1e73af7028ce947
Naman <namansingla830@gmail.com>
2026-08-31 07:17:36 +0530

    add explaantion

---
+++ Recursion/subsequence_pattern/questions/03_combination_sum.js
@@ -0,0 +1,16 @@
+/*
+1. You are given an array candidates and a target.
+2. You need to find all combinations whose sum equals target.
+3. Important: you can use the same number multiple times.
+
+Example:-
+candidates = [2, 3, 6, 7]
+target = 7
+
+Possible combinations:
+2 + 2 + 3 = 7
+7 = 7
+
+So:
+[[2,2,3], [7]]
+*/

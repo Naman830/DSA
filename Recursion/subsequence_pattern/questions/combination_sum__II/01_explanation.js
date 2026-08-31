dc7b904c5375b590db1f3ec11f99cc6dd71b9b82
Naman <namansingla830@gmail.com>
2026-08-31 07:51:14 +0530

    add explnation

---
+++ Recursion/subsequence_pattern/questions/combination_sum__II/01_explanation.js
@@ -0,0 +1,11 @@
+/*
+You are given an array candidates and a target.
+You need to find all unique combinations whose sum equals target.
+
+Important difference from Combination Sum I
+
+In Combination Sum II:
+Each number can be used at most once.
+The array can contain duplicates.
+The answer must contain unique combinations.
+*/

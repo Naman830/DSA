2ed6b1eca2613f06ec34677c2bac1be582e06455
Naman <namansingla830@gmail.com>
2026-08-31 07:18:19 +0530

    add key observation

--- Recursion/subsequence_pattern/questions/03_combination_sum.js
+++ Recursion/subsequence_pattern/questions/03_combination_sum.js
@@ -13,4 +13,18 @@ Possible combinations:
 
 So:
 [[2,2,3], [7]]
+
+2. Key observations
+Same element can be picked multiple times.
+
+Order does NOT matter.
+[2,3,2] == [2,2,3]
+
+We only want one of them.
+
+If sum === target → we found an answer.
+If sum > target → stop exploring that path.
+
+The most important idea is:
+After picking a number, we stay at the same index because we are allowed to pick it again.
 */

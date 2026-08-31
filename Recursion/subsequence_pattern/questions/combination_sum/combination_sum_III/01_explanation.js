510d431a92d62215eea61ed03930ab0893199ebb
Naman <namansingla830@gmail.com>
2026-08-31 08:31:04 +0530

    add example

--- Recursion/subsequence_pattern/questions/combination_sum/combination_sum_III/01_explanation.js
+++ Recursion/subsequence_pattern/questions/combination_sum/combination_sum_III/01_explanation.js
@@ -5,4 +5,13 @@ k → how many numbers you must choose
 n → target sum
 
 Choose exactly k distinct numbers from 1 to 9 such that their sum is exactly n.
+
+Example:
+Input: k = 3, n = 7
+
+Output:
+[[1,2,4]]
+
+Because:
+1 + 2 + 4 = 7
 */

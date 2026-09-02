dabec45a6c385f73dd392f5f2fdbef9fa39d7459
Naman <namansingla830@gmail.com>
2026-09-02 09:30:00 +0530

    add explanation

---
+++ Recursion/subsequence_pattern/questions/03_letter_combo_of_a_phoneNumber.js
@@ -0,0 +1,7 @@
+/*
+Explanation:-
+You are given a string of digits from 2 to 9.
+Each digit represents some letters, just like on an old phone keypad:
+You need to return all possible strings that can be formed by choosing one letter from each digit.
+
+*/

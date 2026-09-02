1212d051091e11aa730843a14e44225cca00f00a
Naman <namansingla830@gmail.com>
2026-09-02 09:30:11 +0530

    add key observation'

--- Recursion/subsequence_pattern/questions/03_letter_combo_of_a_phoneNumber.js
+++ Recursion/subsequence_pattern/questions/03_letter_combo_of_a_phoneNumber.js
@@ -4,4 +4,11 @@ You are given a string of digits from 2 to 9.
 Each digit represents some letters, just like on an old phone keypad:
 You need to return all possible strings that can be formed by choosing one letter from each digit.
 
+Key Observation:-
+Every digit = one recursion level.
+At each level, we have 3 or 4 choices.
+We choose one letter and move to the next digit.
+When all digits are processed, we have one complete answer.
+After exploring one choice, we backtrack and try the next choice.
+If digits === "", return [].
 */

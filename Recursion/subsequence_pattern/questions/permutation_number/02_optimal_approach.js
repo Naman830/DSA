2edc8d07956ebd54ca7b7fe8614b21a3100ec642
Naman <namansingla830@gmail.com>
2026-09-03 07:23:48 +0530

    add js code

--- Recursion/subsequence_pattern/questions/permutation_number/02_optimal_approach.js
+++ Recursion/subsequence_pattern/questions/permutation_number/02_optimal_approach.js
@@ -13,3 +13,38 @@ After generating all permutations starting with 2, we must restore:
 
 This is called backtracking
 */
+
+function permute(nums) {
+  const result = [];
+  function backtrack(index) {
+    if (index === nums.length) {
+      result.push([...nums]);
+      return;
+    }
+
+    for (let i = index; i < nums.length; i++) {
+      // Choose:
+      // Put nums[i] at nums[index]
+      [nums[index], nums[i]] = [nums[i], nums[index]];
+
+      // Explore:
+      // Generate permutations for the remaining positions
+      backtrack(index + 1);
+
+      // Undo:
+      // Restore the original array for the next choice
+      [nums[index], nums[i]] = [nums[i], nums[index]];
+    }
+  }
+
+  backtrack(0);
+
+  return result;
+}
+
+// Test cases
+console.log(permute([1, 2, 3]));
+
+console.log(permute([0, 1]));
+
+console.log(permute([1]));

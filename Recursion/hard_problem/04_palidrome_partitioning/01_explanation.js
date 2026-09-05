0e9dae1d98808fd3ba5ac6ad0eeaeaf60e85d306
Naman <namansingla830@gmail.com>
2026-09-05 08:11:24 +0530

    add explanation

---
+++ Recursion/hard_problem/04_palidrome_partitioning/01_explanation.js
@@ -0,0 +1,19 @@
+/*
+Explanation:-
+Given a string s, partition it into pieces such that every piece is a palindrome.
+
+Return all possible palindrome partitions.
+
+Example
+Input:  "aab"
+
+Possible partitions:
+["a", "a", "b"]   → all are palindrome
+["aa", "b"]       → all are palindrome
+
+Answer:
+[
+  ["a", "a", "b"],
+  ["aa", "b"]
+]
+*/

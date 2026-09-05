adf5fa5b5ad0416e5b510e16b93352d5167b7a6e
Naman <namansingla830@gmail.com>
2026-09-05 06:55:24 +0530

    add explanation

---
+++ Recursion/hard_problem/02_	Sudoku /01_explanation.js
@@ -0,0 +1,12 @@
+/*
+Explanation:-
+You are given a 9 × 9 Sudoku board. Some cells are filled and some are ".".
+
+Fill all empty cells so that:
+
+Each row contains 1–9 only once.
+Each column contains 1–9 only once.
+Each 3 × 3 box contains 1–9 only once.
+
+The goal is to modify the board in-place.
+*/

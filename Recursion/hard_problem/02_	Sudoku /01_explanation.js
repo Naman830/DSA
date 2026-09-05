5066b6bc39c759ca38d9d5a66354074052fe67cb
Naman <namansingla830@gmail.com>
2026-09-05 06:56:00 +0530

    add key observation

--- Recursion/hard_problem/02_	Sudoku /01_explanation.js
+++ Recursion/hard_problem/02_	Sudoku /01_explanation.js
@@ -10,3 +10,18 @@ Each 3 × 3 box contains 1–9 only once.
 
 The goal is to modify the board in-place.
 */
+
+/*
+Key Observation:-
+
+For every empty cell:
+
+Try numbers 1 → 9.
+Check whether the number is valid in its row, column, and 3×3 box.
+If valid → place it.
+Recursively solve the remaining board.
+If it doesn't work → undo it (backtrack) and try the next number.
+
+The main idea:
+Choose → Explore → Undo
+*/

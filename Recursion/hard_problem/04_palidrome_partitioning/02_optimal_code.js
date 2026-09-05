e34375d7edd40092614d7cc930f34baa9d316a46
Naman <namansingla830@gmail.com>
2026-09-05 08:18:24 +0530

    add full js code

--- Recursion/hard_problem/04_palidrome_partitioning/02_optimal_code.js
+++ Recursion/hard_problem/04_palidrome_partitioning/02_optimal_code.js
@@ -16,4 +16,49 @@ function partition(s) {
     // All characters matched
     return true;
   }
+
+  function backtrack(start) {
+    // If we reached the end of the string,
+    // we have created one complete valid partition.
+    if (start === s.length) {
+      result.push([...current]);
+      return;
+    }
+
+    // Try every possible substring starting from `start`
+    for (let end = start; end < s.length; end++) {
+      if (!isPalindrome(start, end)) {
+        continue;
+      }
+
+      // Choose:
+      // Add the palindrome substring to our current partition
+      current.push(s.slice(start, end + 1));
+
+      backtrack(end + 1);
+
+      current.pop();
+    }
+  }
+  // Start partitioning from index 0
+  backtrack(0);
+
+  return result;
 }
+
+// Test cases
+
+console.log(partition("aab"));
+// [
+//   [ 'a', 'a', 'b' ],
+//   [ 'aa', 'b' ]
+// ]
+
+console.log(partition("a"));
+// [ [ 'a' ] ]
+
+console.log(partition("aba"));
+// [
+//   [ 'a', 'b', 'a' ],
+//   [ 'aba' ]
+// ]

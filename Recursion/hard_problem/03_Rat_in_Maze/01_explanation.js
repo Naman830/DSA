1af26756950d2878bb3fa5979fdb838c3227ea8e
Naman <namansingla830@gmail.com>
2026-09-05 07:40:47 +0530

    add explanation

---
+++ Recursion/hard_problem/03_Rat_in_Maze/01_explanation.js
@@ -0,0 +1,9 @@
+/*
+Explanation:-
+The goal is to move a rat from the top-left (0,0) to the bottom-right (n-1,n-1) of a maze.
+
+1 → cell is open, we can move.
+0 → cell is blocked.
+We try a direction → if it works, continue.
+If it leads to a dead end → backtrack and try another direction.
+*/

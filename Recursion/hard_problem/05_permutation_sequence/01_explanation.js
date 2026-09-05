49d4c13246f76bd4f3f5abddd4d9954e48e89ad5
Naman <namansingla830@gmail.com>
2026-09-05 09:20:19 +0530

    add explanation

---
+++ Recursion/hard_problem/05_permutation_sequence/01_explanation.js
@@ -0,0 +1,23 @@
+/*
+Explanation:-
+
+You are given:
+n → numbers from 1 to n
+k → which permutation you want
+
+Return the k-th permutation in lexicographical (dictionary) order.
+Example:
+
+n = 3
+Permutations:
+123 -1
+132 -2
+213 -3
+231 -4
+312
+321
+
+k = 4
+
+Answer = "231"
+*/

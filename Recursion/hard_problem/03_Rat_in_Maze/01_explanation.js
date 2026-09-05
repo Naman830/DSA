556a7b8b04b83aad2e70d72c0065c312d71ca116
Naman <namansingla830@gmail.com>
2026-09-05 07:41:20 +0530

    add key observation

--- Recursion/hard_problem/03_Rat_in_Maze/01_explanation.js
+++ Recursion/hard_problem/03_Rat_in_Maze/01_explanation.js
@@ -7,3 +7,21 @@ The goal is to move a rat from the top-left (0,0) to the bottom-right (n-1,n-1) 
 We try a direction → if it works, continue.
 If it leads to a dead end → backtrack and try another direction.
 */
+
+/*
+Key Observation:-
+Suppose:
+
+1  0  0  0
+1  1  0  1
+1  1  0  0
+0  1  1  1
+
+One valid path is:
+🐀 → ↓ → ↓ → → → ↓ "DLRU"
+
+The important idea:
+Choose → Explore → Undo → Try next choice
+
+That's the core of backtracking.
+*/

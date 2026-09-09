8c077147a0c8367951859978c65563a33f60b347
Naman <namansingla830@gmail.com>
2026-09-09 08:45:18 +0530

    add js code with tc and sc

--- Binary_Trees/Traversals/DFS/02_iiterative_traversal/03_postorder_traversal/02_one_stack.js
+++ Binary_Trees/Traversals/DFS/02_iiterative_traversal/03_postorder_traversal/02_one_stack.js
@@ -27,4 +27,67 @@ Check Right
        ↓
 Right not processed → Go Right
 Right processed      → Pop + Visit
+
+TC: O(n)
+SC: O(n)
 */
+
+// Create a Binary Tree Node
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+function postorderTraversal(root) {
+  const result = [];
+  const stack = [];
+
+  // If tree is empty
+  if (root === null) {
+    return result;
+  }
+
+  let current = root;
+  let lastVisited = null;
+
+  while (current !== null || stack.length > 0) {
+    // 1. Go as left as possible
+    while (current !== null) {
+      stack.push(current);
+      current = current.left;
+    }
+
+    // Node at top of stack
+    const node = stack[stack.length - 1];
+
+    // 2. If right child exists and is not processed,
+    //    go to the right subtree
+    if (node.right !== null && node.right !== lastVisited) {
+      current = node.right;
+    }
+
+    // 3. Otherwise, both left and right are processed
+    //    so we can visit the current node
+    else {
+      result.push(node.val);
+
+      // Remember this node as the last processed node
+      lastVisited = stack.pop();
+    }
+  }
+
+  return result;
+}
+
+const root = new TreeNode(1);
+
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+console.log("Postorder:", postorderTraversal(root));
+// Output: [4, 5, 2, 3, 1]

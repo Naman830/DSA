2b9f273774ce3551a6a777e91961b629fce5ed3a
Naman <namansingla830@gmail.com>
2026-09-09 07:53:19 +0530

    add tc and sc

--- Binary_Trees/Traversals/DFS/02_iiterative_traversal/01_preoder_traversal.js
+++ Binary_Trees/Traversals/DFS/02_iiterative_traversal/01_preoder_traversal.js
@@ -4,6 +4,10 @@ Key Obervation:-
 Preorder = Root → Left → Right
 Use a stack to simulate recursion.
 Push the right child first, then the left child because stack is LIFO.
+
+Complexity:-
+TC: O(n)
+SC: O(n)
 */
 
 class TreeNode {

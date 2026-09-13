a0479ea1daea5e5f2f8dbafa0ad06225fd8a9318
Naman <namansingla830@gmail.com>
2026-09-13 14:46:06 +0530

    add main js code

--- Binary_Trees/Traversals/DFS/01_Traversals/02_in_pre_post_traversal.js
+++ Binary_Trees/Traversals/DFS/01_Traversals/02_in_pre_post_traversal.js
@@ -19,13 +19,71 @@ Postorder = 3rd time we see the node
 
 we use: [node, state]
 */
-
 class TreeNode {
   constructor(val) {
     this.val = val;
     this.left = null;
     this.right = null;
   }
+}
+
+function allTraversals(root) {
+  // Store the three traversals
+  let preorder = [];
+  let inorder = [];
+  let postorder = [];
+
+  // If tree is empty, return empty arrays
+  if (root === null) {
+    return [preorder, inorder, postorder];
+  }
+
+  // Stack stores [node, state]
+  let stack = [];
+
+  // Start with root and state 1
+  stack.push([root, 1]);
+
+  while (stack.length > 0) {
+    // Remove the top element
+    let [node, state] = stack.pop();
+
+    // STATE 1: Preorder
+    if (state === 1) {
+      // Visit root
+      preorder.push(node.val);
+
+      // Move to state 2
+      stack.push([node, 2]);
+
+      // Process left subtree
+      if (node.left !== null) {
+        stack.push([node.left, 1]);
+      }
+    }
+
+    // STATE 2: Inorder
+    else if (state === 2) {
+      // Visit root
+      inorder.push(node.val);
+
+      // Move to state 3
+      stack.push([node, 3]);
+
+      // Process right subtree
+      if (node.right !== null) {
+        stack.push([node.right, 1]);
+      }
+    }
+
+    // STATE 3: Postorder
+    else {
+      // Visit root
+      postorder.push(node.val);
+    }
+  }
+
+  return [preorder, inorder, postorder];
 }
 
 // Create tree

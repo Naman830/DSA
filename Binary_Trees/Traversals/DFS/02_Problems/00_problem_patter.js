4ed5c95d7913651a36abc7edfb9dd700fa3294d3
Naman <namansingla830@gmail.com>
2026-09-13 14:49:34 +0530

    add pattern

---
+++ Binary_Trees/Traversals/DFS/02_Problems/00_problem_patter.js
@@ -0,0 +1,31 @@
+/*
+GROUP 1
+│
+├── 1. Maximum Depth
+├── 2. Same Tree
+├── 3. Symmetric Tree
+└── 4. Invert Binary Tree
+        │
+        ▼
+GROUP 2
+│
+├── 5. Balanced Binary Tree
+├── 6. Path Sum
+├── 7. Path Sum II
+└── 8. Diameter of Binary Tree
+        │
+        ▼
+GROUP 3
+│
+├── 9. Zigzag Level Order Traversal
+├── 10. Right Side View
+├── 11. Top View
+└── 12. Bottom View
+        │
+        ▼
+GROUP 4
+│
+├── 13. Boundary Traversal
+├── 14. Vertical Order Traversal
+└── 15. Lowest Common Ancestor
+*/

87d856fba77b1a8b70a8c6d545d41c23949ab6e6
Naman <namansingla830@gmail.com>
2026-09-15 08:39:31 +0530

    add explanation

---
+++ Binary_Trees/Traversals/DFS/02_Problems/03_symmetric_tree.js
@@ -0,0 +1,22 @@
+/*
+1. Explain the question
+Given a binary tree, check whether it is symmetric around its center.
+In simple words, the left subtree should be a mirror image of the right subtree.
+
+Example:
+
+        1
+       / \
+      2   2
+     / \ / \
+    3  4 4  3
+Output: true
+
+
+        1
+       / \
+      2   2
+     /     \
+    3       3
+Output: false
+*/

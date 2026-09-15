6cddc5e06f51248708112ac1bc73585d665303ca
Naman <namansingla830@gmail.com>
2026-09-15 08:04:26 +0530

    add explanation

---
+++ Binary_Trees/Traversals/DFS/02_Problems/01_maximum_depth.js
@@ -0,0 +1,12 @@
+/*
+You are given the root of a binary tree. Find its maximum depth, which means the number of nodes on the longest path from the root to a leaf.
+
+        1
+       / \
+      2   3
+     /
+    4
+
+The longest path is 1 → 2 → 4.
+Therefore, the maximum depth is 3.
+*/

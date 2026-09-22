9d21d66db856810d7104e809f765d01ae1443adb
Naman <namansingla830@gmail.com>
2026-09-22 07:17:30 +0530

    add explanation

---
+++ Binary_Trees/Traversals/DFS/02_Problems/06_maximum_path_sum.js
@@ -0,0 +1,23 @@
+/*
+Explanation:-
+
+A path is a sequence of connected nodes where you can start and end at any two nodes.
+The path does not need to pass through the root.
+
+Our goal is to find the maximum possible sum of node values along any path.
+
+Example:
+        1
+       / \
+      2   3
+         / \
+        4   5
+
+The maximum path is:
+4 → 3 → 5
+
+Sum:
+4 + 3 + 5 = 12
+
+So the answer is 12.
+*/

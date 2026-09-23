08eb0ef98a93c9dd033bbaeba857067eefc3c437
Naman <namansingla830@gmail.com>
2026-09-23 06:14:45 +0530

    addd explanation

---
+++ Binary_Trees/Traversals/DFS/02_Problems/07_children_sum_property.js
@@ -0,0 +1,19 @@
+/*
+The Children Sum Property says:
+For every non-leaf node, its value must be equal to the sum of its left and right child values.
+
+For a missing child, consider its value as 0.
+
+Example:
+        10
+       /  \
+      4    6
+     / \    \
+    2   2    6
+
+10 = 4 + 6 ✅
+4 = 2 + 2 ✅
+6 = 0 + 6 ✅
+
+So the tree satisfies the property.
+*/

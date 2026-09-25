43e7942d790e37cde2b21e7f363b769423b721f9
Naman <namansingla830@gmail.com>
2026-09-25 07:53:50 +0530

    add explanation

---
+++ Binary_Trees/Traversals/BFS/02_View/03_right&left_view.js
@@ -0,0 +1,20 @@
+/*
+Explain the Question
+Given a binary tree, we need to find:
+
+Right View: nodes visible when looking at the tree from the right side.
+Left View: nodes visible when looking at the tree from the left side.
+
+Example:
+          1
+        /   \
+       2     3
+      / \     \
+     4   5     6
+        /
+       7
+Left View  = [1, 2, 4, 7]
+Right View = [1, 3, 6, 7]
+
+The key idea is that we need exactly one node from every level.
+*/

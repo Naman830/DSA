e10acbf2b1e7a558f27b8de9e08aad432bfaf1a9
Naman <namansingla830@gmail.com>
2026-09-25 07:57:37 +0530

    add js code

--- Binary_Trees/Traversals/BFS/02_View/03_right&left_view.js
+++ Binary_Trees/Traversals/BFS/02_View/03_right&left_view.js
@@ -32,4 +32,61 @@ Left view → visit left before right.
 If result.length === level, we have reached that level for the first time.
 */
 
+/*
+1. For Right View, perform:
+Root → Right → Left
 
+2. For Left View, perform:
+Root → Left → Right
+*/
+
+// Optimal Right View
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function rightSideView(root) {
+  const result = [];
+
+  function dfs(node, level) {
+    // Base case
+    if (node === null) return;
+
+    // First node reached at this level
+    // Because we visit RIGHT first,
+    // this node belongs to the right view.
+    if (level === result.length) {
+      result.push(node.val);
+    }
+
+    // Visit right subtree first
+    dfs(node.right, level + 1);
+
+    // Then visit left subtree
+    dfs(node.left, level + 1);
+  }
+
+  dfs(root, 0);
+
+  return result;
+}
+
+// Creating tree
+const root = new TreeNode(1);
+
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+root.right.right = new TreeNode(6);
+
+root.left.right.left = new TreeNode(7);
+
+console.log("Right View:", rightSideView(root));
+// [1, 3, 6, 7]

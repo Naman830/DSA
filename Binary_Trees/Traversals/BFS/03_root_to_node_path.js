e711e99304e268641b38f9c298a8058debe3a1f0
Naman <namansingla830@gmail.com>
2026-09-26 17:17:17 +0530

    add explanation

---
+++ Binary_Trees/Traversals/BFS/03_root_to_node_path.js
@@ -0,0 +1,20 @@
+/*
+Explain the question
+Given a Binary Tree and a target value, return the path from the root node to the target node.
+
+Example:
+        1
+       / \
+      2   3
+     / \
+    4   5
+       /
+      6
+
+Target = 6
+
+Output:
+[1, 2, 5, 6]
+
+We start at the root 1 and find the sequence of nodes that leads to 6.
+*/

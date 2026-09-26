109ebd67c12327a6fb31d3e0c140c2b0305a6d36
Naman <namansingla830@gmail.com>
2026-09-26 17:18:16 +0530

    add key observation

--- Binary_Trees/Traversals/BFS/03_root_to_node_path.js
+++ Binary_Trees/Traversals/BFS/03_root_to_node_path.js
@@ -18,3 +18,19 @@ Output:
 
 We start at the root 1 and find the sequence of nodes that leads to 6.
 */
+
+/*
+Key observation
+
+We can use DFS to search for the target.
+While moving down the tree, add the current node to path.
+
+If the current node is the target → we found the path.
+
+Search the left and right subtrees.
+
+If the target isn't found through the current node, backtrack by removing that node from path.
+
+This is the important pattern: [WE WILL FOLLOW THE INORDER TRAVERSAL FOR FAST & EASY WAY]
+Choose → Explore → Undo
+*/
